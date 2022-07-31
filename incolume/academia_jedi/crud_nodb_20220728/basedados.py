2522d771ddd500cba95031e242b77de3f410c612
Britodfbr <britodfbr@gmail.com>
2022-07-31 11:36:25 -0300

    refactor: Tratativas aplicadas em CLI

--- incolume/academia_jedi/crud_nodb_20220728/basedados.py
+++ incolume/academia_jedi/crud_nodb_20220728/basedados.py
@@ -1,32 +1,39 @@
+import logging
 from model import Pessoa
+import logging
 
+logging.basicConfig(level=logging.WARNING)
 
 db = {}
 
+def debug_mode(status: bool = False):
+    if status:
+        logging.basicConfig(level=logging.DEBUG)
 
-def create(pessoa: Pessoa):
+def create_person(pessoa: Pessoa, debug: bool = False):
+    debug_mode(debug)
     db[pessoa.id] = pessoa
+    logging.debug(f'Adicionado: {pessoa.to_dict()}')
     return True
 
 
-def update(id:int, pessoa: Pessoa):
+def update_person(id:int, pessoa: Pessoa):
     if db.get(id):
         db.update({id: pessoa})
         return True
     return False
 
 
-def delete(id: int):
+def delete_person(id: int):
     if db.get(id):
         db.pop(id)
         return True
     return False
 
 
-def select_all():
+def select_all_person():
     return [pessoa for key, pessoa in db.items()]
 
 
-def select(id: int):
+def select_person(id: int):
     return db.get(id)
-
--- incolume/academia_jedi/crud_nodb_20220728/cli.py
+++ incolume/academia_jedi/crud_nodb_20220728/cli.py
@@ -1,24 +1,87 @@
 import click
-from basedados import select_all
+from basedados import create_person, select_all_person, select_person
+from model import Pessoa
+import click
+
+# CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
+# @click.group(context_settings=CONTEXT_SETTINGS, invoke_without_command=True)
+# @click.option('--debug/--no-debug', default=False, help='Activate debug mode.')
+# @click.pass_context
+# def cli(ctx, **kwargs):
+#     # ensure that ctx.obj exists and is a dict (in case `cli()` is called
+#     # by means other than the `if` block below)
+#     ctx.ensure_object(dict)
+#     ctx.obj.update(**kwargs)
+#     if ctx.invoked_subcommand is None:
+#         click.secho(cli.get_help(ctx), fg='red')
+
+
+# @cli.command()
+# @click.pass_context
+# def show(ctx):
+#     """Show context content."""
+#     click.secho(f'{ctx.obj}')
+
+
+# @cli.command()
+# @click.pass_context
+# def sync(ctx):
+#     """Show debug state."""
+#     click.echo('Debug is %s' % (ctx.obj['debug'] and 'on' or 'off'))
+
+
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+@click.group(context_settings=CONTEXT_SETTINGS, invoke_without_command=True)
+@click.option('--debug/--no-debug', default=False, help='Activate debug mode.')
+@click.pass_context
+def run(ctx, **kwargs):
+    # ensure that ctx.obj exists and is a dict (in case `cli()` is called
+    # by means other than the `if` block below)
+    ctx.ensure_object(dict)
+    ctx.obj.update(**kwargs)
+    if ctx.invoked_subcommand is None:
+        click.secho(run.get_help(ctx), fg='red')
 
-# Example from https://stackoverflow.com/a/50442496/5132101
-#
-# @click.command(context_settings=CONTEXT_SETTINGS)
-# @click.option('--toduhornot', is_flag=True, help='prints "duh..."')
-# def duh(toduhornot):
-#     if toduhornot:
-#         click.echo('duh...')
-#     else:
-#         with click.Context(duh) as ctx:
-#             click.echo(ctx.get_help())
+
+@run.command()
+@click.pass_context
+def show(ctx):
+    """Show context content."""
+    click.secho(f'{ctx.obj}')
 
 
-@click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('-a', '--read_all', is_flag=True, help='Read all records')
-def run(read_all):
-    if not read_all:
-        with click.Context(run) as ctx:
-            click.secho(ctx.get_help(), fg='red')
-    click.echo(select_all())
+@run.command()
+@click.option('-p', '--person', type=dict, help='Index of record.')
+@click.pass_context
+def insert(ctx, person):
+    """Insert one record into database.
+
+    ex: {
+        'name': 'name',
+        'date_born': '20/06/1978',
+        'email': ['email1', 'email2'], # optional
+        'telefone': ['phone1', 'phone2],  # optional
+        'address': ['add1', 'add2]}   # optional
+    """
+    click.secho(create_person(Pessoa(**person), ctx.obj.get('debug')), fg='green')
+
+@run.command()
+@click.pass_context
+def sync(ctx):
+    """Show debug state."""
+    click.echo('Debug is %s' % (ctx.obj['debug'] and 'on' or 'off'))
+
+@run.command()
+@click.pass_context
+def read_all(ctx):
+    """Show all records into database."""
+    click.secho(select_all_person(), fg='green')
+
+@run.command()
+@click.option('-i', '--index', type=int, help='Index of record.')
+@click.pass_context
+def read_one(ctx, index):
+    """Show one record into database."""
+    click.secho(select_person(index), fg='green')
