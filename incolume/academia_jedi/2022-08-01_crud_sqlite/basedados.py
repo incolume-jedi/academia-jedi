363ddc301be960c2995d8ac3b3bccc4b27a50a5f
Britodfbr <britodfbr@gmail.com>
2022-08-01 16:15:00 -0300

    feat: configuração para sqlite

--- incolume/academia_jedi/2022-08-01_crud_sqlite/basedados.py
+++ incolume/academia_jedi/2022-08-01_crud_sqlite/basedados.py
@@ -1,8 +1,33 @@
 import logging
 from model import Pessoa
+from pathlib import Path
 import logging
+import sqlite3
 
 logging.basicConfig(level=logging.WARNING)
+file_sqlite = Path('pessoas.sqlite')
+
+
+con = sqlite3.connect(file_sqlite)
+cur = con.cursor()
+USER_DDL = 'CREATE TABLE IF NOT EXISTS users(user_id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL, date_born DATETIME NOT NULL)'
+EMAIL_DDL = '''CREATE TABLE IF NOT EXISTS emails(
+    email_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER, email TEXT NOT NULL,
+    CONSTRAINT fk_users FOREIGN KEY (user_id)
+    REFERENCES users(user_id))'''
+TEL_DDL = '''CREATE TABLE IF NOT EXISTS telefones(
+    telefone_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    telefone TEXT NOT NULL,
+    CONSTRAINT fk_users FOREIGN KEY (user_id)
+    REFERENCES users(user_id))'''
+ADD_DDL = '''CREATE TABLE IF NOT EXISTS addresses(
+    address_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    address TEXT NOT NULL,
+    CONSTRAINT fk_users FOREIGN KEY (user_id)
+    REFERENCES users(user_id))'''
 
 db = {}
 
@@ -12,8 +37,12 @@ def debug_mode(status: bool = False):
 
 def create_person(pessoa: Pessoa, debug: bool = False):
     debug_mode(debug)
-    db[pessoa.id] = pessoa
-    logging.debug(f'Adicionado: {pessoa.__dict__}')
+    cur.execute(USER_DDL)
+    cur.execute(EMAIL_DDL)
+    cur.execute(TEL_DDL)
+    cur.execute(ADD_DDL)
+    # db[pessoa.id] = pessoa
+    # logging.debug(f'Adicionado: {pessoa.__dict__}')
     return True
 
 
