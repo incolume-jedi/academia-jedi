660b189aa4365da403f68b022aae4c22a5d173d0
Britodfbr <britodfbr@gmail.com>
2022-09-26 09:06:26 -0300

    feat: Carregamento de Pessoas como dict

--- incolume/academia_jedi/ajedi20220926_arquivos_json/handler_json.py
+++ incolume/academia_jedi/ajedi20220926_arquivos_json/handler_json.py
@@ -11,15 +11,26 @@ logFormat = '%(asctime)s; %(levelname)-8s; %(name)s; %(module)s;' \
 logging.basicConfig(level=logging.DEBUG, format=logFormat)
 
 
-def json_0():
+def json_0_write_pessoas():
     logging.debug(f"ran ..")
     pessoas = massa_pessoas(is_json=True)
     with open("pessoas.json", "w") as file:
         json.dump(pessoas, file, indent=4)
 
 
+def json_1_read_pessoas():
+    """Ler arquivo JSON e trazer valores como dict"""
+    logging.debug(f"ran ..")
+    with open("pessoas.json") as file:
+        pessoas = [json.loads(pessoa) for pessoa in json.load(file)]
+
+    for pessoa in pessoas:
+        logging.debug(pessoa)
+
+
 def run():
-    json_0()
+    json_0_write_pessoas()
+    json_1_read_pessoas()
 
 
 if __name__ == '__main__':    # pragma: no cover
