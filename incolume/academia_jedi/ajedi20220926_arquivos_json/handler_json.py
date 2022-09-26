608e805d246cff2b7c6faf6b179939e869b2bf02
Britodfbr <britodfbr@gmail.com>
2022-09-26 09:07:39 -0300

    feat: Carregamento de Pessoas como instancia de Pessoa

--- incolume/academia_jedi/ajedi20220926_arquivos_json/handler_json.py
+++ incolume/academia_jedi/ajedi20220926_arquivos_json/handler_json.py
@@ -5,6 +5,8 @@ import inspect
 import json
 from incolume.academia_jedi.ajedi20220925_massa_dados_faker_protocol \
     .generator_pessoas import massa_pessoas
+from incolume.academia_jedi.ajedi20220925_massa_dados_faker_protocol.models import \
+    Pessoa
 
 logFormat = '%(asctime)s; %(levelname)-8s; %(name)s; %(module)s;' \
             ' %(funcName)s; %(threadName)s; %(thread)d; %(message)s'
@@ -28,9 +30,20 @@ def json_1_read_pessoas():
         logging.debug(pessoa)
 
 
+def json_2_read_pessoas():
+    """Ler arquivo JSON e trazer valores como instancia de Pessoa"""
+    logging.debug(f"ran ..")
+    with open("pessoas.json") as file:
+        pessoas = [Pessoa(**json.loads(pessoa)) for pessoa in json.load(file)]
+
+    for pessoa in pessoas:
+        logging.debug(pessoa)
+
+
 def run():
     json_0_write_pessoas()
     json_1_read_pessoas()
+    json_2_read_pessoas()
 
 
 if __name__ == '__main__':    # pragma: no cover
