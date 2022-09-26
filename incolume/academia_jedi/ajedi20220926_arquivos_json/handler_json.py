598f74c4c89c2dd91e3be84e88fd7b20e3ad763e
Britodfbr <britodfbr@gmail.com>
2022-09-26 08:26:50 -0300

    Escrita de pessoas em JSON
    pana na conversão de datetime para JSON

---
+++ incolume/academia_jedi/ajedi20220926_arquivos_json/handler_json.py
@@ -0,0 +1,27 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+import logging
+import inspect
+import json
+from incolume.academia_jedi.ajedi20220925_massa_dados_faker_protocol \
+    .generator_pessoas import massa_pessoas
+
+logFormat = '%(asctime)s; %(levelname)-8s; %(name)s; %(module)s;' \
+            ' %(funcName)s; %(threadName)s; %(thread)d; %(message)s'
+logging.basicConfig(level=logging.DEBUG, format=logFormat)
+
+
+def json_0():
+    logging.debug(f"ran ..")
+    pessoas = massa_pessoas()
+    with open("pessoas.json", "w") as file:
+        for pessoa in pessoas:
+            json.dump(pessoa.__dict__, file, indent=4)
+
+
+def run():
+    json_0()
+
+
+if __name__ == '__main__':    # pragma: no cover
+    run()
