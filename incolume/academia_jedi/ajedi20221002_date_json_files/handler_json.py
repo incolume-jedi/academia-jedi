23d96bea35f58959220b2166093813e9c12e056b
Britodfbr <britodfbr@gmail.com>
2022-10-03 15:19:02 -0300

    style: Registro de log

--- incolume/academia_jedi/ajedi20221002_date_json_files/handler_json.py
+++ incolume/academia_jedi/ajedi20221002_date_json_files/handler_json.py
@@ -4,8 +4,13 @@ from pathlib import Path
 from datetime import datetime
 from itertools import count
 from copy import copy
+import logging
+from inspect import stack
 
 counter = count()
+logFormat = '%(asctime)s; %(levelname)-8s; %(name)s; %(module)s;' \
+            ' %(funcName)s; %(threadName)s; %(thread)d; %(message)s'
+logging.basicConfig(format=logFormat, level=logging.DEBUG)
 
 
 @dataclass
@@ -16,6 +21,7 @@ class Event:
     index: int = field(default_factory=lambda: next(counter))
 
     def __repr__(self):
+        logging.debug(f'{self.__class__.__name__}{stack()[0][3]}')
         return (
             "{}(message='{message}',create_at='{create_at}', index={index})"
             .format(self.__class__.__name__, **self.__dict__)
@@ -27,20 +33,25 @@ class Event:
     def toJSON(self):
         o = copy(self.__dict__)
         o['create_at'] = self.create_at.strftime('%Y-%m-%d %H:%M:%S.%f')
+        logging.debug(f'{self.__class__.__name__}({o}).{stack()[0][3]}')
         return json.dumps(o, sort_keys=True, indent=4)
 
 
 def get_events():
+    logging.debug(f'{stack()[0][3]}')
     return [Event(f'event_{x:03}').toJSON() for x in range(1, 101)]
 
 
 def write_json(list_events: list, filename: (Path | str)):
+    logging.debug(f'{stack()[0][3]}')
     filename = filename or 'events.json'
     with Path(filename).open('w') as file:
         json.dump(list_events, file, indent=4)
+        logging.debug(f'write file "{filename}"')
 
 
 def run():
+    logging.debug(f'{stack()[0][3]}')
     e = Event('')
     print(
         e,
