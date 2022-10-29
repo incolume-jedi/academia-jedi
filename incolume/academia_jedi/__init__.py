e782aa1facfad98b441d70b71df43c62c2f7de18
Britodfbr <britodfbr@gmail.com>
2022-10-29 10:40:05 -0300

    chore: Unificado o versionamento do projeto

--- incolume/academia_jedi/__init__.py
+++ incolume/academia_jedi/__init__.py
@@ -1,1 +1,9 @@
-__version__ = '0.1.0'
+from tomli import load
+from pathlib import Path
+
+versionfile = Path(__file__).parent / 'version.txt'
+
+with Path(__file__).parents[2].joinpath('pyproject.toml').open('rb') as file:
+    versionfile.write_text(f"{load(file)['tool']['poetry']['version']}\n")
+
+__version__ = versionfile.read_text().strip()
