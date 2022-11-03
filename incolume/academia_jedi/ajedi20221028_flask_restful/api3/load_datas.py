8d60e4336e2077b26bf25743d62c61796d2fa2d5
Britodfbr <britodfbr@gmail.com>
2022-11-03 14:08:31 -0300

    refactor: Sanitização de código

--- incolume/academia_jedi/ajedi20221028_flask_restful/api3/load_datas.py
+++ incolume/academia_jedi/ajedi20221028_flask_restful/api3/load_datas.py
@@ -2,28 +2,13 @@ import requests
 import csv
 import pandas as pd
 
-
 __author__ = "@britodfbr"  # pragma: no cover
-
-# with open("data.csv") as f:
-#     r = csv.reader(f)
-#     data = [row for row in r if row]
-
 
 df = pd.read_csv(
     'https://raw.githubusercontent.com/jhnwr/flask-restful-demo/main/data.csv',
     header=None,
     names=["game_id", "home_team", "away_team", "home_score", "away_score"]
 )
-
-# print(df)
-# print(df.columns)
-# print(df.info())
-# for i, values in df.iterrows():
-#     print(values, end='\n---\n')
-
-# for values in df.values:
-#     print(values)
 
 
 def post_data(item):
@@ -33,13 +18,6 @@ def post_data(item):
                       "AppleWebKit/537.36 (KHTML, like Gecko) "
                       "Chrome/104.0.5112.102 Safari/537.36",
     }
-    # payload = {
-    #     "game_id": item[0],
-    #     "home_team": item[1],
-    #     "away_team": item[2],
-    #     "home_score": item[3],
-    #     "away_score": item[4],
-    # }
     payload = item.to_dict()
     resp = requests.post(
         "http://localhost:5555",
@@ -49,9 +27,5 @@ def post_data(item):
     return resp.json()
 
 
-# for item in data:
-#     print(post_data(item))
-
 for i, values in df.iterrows():
     print(post_data(values))
-
