fa8dafe5da0de0b8f7df77bbd2f552dc4a19b1d6
Britodfbr <britodfbr@gmail.com>
2022-11-06 19:28:10 -0300

    refactor: passagem de parametro chaveado

--- incolume/academia_jedi/ajedi20221106_operador_morsa/main.py
+++ incolume/academia_jedi/ajedi20221106_operador_morsa/main.py
@@ -16,19 +16,21 @@ class Viewer(Enum):
     CLI = auto()
     EASYGUI = auto()
 
-    def __call__(self, *args, **kwargs):
+    def __call__(self, **kwargs):
         logging.debug(self.__dict__)
         logging.debug(self.name)
-
+        msg = kwargs.get('msg')
+        title = kwargs.get('title', 'App')
+        default = kwargs.get('default', '')
         match self.name:
             case 'CLI':
-                return gretting(input('What is your name? '))
+                return gretting(input(f'{msg} '))
             case 'EASYGUI':
                 return gretting(
                     eg.enterbox(
-                        msg='What is your username?',
-                        title='Get username',
-                        default=''
+                        msg=f'{msg} ',
+                        title=title,
+                        default=default
                     )
                 )
             case _:
@@ -42,8 +44,9 @@ def viewer():
 def run():
     # gretting(input('What is your name? '))
     # print(viewer())
-    print(Viewer.CLI(msg=''))
-    print(Viewer.EASYGUI(msg=''))
+    print(Viewer.CLI(msg='What is your name?'))
+    print(Viewer.EASYGUI(msg='What is your name?'))
+    print(Viewer.EASYGUI(msg='What is your name?', title='Get username'))
 
 
 if __name__ == '__main__':  # pragma: no cover
