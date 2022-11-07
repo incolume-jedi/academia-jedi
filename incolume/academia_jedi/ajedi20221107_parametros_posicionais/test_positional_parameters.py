6cde60ae205a47706e038eac286c85a6c5103107
Britodfbr <britodfbr@gmail.com>
2022-11-07 11:48:38 -0300

    wip

--- incolume/academia_jedi/ajedi20221107_parametros_posicionais/test_positional_parameters.py
+++ incolume/academia_jedi/ajedi20221107_parametros_posicionais/test_positional_parameters.py
@@ -12,6 +12,22 @@ from positional_parameters import (
 __author__ = '@britodfbr'  # pragma: no cover
 
 
+@pytest.mark.parametrize(
+    'tparam dparam expected'.split(),
+    (
+        ((10, 20, 30), {'d': 40, 'e': 50, 'f': 60}, None),
+    ),
+)
+def test_func(tparam, dparam, expected):
+    assert func(*tparam, **dparam) == expected
+
+
+@pytest.mark.parametrize(
+    'a b c d'.split(),
+    (
+        (1, 2),
+    ),
+)
 class MyTestCase:
     def test_something(self, a, b):
         assert func(a, b)
