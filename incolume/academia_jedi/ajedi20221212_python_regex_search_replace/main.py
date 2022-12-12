e2525449f28d6f2eb32497e346d670898db81827
britodfbr <britodfbr@gmail.com>
2022-12-12 15:11:25 -0300

    feat: regex em python

---
+++ incolume/academia_jedi/ajedi20221212_python_regex_search_replace/main.py
@@ -0,0 +1,14 @@
+import re  
+
+def convert_case(match_obj):
+    """https://flexiple.com/python/python-regex-replace/"""
+
+    if match_obj.group(1) is not None:
+      return match_obj.group(1).lower()
+
+    if match_obj.group(2) is not None:
+      return match_obj.group(2).upper()
+
+if __name__  == '__main__':
+    str = "jOE kIM mAx ABY lIzA"
+    print(re.sub(r"([A-Z]+) | ([a-z]+)", convert_case, str))
