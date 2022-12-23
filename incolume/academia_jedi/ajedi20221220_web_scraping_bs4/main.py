5d88ca59ad2d27d9cbfa55e952083baec114a5c1
Britodfbr <britodfbr@gmail.com>
2022-12-22 22:07:30 -0300

    feat: raspagem de dados Filmes IMDB

---
+++ incolume/academia_jedi/ajedi20221220_web_scraping_bs4/main.py
@@ -0,0 +1,16 @@
+# import scrap_imdb
+from tempfile import gettempdir
+from pathlib import Path
+from my_scrap_imdb import scraping_ranking
+from my_scrap_imdb1 import scraping_ranking1
+from my_scrap_imdb2 import ScrapingIMDB
+__author__ = "@britodfbr"  # pragma: no cover
+
+if __name__ == '__main__':    # pragma: no cover
+    scraping_ranking(excel_output=Path(gettempdir())/'abc.xlsx')
+    scraping_ranking1(excel_output=Path(gettempdir())/'bcd.xlsx')
+    scrap_imdb = ScrapingIMDB()
+    scrap_imdb.connect().get_soup().get_movies().save_excel(
+        excel_output=Path(gettempdir())/'xpto.xlsx')
+    ScrapingIMDB().scraping(excel_output='a1b2.xlsx')
+
