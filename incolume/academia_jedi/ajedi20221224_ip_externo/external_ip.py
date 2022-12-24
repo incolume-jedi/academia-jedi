adb0aab9ea99e85438a44ca4e2ec22f4e0ae88a3
Britodfbr <britodfbr@gmail.com>
2022-12-24 11:05:04 -0300

    feat: get external ip
    baseado em https://www.youtube.com/watch?v=jF3_g8eDeEo

---
+++ incolume/academia_jedi/ajedi20221224_ip_externo/external_ip.py
@@ -0,0 +1,32 @@
+"""Baseado em https://www.youtube.com/watch?v=jF3_g8eDeEo"""
+
+import re
+from urllib.request import urlopen
+import logging
+
+__author__ = "@britodfbr"  # pragma: no cover
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s;%(levelname)-8s;%(name)s;"
+           "%(module)s;%(funcName)s;%(message)s",
+)
+
+
+def get_ip():
+    req = urlopen("http://checkip.dyndns.com/")   # necessário API
+    logging.debug(type(req))
+    data = str(req.read())
+    logging.debug(data)
+    logging.debug(type(data))
+
+    ip = re.search(r'(\d+(\.\d+){3})', data).group()
+    logging.debug(ip)
+    return ip
+
+
+def run():
+    print(get_ip())
+
+
+if __name__ == '__main__':  # pragma: no cover
+    run()
