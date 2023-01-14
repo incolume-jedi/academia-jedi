2789692c35ac464e6f5aa77e5961b6ba31395752
Britodfbr <britodfbr@gmail.com>
2023-01-14 15:04:21 -0300

    feat: implementação com pacote pwinput

---
+++ incolume/academia_jedi/ajedi20230114_cli_password/main.py
@@ -0,0 +1,22 @@
+import pwinput
+
+
+def get_pwd(msg: str = ''):
+    msg = msg or 'Informe tua senha: '
+    return input(msg)
+
+
+def get_pwd_ofuscated(msg: str = ''):
+    msg = msg or 'Informe tua senha: '
+    return pwinput.pwinput(msg, mask='*')
+
+
+def run():
+    print(get_pwd())
+    print(get_pwd_ofuscated())
+
+
+if __name__ == '__main__':    # pragma: no cover
+    run()
+
+
