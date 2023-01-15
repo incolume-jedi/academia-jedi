eeac3dde1ebd0f37bdc170a7d155947ec4129f01
Britodfbr <britodfbr@gmail.com>
2023-01-15 12:26:28 -0300

    feat: map vs list comprehention

---
+++ incolume/academia_jedi/ajedi20230115_pythonic/truncus.py
@@ -0,0 +1,12 @@
+from typing import List
+
+import logging
+from faker import Faker
+
+Faker.seed(13)
+fake = Faker('pt_BR')
+
+
+def brazilian_name_list(length: int = 1) -> List[str]:
+    logging.debug('Lista de nomes gerados.')
+    return [f'{fake.first_name()} {fake.last_name()}' for _ in range(length)]
