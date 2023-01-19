77bfa146575bb24752f207e9538f4c60411081ba
Britodfbr <britodfbr@gmail.com>
2023-01-19 06:12:08 -0300

    feat: Definindo constante com typing

---
+++ incolume/academia_jedi/ajedi20230118_constaints/main.py
@@ -0,0 +1,12 @@
+from no_typing import CONSTANT1
+from with_typing import CONSTANT2
+
+__author__ = "@britodfbr"  # pragma: no cover
+
+
+def run():
+    pass
+
+
+if __name__ == '__main__':  # pragma: no cover
+    run()
---
+++ incolume/academia_jedi/ajedi20230118_constaints/no_typing.py
@@ -0,0 +1,15 @@
+import logging
+
+__author__ = "@britodfbr"  # pragma: no cover
+
+
+CONSTANT1 = 10
+CONSTANT1 += 10
+
+
+def run():
+    """ """
+
+
+if __name__ == '__main__':  # pragma: no cover
+    run()
