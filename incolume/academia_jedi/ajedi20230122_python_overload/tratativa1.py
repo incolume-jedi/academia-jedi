daa4ef268f07727beab1cb2b54ab2e3613c67863
Britodfbr <britodfbr@gmail.com>
2023-01-22 10:50:51 -0300

    feat: Sobrecarga de funções e metodos em python

---
+++ incolume/academia_jedi/ajedi20230122_python_overload/tratativa1.py
@@ -0,0 +1,36 @@
+"""Exemplo singledispatch."""
+
+from functools import singledispatch
+import logging
+
+__author__ = "@britodfbr"  # pragma: no cover
+
+
+@singledispatch
+def inverse(arg):
+    logging.debug('..')
+    ...
+
+
+@inverse.register
+def _(arg: int):
+    logging.info(f'{arg=}; {type(arg)=}')
+    return -arg
+
+
+@inverse.register
+def _(arg: str):
+    logging.info(f'{arg=}; {type(arg)=}')
+    return arg[::-1]
+
+
+@inverse.register
+def _(arg: float):
+    logging.info(f'{arg=}; {type(arg)=}')
+    return -arg
+
+
+@inverse.register
+def _(arg: bool):
+    logging.info(f'{arg=}; {type(arg)=}')
+    return not arg
