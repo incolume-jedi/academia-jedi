6ada1d1b2c3a3c0883512daa6a6747b83f25ce89
britodfbr <britodfbr@gmail.com>
2023-02-07 13:02:41 -0300

    feat: Exemplos com GUI

---
+++ incolume/academia_jedi/ajedi20230203_GUI_increment_count/example1.py
@@ -0,0 +1,15 @@
+import customtkinter as ctk
+
+
+class App:
+    def __init__(self, master: ctk.CTk):
+        self.master = master
+        self.master.geometry('250x200')
+        self.master.resizable(False, False)
+        self.master.wm_title('Increment Count')
+
+
+if __name__ == '__main__':
+    app = ctk.CTk()
+    gui = App(master=app)
+    app.mainloop()
