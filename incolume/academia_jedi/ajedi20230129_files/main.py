af515920274fda305c3e499c5bbaf5e00bab7d73
Britodfbr <britodfbr@gmail.com>
2023-02-16 17:15:04 -0300

    refactor: Exemplos com dbm acrescentado

--- incolume/academia_jedi/ajedi20230129_files/main.py
+++ incolume/academia_jedi/ajedi20230129_files/main.py
@@ -4,6 +4,7 @@ from files_shelve import run as run1
 from files_csv import run as run2
 from files_json import run as run3
 from files_pickles import run as run4
+from files_dbm import run as run5
 
 
 logging.basicConfig(
@@ -19,6 +20,7 @@ def main():
     run2()
     run3()
     run4()
+    run5()
 
 
 if __name__ == "__main__":
