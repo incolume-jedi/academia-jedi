4ad285c67d5197a690824f965f252a8a5b281c70
britodfbr <britodfbr@gmail.com>
2023-02-22 16:22:00 -0300

    feat: Acrescentado registro de logging

--- incolume/academia_jedi/ajedi20230222_opencv/tratativa1.py
+++ incolume/academia_jedi/ajedi20230222_opencv/tratativa1.py
@@ -6,10 +6,14 @@ sudo apt install tesseract-ocr
 import cv2
 import pytesseract
 from pathlib import Path
+import logging
+
 
 def exemplo1()-> None:
     """"""
     img_file = Path(__file__).parent.joinpath('images', 'img2.png')
+    logging.debug(img_file)
+
     img = cv2.imread(img_file.as_posix())
 
     resultado = pytesseract.image_to_string(img)
@@ -22,6 +26,8 @@ def exemplo2()->None:
     A base para portugues deve ser disponibilizada em /usr/share/tesseract-ocr/4.00/tessdata/por.traineddata
     """
     img_file = Path(__file__).parent.joinpath('images', 'img3.png')
+    logging.debug(img_file)
+
     img = cv2.imread(img_file.as_posix())
 
     resultado = pytesseract.image_to_string(img, lang='por')
