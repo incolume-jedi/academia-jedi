4278cb187db3efb70e229493751e1f96aa2f173e
Britodfbr <britodfbr@gmail.com>
2023-02-26 11:59:29 -0300

    refactor: Unificação de temas por assunto - Operador Morsa
    incolume/academia_jedi/ajedi20220928_morsa_op/main.py -> incolume/academia_jedi/ajedi20230226_operador_morsa/exemplo1.py

---
+++ incolume/academia_jedi/ajedi20230226_operador_morsa/exemplo1.py
@@ -0,0 +1,13 @@
+"""
+Unificado a partir do arquivo:
+   incolume/academia_jedi/ajedi20220928_morsa_op/main.py
+"""
+
+
+__author__ = "@britodfbr"  # pragma: no cover
+
+
+def menu():
+    """Operador morsa."""
+    while (op := input('Escolha um numero (zero (0) para sair): ')) != '0':
+        print(op)
