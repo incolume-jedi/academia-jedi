f2ece84c01d1bea279a0b19ef5f0e053b5622fa2
britodfbr <britodfbr@gmail.com>
2023-02-28 12:31:05 -0300

    feat: Abrir e carregar conteúdo XML em dict

--- incolume/academia_jedi/ajedi20230228_XML_NFe/tratativas.py
+++ incolume/academia_jedi/ajedi20230228_XML_NFe/tratativas.py
@@ -1,4 +1,32 @@
 import typing
+import logging
+import xmltodict
+from pathlib import Path
+from collections import OrderedDict
+
+
+FILES_XML: typing.Final = list(Path(__file__).parent.joinpath('NFs_Finais').rglob("*.xml"))
+logging.debug(FILES_XML)
+
+
+def open_files(file: Path|str, mode: str = 'rb') -> OrderedDict:
+    """Abre arquivos XML e converte para dict."""
+    file = Path(file)
+    with file.open(mode) as f:
+        documento = xmltodict.parse(f)
+    return documento
+
+
+def tratativa0():
+    """Carregar conteúdo do arquivo XML em dict."""
+    logging.debug(FILES_XML)
+    xml_file = next(x for x in FILES_XML if x.name.__contains__('Brota'))
+    logging.debug(xml_file)
+    print(open_files(xml_file))
+
+
+def tratativa1():
+    """Nop."""
 
 
 def run():
