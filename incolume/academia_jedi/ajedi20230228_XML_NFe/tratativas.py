3d7afc7761d35fdb3bb3c7c910def378da167637
britodfbr <britodfbr@gmail.com>
2023-02-28 13:28:49 -0300

    feat: Implementações até 1.15.15h.

--- incolume/academia_jedi/ajedi20230228_XML_NFe/tratativas.py
+++ incolume/academia_jedi/ajedi20230228_XML_NFe/tratativas.py
@@ -3,6 +3,7 @@ import logging
 import xmltodict
 from pathlib import Path
 from collections import OrderedDict
+from dataclasses import dataclass
 
 
 FILES_XML: typing.Final = list(Path(__file__).parent.joinpath('NFs_Finais').rglob("*.xml"))
@@ -17,16 +18,68 @@ def open_files(file: Path|str, mode: str = 'rb') -> OrderedDict:
     return documento
 
 
+@dataclass
+class NFe:
+    """"""
+
+
 def tratativa0():
     """Carregar conteúdo do arquivo XML em dict."""
     logging.debug(FILES_XML)
     xml_file = next(x for x in FILES_XML if x.name.__contains__('Brota'))
     logging.debug(xml_file)
-    print(open_files(xml_file))
+    return open_files(xml_file)
 
 
 def tratativa1():
-    """Nop."""
+    """Capturar campo de NFe."""
+    xml_file = next(x for x in FILES_XML if x.name.__contains__('Brota'))
+    logging.debug(xml_file)
+    documento = open_files(xml_file)
+    return documento['nfeProc']['NFe']['infNFe']['ide']['cUF']
+
+
+def tratativa2():
+    """Capturar campo de Nome."""
+    xml_file = next(x for x in FILES_XML if x.name.__contains__('Brota'))
+    logging.debug(xml_file)
+    documento = open_files(xml_file)
+    return documento['nfeProc']['NFe']['infNFe']['dest']['xNome']
+
+
+def tratativa3():
+    """Retorna Informações da NFe."""
+    xml_file = next(x for x in FILES_XML if x.name.__contains__('Brota'))
+    logging.debug(xml_file)
+    documento = open_files(xml_file)
+    return documento['nfeProc']['NFe']['infNFe']
+
+
+def tratativa4():
+    """Retorna o Valor total da NFe."""
+    return tratativa3()['total']['ICMSTot']['vNF']
+
+
+def tratativa5():
+    """Retorna o Nome e CNPF do Emissor."""
+    return tratativa3()['emit']['xNome'], tratativa3()['emit']['CNPJ']
+
+
+def tratativa6():
+    """Resposta do problema proposto."""
+    resposta = {
+        'valor_total': tratativa3()['total']['ICMSTot']['vNF'],
+        'cnpj_vendeu': tratativa3()['emit']['CNPJ'],
+        'nome_vendeu': tratativa3()['emit']['xNome'],
+        'cpf_comprou': tratativa3()['dest']['CPF'],
+        'nome_comprou': tratativa3()['dest']['xNome'],
+        'lista_produtos': [(prod['prod']['qCom'], prod['prod']['xProd'], prod['prod']['vUnCom'], prod['prod']['vProd']) for prod in tratativa3()['det']],
+    }
+    return resposta
+
+
+def tratativa7():
+    """"""
 
 
 def run():
