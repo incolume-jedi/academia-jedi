24e876982e289f902ed395f172b0313c5ce99499
Britodfbr <britodfbr@gmail.com>
2023-10-24 11:42:33 -0300

    style: #42 fix line length

--- incolume/academia_jedi/ajedi20231023_estrutura_decisao/estrutura_decisao.py
+++ incolume/academia_jedi/ajedi20231023_estrutura_decisao/estrutura_decisao.py
@@ -70,35 +70,52 @@ def exercicio9():
 
 
 def exercicio10():
-    """Faça um Programa que pergunte em que turno você estuda.
+    """ Checar turno.
 
-        Peça para digitar M-matutino ou V-Vespertino ou N- Noturno. Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
-    As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe contrataram para desenvolver o programa que calculará os reajustes.
+    Faça um Programa que pergunte em que turno você estuda.
+
+        Peça para digitar M-matutino ou V-Vespertino ou N- Noturno.
+        Imprima a mensagem "Bom Dia!", "Boa Tarde!" ou "Boa Noite!"
+        ou "Valor Inválido!", conforme o caso.
     """
 
 
 def exercicio11():
-    """Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no salário atual:
+    """ Reajustar salário.
+
+    As Organizações Tabajara resolveram dar um aumento de salário aos seus
+    colaboradores e lhe contrataram para desenvolver o programa que
+    calculará os reajustes.
+
+    Faça um programa que recebe o salário de um colaborador e o reajuste
+    segundo o seguinte critério, baseado no salário atual:
     salários até R$ 280,00 (incluindo) : aumento de 20%
     salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
     salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
     salários de R$ 1500,00 em diante : aumento de 5% Após o aumento ser realizado, informe na tela:
-    o salário antes do reajuste;
-    o percentual de aumento aplicado;
-    o valor do aumento;
-    o novo salário, após o aumento.
+    - o salário antes do reajuste;
+    - o percentual de aumento aplicado;
+    - o valor do aumento;
+    - o novo salário, após o aumento.
     """
 
 
 def exercicio12():
     """Faça um programa para o cálculo de uma folha de pagamento.
 
-    sabendo que os descontos são do Imposto de Renda, que depende do salário bruto (conforme tabela abaixo) e 3% para o Sindicato e que o FGTS corresponde a 11% do Salário Bruto, mas não é descontado (é a empresa que deposita). O Salário Líquido corresponde ao Salário Bruto menos os descontos. O programa deverá pedir ao usuário o valor da sua hora e a quantidade de horas trabalhadas no mês.
+    Sabendo que os descontos são do Imposto de Renda, que depende do
+    salário bruto (conforme tabela abaixo) e 3% para o Sindicato e que o FGTS
+     corresponde a 11% do Salário Bruto, mas não é descontado
+     (é a empresa que deposita). O Salário Líquido corresponde ao
+     Salário Bruto menos os descontos. O programa deverá pedir ao usuário
+      o valor da sua hora e a quantidade de horas trabalhadas no mês.
     Desconto do IR:
     Salário Bruto até 900 (inclusive) - isento
     Salário Bruto até 1500 (inclusive) - desconto de 5%
     Salário Bruto até 2500 (inclusive) - desconto de 10%
-    Salário Bruto acima de 2500 - desconto de 20% Imprima na tela as informações, dispostas conforme o exemplo abaixo. No exemplo o valor da hora é 5 e a quantidade de hora é 220.
+    Salário Bruto acima de 2500 - desconto de 20% Imprima na tela as
+    informações, dispostas conforme o exemplo abaixo. No exemplo o
+    valor da hora é 5 e a quantidade de hora é 220.
             Salário Bruto: (5 * 220)        : R$ 1100,00
             (-) IR (5%)                     : R$   55,00
             (-) INSS ( 10%)                 : R$  110,00
