5c91de7d26ec8218b9216bed6675c3470b591103
Britodfbr <britodfbr@gmail.com>
2023-10-26 09:24:25 -0300

    test: #43 melhoria nos testes unitários para raspagem httpx

--- incolume/academia_jedi/ajedi20231026_raspagem_httpx/test_raspagem.py
+++ incolume/academia_jedi/ajedi20231026_raspagem_httpx/test_raspagem.py
@@ -1,8 +1,39 @@
-from incolume.academia_jedi.ajedi20231026_raspagem_httpx.raspagem import get_content, write_content
+"""Testing it."""
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+
+from incolume.academia_jedi.ajedi20231026_raspagem_httpx.raspagem import (
+    get_content,
+    write_content,
+)
 
 
-def test_get_content():
-    """Test this function."""
-    entrance = 'https://www.diariodasleis.com.br/legislacao/federal/exibe_artigo.php?ifl=203526'
-    expected = ''
-    assert get_content(entrance)
+def test_get_content_ifcontent() -> None:
+    """Testar se tem conteúdo."""
+    entrance = (
+        'https://www.diariodasleis.com.br/legislacao/'
+        'federal/exibe_artigo.php?ifl=203526'
+    )
+    result = get_content(entrance)
+    assert result is not None
+
+
+def test_get_content_type_list() -> None:
+    """Testar se tem conteúdo."""
+    entrance = (
+        'https://www.diariodasleis.com.br/legislacao/'
+        'federal/exibe_artigo.php?ifl=203526'
+    )
+    result = get_content(entrance)
+    assert isinstance(result, list)
+
+
+def test_write_content() -> None:
+    """Testar write_content."""
+    url = (
+        'https://www.diariodasleis.com.br/legislacao/federal/'
+        'exibe_artigo.php?ifl=203526'
+    )
+    with Path(NamedTemporaryFile().name) as file:
+        write_content(file, get_content(url))
+        assert file.is_file()
