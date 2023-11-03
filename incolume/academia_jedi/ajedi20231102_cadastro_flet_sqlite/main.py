eb7f0c7384365614a17bf2a30bc62bbc65bb3b0e
Britodfbr <britodfbr@gmail.com>
2023-11-02 22:05:54 -0300

    feat: #48 função principal para APP

---
+++ incolume/academia_jedi/ajedi20231102_cadastro_flet_sqlite/main.py
@@ -0,0 +1,10 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+import flet
+from app import main
+
+__author__ = "@britodfbr"  # pragma: no cover
+
+
+if __name__ == "__main__":
+    flet.app(target=main)
