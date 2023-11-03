f274aaaa4469f45f322b5040cfcd548819b9b3ee
Britodfbr <britodfbr@gmail.com>
2023-11-02 22:09:24 -0300

    refactor: #48 Nova implementação equivalente ao video demonstrativo

--- incolume/academia_jedi/ajedi20231102_cadastro_flet_sqlite/database.py
+++ incolume/academia_jedi/ajedi20231102_cadastro_flet_sqlite/database.py
@@ -2,21 +2,21 @@
 import logging
 import sqlite3
 from os import getenv
-from dotenv import load_dotenv
 from pathlib import Path
 
 __author__ = "@britodfbr"  # pragma: no cover
 
-load_dotenv()
-
 
 def get_connection(filesqlite: Path = None) -> sqlite3.Connection:
     """Connetion database."""
-    file = (filesqlite or
-            Path(getenv('APP_INCOLUME_DB')) or Path(__file__) / 'db/cad.db')
-    logging.debug(f'{file.as_posix()=}')
-    conn = sqlite3.connect(file, check_same_thread=False)
-    try:
-        yield conn
-    finally:
-        conn.close()
+    if not filesqlite:
+        filesqlite = Path(getenv('APP_INCOLUME_DB')) \
+            if getenv('APP_INCOLUME_DB') else None
+    if not filesqlite:
+        filesqlite = Path(__file__).parent / 'db/cad.db'
+
+    logging.debug(f'{filesqlite.as_posix()=}')
+    conn = sqlite3.connect(filesqlite, check_same_thread=False)
+    return conn
+
+
