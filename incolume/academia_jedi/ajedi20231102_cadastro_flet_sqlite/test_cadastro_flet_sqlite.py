5eb75bb3ada13ed6d5c12c5ba3213a5ae03a0baf
Britodfbr <britodfbr@gmail.com>
2023-11-03 22:14:32 -0300

    tests: #49 Fatoração de testes com fixtures

--- incolume/academia_jedi/ajedi20231102_cadastro_flet_sqlite/test_cadastro_flet_sqlite.py
+++ incolume/academia_jedi/ajedi20231102_cadastro_flet_sqlite/test_cadastro_flet_sqlite.py
@@ -4,33 +4,35 @@ from os import environ, getenv
 from pathlib import Path
 from tempfile import NamedTemporaryFile
 
+import pytest
+
 from incolume.academia_jedi.\
     ajedi20231102_cadastro_flet_sqlite.database import (
     get_connection,
 )
 
 
-def test_get_connection_forma1(caplog) -> None:
+@pytest.fixture()
+def tmpfile() -> Path:
+    """Return a tempfile."""
+    return Path(NamedTemporaryFile().name)
+
+
+def test_get_connection_forma1(caplog, tmpfile) -> None:
     """Test get_connection forma1."""
-    with (
-        Path(NamedTemporaryFile().name) as file,
-        caplog.at_level(logging.DEBUG),
-    ):
-        get_connection(file)
-        assert file.as_posix() in caplog.text
+    with tmpfile, caplog.at_level(logging.DEBUG):
+        get_connection(tmpfile)
+        assert tmpfile.as_posix() in caplog.text
 
 
-def test_get_connection_forma2(caplog) -> None:
+def test_get_connection_forma2(caplog, tmpfile) -> None:
     """Test get_connection forma2."""
-    with (
-        Path(NamedTemporaryFile().name) as file,
-        caplog.at_level(logging.DEBUG),
-    ):
-        environ['APP_INCOLUME_DB'] = file.as_posix()
+    with tmpfile, caplog.at_level(logging.DEBUG):
+        environ['APP_INCOLUME_DB'] = tmpfile.as_posix()
         get_connection()
         assert (
             getenv('APP_INCOLUME_DB') in caplog.text
-            and getenv('APP_INCOLUME_DB') == file.as_posix()
+            and getenv('APP_INCOLUME_DB') == tmpfile.as_posix()
         )
 
 
