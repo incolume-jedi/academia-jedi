7c4ab78709943a0e01730dba6f29f810ccb1e410
Britodfbr <britodfbr@gmail.com>
2023-11-06 14:14:59 -0300

    refactor!: #53 inconsistencia durante a implementação
    BREAKING CHANGE: Fail durante implementação

--- incolume/academia_jedi/ajedi20231106_preventing_del_obj/main.py
+++ incolume/academia_jedi/ajedi20231106_preventing_del_obj/main.py
@@ -1,14 +1,15 @@
 class Foo:
     """Class Foo."""
+    __all_foos = []
+
     def __init__(self, name: str = 'Foo') -> None:
         """Self init."""
+        self.__all_foos.append(self)
         self.name = name
 
 
 class Bar:
     """Class Bar."""
-    __all_foos = []
 
     def __init__(self, name: str = 'Bar'):
         self.name = name
-        self.__all_foos.append(self)
--- incolume/academia_jedi/ajedi20231106_preventing_del_obj/test_main.py
+++ incolume/academia_jedi/ajedi20231106_preventing_del_obj/test_main.py
@@ -1,22 +1,26 @@
 """Test preventing del obj."""
 import pytest
-from incolume.academia_jedi.ajedi20231106_preventing_del_obj.main import Foo, Bar
+from incolume.academia_jedi.ajedi20231106_preventing_del_obj.main import Foo, \
+    Bar
 
 __author__ = "@britodfbr"  # pragma: no cover
 
 
-def test_Bar():
-    """Test bar"""
-    f = Bar()
-    f.name
-    assert f.name == 'Bar'
+class TestCase:
+    """TestCase."""
 
+    __test__ = False
 
-def test_foo():
-    """Test foo."""
-    f = Foo()
-    with pytest.raises(AttributeError, match='*object has no attribute*'):
-        del f.name
-        assert f.name == ''
+    def test_Bar(self):
+        """Test bar"""
+        f = Bar()
+        with pytest.raises(AttributeError, match='object has no attribute'):
+            del f.name
+            assert f.name == 'Bar'
 
-
+    def test_foo(self):
+        """Test foo."""
+        f = Foo()
+        with pytest.raises(AttributeError, match='*object has no attribute*'):
+            del f.name
+            assert f.name == 'Foo'
