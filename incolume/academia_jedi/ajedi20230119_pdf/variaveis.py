1741b93ff6f425159da09eebe3f5f200848afeeb
Britodfbr <britodfbr@gmail.com>
2023-11-07 14:52:16 -0300

    fix: #55 correção no path para data_files/pdf

--- incolume/academia_jedi/ajedi20230119_pdf/variaveis.py
+++ incolume/academia_jedi/ajedi20230119_pdf/variaveis.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 # PDF files
 root = Path.cwd()
-pdfdir = root.joinpath('data_files', 'pdf')
+pdfdir = root.resolve().parents[2].joinpath('data_files', 'pdf')
 assert pdfdir.is_dir(), f'Ops: {pdfdir} ..'
 
 pdffiles = sorted(pdfdir.glob('*.pdf'))
