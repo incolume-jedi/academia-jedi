5bf230db146f7faa1bd6874e356e7fa91611ec90
Britodfbr <britodfbr@gmail.com>
2023-11-07 13:23:36 -0300

    test: #55 implementado testes esquecido da tarefa

---
+++ incolume/academia_jedi/ajedi20221003_datetime_milliseconds/test_handler_datetime.py
@@ -0,0 +1,25 @@
+import re
+
+import pytest
+import incolume.academia_jedi.ajedi20221003_datetime_milliseconds.handler_datetime as pkg
+
+__author__ = '@britodfbr'  # pragma: no cover
+
+
+class TestCase:
+    def test_example01(self, capsys):
+        """Test example01."""
+        pkg.example01()
+        stream, err = capsys.readouterr()
+        assert re.match(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}', stream)
+    def test_example02(self, capsys):
+        """Test example02."""
+        pkg.example02()
+        stream, err = capsys.readouterr()
+        assert re.match(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{3}', stream)
+
+    def test_example03(self, capsys):
+        """Test example03."""
+        pkg.example03()
+        stream, err = capsys.readouterr()
+        assert re.match(r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}', stream)
