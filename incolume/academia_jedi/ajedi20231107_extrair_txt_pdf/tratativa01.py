6988e8b0aa1c7768690a6e312ee2a230c45f2a59
Britodfbr <britodfbr@gmail.com>
2023-11-08 10:09:31 -0300

    style: #55 sanitização PEP8

--- incolume/academia_jedi/ajedi20231107_extrair_txt_pdf/tratativa01.py
+++ incolume/academia_jedi/ajedi20231107_extrair_txt_pdf/tratativa01.py
@@ -1,24 +1,22 @@
 """Extrair texto com opencv e tesseract."""
 
 import logging
-from typing import Any
+from os import environ
+from pathlib import Path
 
 import cv2
 import pytesseract
-from pathlib import Path
-from os import getenv, environ
 
-
-__author__ = "@britodfbr"  # pragma: no cover
+__author__ = '@britodfbr'  # pragma: no cover
 
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s;%(levelname)-8s;%(name)s;'
-           '%(module)s;%(funcName)s;%(message)s',
+    '%(module)s;%(funcName)s;%(message)s',
 )
 
 
-def exemplo01(img_file: Path = None) -> Any:
+def exemplo01(img_file: Path | None = None) -> str:
     """Extrair texto ascii da imagem."""
     logging.debug(img_file)
     img = cv2.imread(img_file.as_posix())
@@ -28,7 +26,7 @@ def exemplo01(img_file: Path = None) -> Any:
     return resultado
 
 
-def exemplo2(img_file: Path = None) -> Any:
+def exemplo02(img_file: Path | None = None) -> str:
     """Extrair texto utf8 da imagem.
 
     A base para portugues deve ser disponibilizada em
@@ -40,19 +38,18 @@ def exemplo2(img_file: Path = None) -> Any:
     logging.debug(img_file)
     img = cv2.imread(img_file.as_posix())
 
-    resultado = pytesseract.image_to_string(img, lang='por').strip()
-    return resultado
+    return pytesseract.image_to_string(img, lang='por').strip()
 
 
-def exemplo3(img_file: Path = None) -> Any:
+def exemplo03(img_file: Path | None = None) -> str:
     """Extrair texto utf8 da imagem.
 
     A base para portugues deve ser disponibilizada em
-    /usr/share/tesseract-ocr/4.00/tessdata/por.traineddata."""
-
+    /usr/share/tesseract-ocr/4.00/tessdata/por.traineddata.
+    """
     logging.debug(img_file)
     img = cv2.imread(img_file.as_posix())
 
     resultado = pytesseract.image_to_string(img, lang='por')
-    print(resultado.strip())
+    logging.debug(resultado.strip())
     return resultado
