cb88d405581711a4825844d021fe7ff7e84bbee0
Britodfbr <britodfbr@gmail.com>
2024-03-21 09:59:25 -0300

    refactor: adaptação baseada em https://youtu.be/DGl9yBpRl2Q?si=LFCbYpf1fdDqnTTu

--- incolume/academia_jedi/ajedi20240321_ft_rotas/main.py
+++ incolume/academia_jedi/ajedi20240321_ft_rotas/main.py
@@ -11,10 +11,17 @@ def main(page: ft.Page) -> NoReturn:
     page.add(ft.Text(f"Initial route: {page.route}"))
 
     def route_change(e: ft.RouteChangeEvent):
+        """Route change."""
         page.add(ft.Text(f"New route: {e.route}"))
+
+    def route_compras(e: ft.ControlEvent):
+        """Route compras."""
+        page.route = '/compras'
+        page.update()
 
     page.on_route_change = route_change
     page.update()
+    page.add(ft.ElevatedButton('/compras', on_click=route_compras))
 
 
 if __name__ == '__main__':  # pragma: no cover
