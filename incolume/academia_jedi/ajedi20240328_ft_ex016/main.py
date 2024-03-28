0b9bf4ea2e96c8847cfe2d02a44702fef8286ee2
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2024-03-28 16:04:09 -0300

    feat: exemplo de datagrid em flet
    extraído de https://t.me/programadoraventureiro/5263

---
+++ incolume/academia_jedi/ajedi20240328_ft_ex016/main.py
@@ -0,0 +1,38 @@
+
+import flet as ft
+
+def main(page: ft.Page):
+    page.add(
+        ft.DataTable(
+            columns=[
+                ft.DataColumn(
+                    label=ft.Container(
+                        width=200,
+                        content=ft.Text("Coluna com tamanho de 200px")
+                    ),
+                ),
+                ft.DataColumn(
+                    label=ft.Container(
+                        width=200,
+                        content=ft.Text("Coluna com tamanho de 200px")
+                    ),
+                ),
+                ft.DataColumn(
+                    label=ft.Container(
+                        width=200,
+                        content=ft.Text("Coluna com tamanho de 200px")
+                    ),
+                ),
+            ],
+            rows=[
+                ft.DataRow(
+                    cells=[
+                        ft.DataCell(content=ft.Text(value='Conteúdo...'))
+                        for _ in range(3)
+                    ]
+                ) for _ in range(10)
+            ],
+        ),
+    )
+
+ft.app(target=main)
