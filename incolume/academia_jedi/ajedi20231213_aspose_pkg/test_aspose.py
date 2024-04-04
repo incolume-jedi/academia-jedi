594bbc50a44a3d144fd4b4f91c81190d105b8ea0
Britodfbr <britodfbr@gmail.com>
2024-04-04 08:07:11 -0300

    style: Formatação lint aplicada (QA, PEP8, e outros)

--- incolume/academia_jedi/ajedi20231213_aspose_pkg/test_aspose.py
+++ incolume/academia_jedi/ajedi20231213_aspose_pkg/test_aspose.py
@@ -1,5 +1,9 @@
+"""Test module."""
+
 import pytest
-from incolume.academia_jedi.ajedi20231213_aspose_pkg import prospect_aspose as pkg
+from incolume.academia_jedi.ajedi20231213_aspose_pkg import (
+    prospect_aspose as pkg,
+)
 import re
 
 
@@ -8,11 +12,16 @@ import re
     [
         pytest.param({}, '.*tmp.*', marks=()),
         pytest.param({'prefix': 'file_'}, '.*file_.*', marks=()),
-        pytest.param({'suffix': '.docx'},'.*.docx$',),
-        pytest.param({'prefix': 'test-', 'suffix': '.pdf'}, '.*test-.+pdf$', ),
-    ]
+        pytest.param(
+            {'suffix': '.docx'},
+            '.*.docx$',
+        ),
+        pytest.param(
+            {'prefix': 'test-', 'suffix': '.pdf'},
+            '.*test-.+pdf$',
+        ),
+    ],
 )
 def test_new_file(entrance: dict, regex: str) -> None:
     """Test it."""
-
     assert re.match(regex, pkg.new_filename(**entrance).as_posix(), flags=re.I)
