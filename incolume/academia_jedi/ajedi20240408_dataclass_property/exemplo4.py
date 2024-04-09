28c5586d8cba2d2ef0c6fa9c669a41ad45ad5e81
Britodfbr <britodfbr@gmail.com>
2024-04-09 08:25:36 -0300

    feat: Exemplos de dataclass + property

--- incolume/academia_jedi/ajedi20240408_dataclass_property/exemplo4.py
+++ incolume/academia_jedi/ajedi20240408_dataclass_property/exemplo4.py
@@ -1,18 +1,18 @@
 """Module."""
 
 __author__ = '@britodfbr'  # pragma: no cover
-from dataclasses import dataclass
-from typing import Union
 
-from dataclass_wizard import property_wizard
+from dataclasses import dataclass, field
+from typing import Union
 
 
 @dataclass
-class Vehicle(metaclass=property_wizard):
-    wheels: Union[int, str] = None
+class Vehicle:
+    wheels: Union[int, str] = field(default=0)
+
     # Uncomment the field below if you want to make your IDE a bit happier.
     # Remember to set an initial value `x` as needed, via `default=x`.
-    #   _wheels: int = field(init=False)
+    _wheels: int = field(init=False, repr=False)
 
     @property
     def wheels(self) -> int:
@@ -20,7 +20,13 @@ class Vehicle(metaclass=property_wizard):
 
     @wheels.setter
     def wheels(self, wheels: Union[int, str]):
-        self._wheels = int(wheels)
+        # self._wheels = int(wheels)
+        self._wheels = (
+            0 if isinstance(wheels, property) else max(int(wheels), 1)
+        )
+
+    def __post_init__(self):
+        """Post init."""
 
 
 if __name__ == '__main__':
