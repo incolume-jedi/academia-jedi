3f8652c3379086196da513a028ec1efa40b3a0db
Britodfbr <britodfbr@gmail.com>
2024-04-14 22:14:16 -0300

    perf: melhoria para sugestão em nomes de arquivos

--- incolume/academia_jedi/ajedi20240414_report_python/files.py
+++ incolume/academia_jedi/ajedi20240414_report_python/files.py
@@ -29,21 +29,22 @@ def realfilename(
     """
     filebase = Path(filebase)
     suffixes = filebase.suffixes or ['.txt']
-    ext = ext or ''.join(suffixes)
+    ext = f'.{ext.strip(".")}' if ext else ''.join(suffixes)
     sep = '_' if separador else ''
     basename = filebase.stem.split('.')[0]
-    filename = filebase.with_name(f'{basename}').with_suffix(f'{ext}')
-    result = filename
     while True:
+        dig = f'{count}'.zfill(digits)
+
+        result = filebase.with_name(
+            f'{basename}{sep}{dig}',
+        ).with_suffix(
+            f'{ext}',
+        )
         result.parent.mkdir(parents=True, exist_ok=True, mode=0o755)
         if not result.is_file():
             ic(f'Suggested name: {result}')
             logging.info('Suggested name: %s', result)
             return result
-        dig = f'{count}'.zfill(digits)
-        result = filename.with_name(f'{basename}{sep}{dig}').with_suffix(
-            f'{ext}',
-        )
         count += 1
 
 
@@ -51,6 +52,7 @@ if __name__ == '__main__':  # pragma: no cover
     file = Path(gettempdir()) / 'fisco.tar.gz'
     realfilename('pulo').write_text('')
     realfilename('tuto').write_text('')
-    realfilename('tox').write_text('')
+    realfilename('tox', digits=2).write_text('')
+    realfilename('tox', ext='cpp').write_text('')
     realfilename('file.asm', separador=False, digits=2).write_text('')
     realfilename(file, separador=False, count=0, digits=2).write_text('')
