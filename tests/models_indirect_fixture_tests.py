c6859f1daa398b8cf9bb248d95e6d98e6528e704
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2024-04-24 10:40:15 -0300

    test: exemplo modelo para fixture indiretos

---
+++ tests/models_indirect_fixture_tests.py
@@ -0,0 +1,17 @@
+import pytest
+
+
+@pytest.fixture
+def fixture_name(request):
+    return request.param
+
+@pytest.mark.parametrize(
+    'fixture_name', 
+    [
+        'foo', 
+        'bar',
+    ], 
+    indirect=True
+)
+def test_indirect(fixture_name):
+    assert fixture_name == 'baz'
