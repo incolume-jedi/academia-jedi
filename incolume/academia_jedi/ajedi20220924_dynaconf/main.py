bc678b536f2e92736494693fe13b96c177c93a75
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2024-04-24 11:45:33 -0300

    refactor: renomeados arquivos do projeto
    com a finalidade de customizar operacionalidade de ferramentas de testes automáticos

--- incolume/academia_jedi/ajedi20220924_dynaconf/main.py
+++ incolume/academia_jedi/ajedi20220924_dynaconf/main.py
@@ -23,26 +23,27 @@ def activate_envvar(activate: bool = False) -> bool:
     return activate
 
 
-activate_envvar(True)
-run()
-
-#  export INCOLUME_APPLICATION=MyApp; python main.py; unset INCOLUME_APPLICATION
-"""
-$ export INCOLUME_NUM=42
-$ export INCOLUME_FLOAT=4.2
-$ export INCOLUME_DICT='{foo="bar"}'
-$ export INCOLUME_BOOL=false
-$ export INCOLUME_NAME=MyApp
-$ dynaconf --instance config.settings list
-    Working in main environment
-    MSG<str> 'Hello World'
-    NUM<int> 42
-    NAME<str> 'MyApp'
-    DICT<dict> {'foo': 'bar'}
-    FLOAT<float> 4.2
-    BOOL<bool> False
-export INCOLUME_MODE='production'; dynaconf --instance config.settings list
-export INCOLUME_MODE='testing'; dynaconf --instance config.settings list
-export INCOLUME_MODE='development'; dynaconf --instance config.settings list
-
-"""
+if __name__ == '__main__':
+    activate_envvar(True)
+    run()
+    
+    #  export INCOLUME_APPLICATION=MyApp; python main.py; unset INCOLUME_APPLICATION
+    """
+    $ export INCOLUME_NUM=42
+    $ export INCOLUME_FLOAT=4.2
+    $ export INCOLUME_DICT='{foo="bar"}'
+    $ export INCOLUME_BOOL=false
+    $ export INCOLUME_NAME=MyApp
+    $ dynaconf --instance config.settings list
+        Working in main environment
+        MSG<str> 'Hello World'
+        NUM<int> 42
+        NAME<str> 'MyApp'
+        DICT<dict> {'foo': 'bar'}
+        FLOAT<float> 4.2
+        BOOL<bool> False
+    export INCOLUME_MODE='production'; dynaconf --instance config.settings list
+    export INCOLUME_MODE='testing'; dynaconf --instance config.settings list
+    export INCOLUME_MODE='development'; dynaconf --instance config.settings list
+    
+    """
