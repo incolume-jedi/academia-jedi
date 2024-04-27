8b2ef741a2fa4bd6c29e1378e73b7c81db66dc66
Britodfbr <britodfbr@gmail.com>
2024-04-27 18:31:15 -0300

    chore!: Tratativa em retrocompatibilidade com Python 3.8

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,38 @@
 """Configurate of tests."""
+from sys import version_info
+
 import pytest
 from tempfile import NamedTemporaryFile
 from pathlib import Path
+
+
+collect_ignore = []
+
+
+if version_info < (3, 9, 0):  # noqa: UP036
+    collect_ignore.extend(
+        (
+            'incolume/academia_jedi/ajedi20220728_crud_nodb/*.py',
+        ),
+    )
+elif version_info < (3, 10, 0):
+    collect_ignore.extend(
+        (
+            'incolume/academia_jedi/ajedi20220728_crud_nodb',
+        ),
+    )
+elif version_info < (3, 11, 0):
+    collect_ignore.extend(
+        (
+            'incolume/academia_jedi/ajedi20220728_crud_nodb',
+        ),
+    )
+elif version_info < (3, 12, 0):
+    collect_ignore.extend(
+        (
+            'incolume/academia_jedi/ajedi20220728_crud_nodb',
+        ),
+    )
 
 
 @pytest.fixture()
