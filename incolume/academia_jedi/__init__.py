8dcb077e5f405a8e3951e077d26d6a4c1d698a72
Ricardo Brito do Nascimento <britodfbr@gmail.com>
2025-02-02 15:29:26 -0300

    fix: Ignorado explicitamente as notificações para QA anteriores ao ano de 2025

--- incolume/academia_jedi/__init__.py
+++ incolume/academia_jedi/__init__.py
@@ -8,7 +8,7 @@ except (ModuleNotFoundError, ImportError):
 from pathlib import Path
 
 version_file = Path(__file__).parent / 'version.txt'
-project_file = Path(__file__).parents[2] /'pyproject.toml'
+project_file = Path(__file__).parents[2] / 'pyproject.toml'
 try:
     with project_file.open('rb') as file:
         version_file.write_text(f"{load(file)['tool']['poetry']['version']}\n")
