1130eb060c22dbeb07f5036f56849e5046f5e460
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-02-07 10:56:49 -0300

    fix: FAILED incolume/academia_jedi/ajedi20231026_raspagem_httpx/test_raspagem.py::test_get_content_ifcontent - AttributeError: \'NoneType\' object has no attribute \'contents\'
    close #78

--- incolume/academia_jedi/ajedi20231026_raspagem_httpx/test_raspagem.py
+++ incolume/academia_jedi/ajedi20231026_raspagem_httpx/test_raspagem.py
@@ -16,30 +16,21 @@ from incolume.academia_jedi.ajedi20231026_raspagem_httpx.raspagem import (
 class TestRapagem:
     """Testcase."""
 
+    url = 'https://www.diariodasleis.com.br/legislacao/federal/exibe_artigo.php?ifl=203526'
+
     def test_get_content_ifcontent(self) -> None:
         """Testar se tem conteúdo."""
-        entrance = (
-            'https://www.diariodasleis.com.br/legislacao/'
-            'federal/exibe_artigo.php?ifl=203526'
-        )
-        result = get_content(entrance)
+        result = get_content(self.url)
         assert result is not None
 
     def test_get_content_type_list(self) -> None:
         """Testar se tem conteúdo."""
-        entrance = (
-            'https://www.diariodasleis.com.br/legislacao/'
-            'federal/exibe_artigo.php?ifl=203526'
-        )
-        result = get_content(entrance)
+        result = get_content(self.url)
         assert isinstance(result, list)
+        assert all(isinstance(x, str) for x in result)
 
     def test_write_content(self) -> None:
         """Testar write_content."""
-        url = (
-            'https://www.diariodasleis.com.br/legislacao/federal/'
-            'exibe_artigo.php?ifl=203526'
-        )
-        with Path(NamedTemporaryFile(prefix=stack()[0][3]).name) as file:
-            write_content(file, get_content(url))
-            assert file.is_file()
+        filename: Path = Path(NamedTemporaryFile(prefix=stack()[0][3]).name)
+        write_content(filename, get_content(self.url))
+        assert filename.is_file()
