c307bb645e1e4f742e265aae35507f57c8a87455
Ricardo Brito do Nascimento <britodfbr@gmail.com>
2025-02-09 09:31:09 -0300

    fix: SintaxeError para match/case
    tratativa em corrigir

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,14 +17,14 @@ if version_info < (3, 9, 0):  # noqa: UP036
     )
 elif version_info < (3, 10, 0):  # noqa: UP036
     collect_ignore.extend(
-        ('incolume/academia_jedi/ajedi20240408_dundler_methods/*',),
+        (
+            'incolume/academia_jedi/ajedi20240408_dundler_methods/*',
+            'incolume/academia_jedi/ajedi20240408_dundler_methods/*',
+        ),
     )
 elif version_info < (3, 11, 0) or version_info < (3, 12, 0):
     collect_ignore.extend(
-        (
-            'incolume/academia_jedi/ajedi20220728_crud_nodb',
-            'incolume/academia_jedi/ajedi20240408_dundler_methods/*',
-        ),
+        ('incolume/academia_jedi/ajedi20220728_crud_nodb',),
     )
 elif version_info < (4, 0, 0):
     collect_ignore.extend(
