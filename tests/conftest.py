2312db1622e3ea58056b29fe573880fd854f1621
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-02-12 10:05:33 -0300

    refactor: Fatoração para diversas melhorias

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,24 +12,24 @@ collect_ignore = []
 
 
 if version_info < (3, 9, 0):  # noqa: UP036
-    collect_ignore.extend(
-        ('incolume/academia_jedi/ajedi20220728_crud_nodb/*.py',),
-    )
+    collect_ignore.extend([
+        r'incolume/academia_jedi/ajedi20220728_crud_nodb/*',
+    ])
 elif version_info < (3, 10, 0):  # noqa: UP036
-    collect_ignore.extend(
-        (
-            'incolume/academia_jedi/ajedi20240408_dundler_methods/*',
-            'incolume/academia_jedi/ajedi20240408_dundler_methods/*',
-        ),
-    )
+    collect_ignore.extend([
+        r'incolume/academia_jedi/ajedi20220728_crud_nodb/*',
+        r'incolume/academia_jedi/ajedi20240408_dundler_methods/*',
+    ])
 elif version_info < (3, 11, 0) or version_info < (3, 12, 0):
-    collect_ignore.extend(
-        ('incolume/academia_jedi/ajedi20220728_crud_nodb',),
-    )
+    collect_ignore.extend([
+        r'incolume/academia_jedi/ajedi20220728_crud_nodb/*'
+        r'incolume/academia_jedi/ajedi20240408_dundler_methods/*',
+    ])
 elif version_info < (4, 0, 0):
-    collect_ignore.extend(
-        ('incolume\academia_jedi\ajedi20231213_aspose_pkg\test_aspose.py',),
+    collect_ignore.append(
+        r'incolume/academia_jedi/ajedi20231213_aspose_pkg/test_aspose.py',
     )
+
 logging.debug(ic(collect_ignore))
 
 
