280b03ed355372b6ba07b27fa4f9dce57caca3be
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-02-17 11:14:42 -0300

    test: Fatorado configuração para testes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 """Configurate of tests."""
 
+from __future__ import annotations
+
 import logging
 from pathlib import Path
 from sys import version_info
@@ -8,21 +10,30 @@ from tempfile import NamedTemporaryFile
 import pytest
 from icecream import ic
 
-collect_ignore = []
+collect_ignore = ['setup.py']
 
 
 if version_info < (3, 9, 0):  # noqa: UP036
-    collect_ignore.extend([
-        r'incolume/academia_jedi/ajedi20220728_crud_nodb/*',
-    ])
-if version_info < (3, 10, 0):  # noqa: UP036
-    collect_ignore.extend([
-        r'incolume/academia_jedi/ajedi20240408_dundler_methods/*',
-    ])
+    files = Path('incolume/academia_jedi/ajedi20220728_crud_nodb/').rglob(
+        '*.py',
+    )
+    logging.debug(ic([collect_ignore.append(file) for file in files]))
+
+if version_info < (3, 10, 0):
+    files = Path(
+        'incolume/academia_jedi/ajedi20240408_dundler_methods/',
+    ).rglob('*.py')
+    logging.debug(ic([collect_ignore.append(file) for file in files]))
+
 if version_info < (3, 11, 0):
-    pass
+    path = Path('incolume/academia_jedi/ajedi20221104_collections_deque')
+    files = path.rglob('*.py')
+    logging.debug(ic([collect_ignore.append(file) for file in files]))
 
 if version_info < (3, 12, 0):
+    pass
+
+if version_info < (3, 13, 0):
     pass
 
 if version_info < (4, 0, 0):
