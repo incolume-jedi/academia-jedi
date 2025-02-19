ad068b88a8dd10748a0a749acd9b8cf2e9e5c27a
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-02-19 07:39:02 -0300

    test: Ajuste na configuração pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,7 @@ from icecream import ic
 
 # ruff: noqa: UP036
 
-collect_ignore = ['setup.py']
+collect_ignore = ['main.py', 'setup.py']
 
 
 if version_info < (3, 9, 0):
@@ -28,13 +28,25 @@ if version_info < (3, 10, 0):
     logging.debug(ic([collect_ignore.append(file) for file in files]))
 
 if version_info < (3, 11, 0):
-    path = Path('incolume/academia_jedi/ajedi20221104_collections_deque')
-    files = path.rglob('*.py')
-    logging.debug(ic([collect_ignore.append(file) for file in files]))
+    logging.debug(
+        ic([
+            collect_ignore.append(file)
+            for file in Path(
+                'incolume/academia_jedi/ajedi20221104_collections_deque',
+            ).rglob('*.py')
+        ]),
+    )
 
 if version_info < (3, 12, 0):
     collect_ignore.append(
         r'incolume/academia_jedi/ajedi20250224_estudo_itertools_batched/test_20250224.py',
+    logging.debug(
+        ic([
+            collect_ignore.append(file)
+            for file in Path(
+                'incolume/academia_jedi/ajedi20231101_handler_docx',
+            ).rglob('*.py')
+        ]),
     )
 
 if version_info < (3, 13, 0):
