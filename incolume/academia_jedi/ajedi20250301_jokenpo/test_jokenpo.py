16fc2e3cf0f46230d069b966f7f35a480956f242
Britodfbr <britodfbr@gmail.com>
2025-03-03 15:29:10 -0300

    feat: solução totalmente implementada e funcional
    close #5

--- incolume/academia_jedi/ajedi20250301_jokenpo/test_jokenpo.py
+++ incolume/academia_jedi/ajedi20250301_jokenpo/test_jokenpo.py
@@ -1,94 +1,288 @@
 """Test Jokenpo."""
-from copy import copy
 
 from faker import Faker
 import pytest
-from incolume.academia_jedi.ajedi20231201_jokenpo import main
+from incolume.academia_jedi.ajedi20250301_jokenpo import main
 from secrets import choice
 
-__author__ = "@britodfbr"  # pragma: no cover
+
+__author__ = '@britodfbr'  # pragma: no cover
+
+Faker.seed(13)
 fake = Faker('pt-BR')
 
 
-@pytest.mark.parametrize(
-    'entrance expected'.split(),
-    [
-        (1, 'PAPEL'),
-        (2, 'TESOURA'),
-        (3, 'PEDRA'),
-    ]
-)
-def test_enum(entrance, expected) -> None:
-    """Test enum."""
-    assert main.Jokenpo(entrance) == getattr(main.Jokenpo, expected)
+def jogador(nome: str = '', lance: main.Jokenpo | None = None) -> main.Jogador:
+    """Definir jogador.
 
+    Args:
+        nome (str, optional): _description_. Defaults to ''.
+        lance (main.Jokenpo | None, optional): _description_. Defaults to None.
 
-def jogador(nome: str = '', lance: main.Jokenpo | None = None):
+    Returns:
+        None: Nonetype
+    """
     nome = nome or fake.name()
     lance = lance or main.Jokenpo(choice(range(1, 4)))
     return main.Jogador(nome=nome, lance=lance)
 
 
-def test_jogador_assinatura() -> None:
-    """Test enum."""
-    assert jogador().__annotations__ == {
-        'derrotas': int, 'empates': int,
-        'vitorias': int,
-        'lance': main.Jokenpo,
-        'nome': str,
-    }
+class TestJokenpo:
+    """Test Case."""
 
+    j1 = jogador(lance=main.Jokenpo('papel'))
+    j2 = jogador(lance=main.Jokenpo('papel'))
 
-@pytest.mark.parametrize(
-    'entrance1 entrance2 expected'.split(),
-    [
-        (
-            jogador(lance=main.Jokenpo(1)),
-            jogador(lance=main.Jokenpo(1)),
-            False
-        ),
-        (
-            jogador(lance=main.Jokenpo(1)),
-            jogador(lance=main.Jokenpo(2)),
-            False
-        ),
-        (
-            jogador(lance=main.Jokenpo(1)),
-            jogador(lance=main.Jokenpo(3)),
-            False
-        ),
-        (
-            jogador(lance=main.Jokenpo(2)),
-            jogador(lance=main.Jokenpo(1)),
-            False
-        ),
-        (
-            jogador(lance=main.Jokenpo(2)),
-            jogador(lance=main.Jokenpo(2)),
-            False
-        ),
-        (
-            jogador(lance=main.Jokenpo(2)),
-            jogador(lance=main.Jokenpo(3)),
-            False
-        ),
-        (
-            jogador(lance=main.Jokenpo(3)),
-            jogador(lance=main.Jokenpo(1)),
-            False
-        ),
-        (
-            jogador(lance=main.Jokenpo(3)),
-            jogador(lance=main.Jokenpo(2)),
-            False
-        ),
-        (
-            jogador(lance=main.Jokenpo(3)),
-            jogador(lance=main.Jokenpo(3)),
-            False
-        ),
-    ]
-)
-def test_jogador_op_relacionais(entrance1, entrance2, expected) -> None:
-    """Test enum."""
-    assert (entrance1 > entrance2) == expected
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            (1, 'PAPEL'),
+            (2, 'TESOURA'),
+            (3, 'PEDRA'),
+            ('papel', 'PAPEL'),
+            ('tesoura', 'TESOURA'),
+            ('pedra', 'PEDRA'),
+        ],
+    )
+    def test_enum(self, entrance, expected) -> None:
+        """Test enum."""
+        assert main.Jokenpo(entrance) == getattr(main.Jokenpo, expected)
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            ('PAPEL', 1),
+            ('TESOURA', 2),
+            ('PEDRA', 3),
+            (1, 1),
+            (2, 2),
+            (3, 3),
+        ],
+    )
+    def test_enum_rev(self, entrance, expected) -> None:
+        """Test enum."""
+        assert main.Jokenpo(entrance) == main.Jokenpo(expected)
+
+    @pytest.mark.parametrize(
+        'entrance',
+        [
+            main.Jogador,
+            main.Jogador('John Doe', main.Jokenpo('pedra')),
+            jogador(),
+        ],
+    )
+    def test_jogador_assinatura(self, entrance) -> None:
+        """Test enum."""
+        parametros = {
+            'derrotas': int,
+            'empates': int,
+            'vitorias': int,
+            'lance': main.Jokenpo,
+            'nome': str,
+        }
+
+        assert parametros == entrance.__annotations__
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            pytest.param(('papel', 1), True),
+            pytest.param(('papel', 'papel'), True),
+            pytest.param(('papel', 'PAPEL'), True),
+            pytest.param(('papel', 2), False),
+            pytest.param(('papel', 'tesoura'), False),
+            pytest.param(('papel', 'TESOURA'), False),
+            pytest.param(('papel', 3), False),
+            pytest.param(('papel', 'pedra'), False),
+            pytest.param(('papel', 'PEDRA'), False),
+            pytest.param(('tesoura', 3), False),
+            pytest.param(('tesoura', 'pedra'), False),
+            pytest.param(('tesoura', 'PEDRA'), False),
+        ],
+    )
+    def test_jokenpo_equals(self, entrance, expected):
+        """Unittest."""
+        value1, value2 = entrance
+        result = main.Jokenpo(value1) == main.Jokenpo(value2)
+        assert result == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            pytest.param(('papel', 1), False),
+            pytest.param(('papel', 'papel'), False),
+            pytest.param(('papel', 'PAPEL'), False),
+            pytest.param(('papel', 2), True),
+            pytest.param(('papel', 'tesoura'), True),
+            pytest.param(('papel', 'TESOURA'), True),
+            pytest.param(('papel', 3), False),
+            pytest.param(('papel', 'pedra'), False),
+            pytest.param(('papel', 'PEDRA'), False),
+            pytest.param(('tesoura', 1), False),
+            pytest.param(('tesoura', 'papel'), False),
+            pytest.param(('tesoura', 'PAPEL'), False),
+            pytest.param(('tesoura', 2), False),
+            pytest.param(('tesoura', 'tesoura'), False),
+            pytest.param(('tesoura', 'TESOURA'), False),
+            pytest.param(('tesoura', 3), True),
+            pytest.param(('tesoura', 'pedra'), True),
+            pytest.param(('tesoura', 'PEDRA'), True),
+            pytest.param(('pedra', 1), True),
+            pytest.param(('pedra', 'papel'), True),
+            pytest.param(('pedra', 'PAPEL'), True),
+            pytest.param(('pedra', 2), False),
+            pytest.param(('pedra', 'tesoura'), False),
+            pytest.param(('pedra', 'TESOURA'), False),
+            pytest.param(('pedra', 3), False),
+            pytest.param(('pedra', 'pedra'), False),
+            pytest.param(('pedra', 'PEDRA'), False),
+        ],
+    )
+    def test_jokenpo_less_than(self, entrance, expected):
+        """Unittest."""
+        value1, value2 = entrance
+        result = main.Jokenpo(value1) < main.Jokenpo(value2)
+        assert result == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            pytest.param(('papel', 1), False),
+            pytest.param(('papel', 'papel'), False),
+            pytest.param(('papel', 'PAPEL'), False),
+            pytest.param(('papel', 2), False),
+            pytest.param(('papel', 'tesoura'), False),
+            pytest.param(('papel', 'TESOURA'), False),
+            pytest.param(('papel', 3), True),
+            pytest.param(('papel', 'pedra'), True),
+            pytest.param(('papel', 'PEDRA'), True),
+            pytest.param(('tesoura', 1), True),
+            pytest.param(('tesoura', 'papel'), True),
+            pytest.param(('tesoura', 'PAPEL'), True),
+            pytest.param(('tesoura', 2), False),
+            pytest.param(('tesoura', 'tesoura'), False),
+            pytest.param(('tesoura', 'TESOURA'), False),
+            pytest.param(('tesoura', 3), False),
+            pytest.param(('tesoura', 'pedra'), False),
+            pytest.param(('tesoura', 'PEDRA'), False),
+            pytest.param(('pedra', 1), False),
+            pytest.param(('pedra', 'papel'), False),
+            pytest.param(('pedra', 'PAPEL'), False),
+            pytest.param(('pedra', 2), True),
+            pytest.param(('pedra', 'tesoura'), True),
+            pytest.param(('pedra', 'TESOURA'), True),
+            pytest.param(('pedra', 3), False),
+            pytest.param(('pedra', 'pedra'), False),
+            pytest.param(('pedra', 'PEDRA'), False),
+        ],
+    )
+    def test_jokenpo_greater_than(self, entrance, expected):
+        """Unittest."""
+        value1, value2 = entrance
+        result = main.Jokenpo(value1) > main.Jokenpo(value2)
+        assert result == expected
+
+    @pytest.mark.parametrize(
+        'entrance1 entrance2 expected'.split(),
+        [
+            pytest.param(
+                jogador(lance=main.Jokenpo(1)),
+                jogador(lance=main.Jokenpo(1)),
+                'Breno Teixeira X Vinicius Gonçalves: Empate.',
+                marks=[],
+            ),
+            pytest.param(
+                jogador(lance=main.Jokenpo(1)),
+                jogador(lance=main.Jokenpo(2)),
+                'Leandro Costela X Maria Alves: Maria Alves Ganhou!!!',
+                marks=[],
+            ),
+            pytest.param(
+                jogador(lance=main.Jokenpo(1)),
+                jogador(lance=main.Jokenpo(3)),
+                'Danilo Cardoso X Levi Pinto: Danilo Cardoso Ganhou!!!',
+                marks=[],
+            ),
+            pytest.param(
+                jogador(lance=main.Jokenpo(2)),
+                jogador(lance=main.Jokenpo(1)),
+                'Fernando Moraes X Luigi Vieira: Fernando Moraes Ganhou!!!',
+                marks=[],
+            ),
+            pytest.param(
+                jogador(lance=main.Jokenpo(2)),
+                jogador(lance=main.Jokenpo(2)),
+                'Lara Moreira X Maysa Lopes: Empate.',
+                marks=[],
+            ),
+            pytest.param(
+                jogador(lance=main.Jokenpo(2)),
+                jogador(lance=main.Jokenpo(3)),
+                'Ana Vitória Monteiro X Yasmin Ferreira:'
+                ' Yasmin Ferreira Ganhou!!!',
+                marks=[],
+            ),
+            pytest.param(
+                jogador(lance=main.Jokenpo('pedra')),
+                jogador(lance=main.Jokenpo('papel')),
+                'Sra. Maria Clara Cunha X Nicole Pires:'
+                ' Nicole Pires Ganhou!!!',
+                marks=[],
+            ),
+            pytest.param(
+                jogador(lance=main.Jokenpo(3)),
+                jogador(lance=main.Jokenpo(2)),
+                'Camila Gonçalves X Davi Silveira: Camila Gonçalves Ganhou!!!',
+                marks=[],
+            ),
+            pytest.param(
+                jogador(lance=main.Jokenpo(3)),
+                jogador(lance=main.Jokenpo(3)),
+                'Dr. João Lucas Ramos X Otávio Freitas: Empate.',
+                marks=[],
+            ),
+        ],
+    )
+    def test_jogador_op_relacionais(
+        self,
+        entrance1,
+        entrance2,
+        expected,
+    ) -> None:
+        """Test enum."""
+        assert main.start_jokenpo(entrance1, entrance2) == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            pytest.param(
+                'papel',
+                "Jogador(nome='Gustavo da Mata',"
+                ' vitorias=0, derrotas=0, empates=1)',
+                marks=[],
+            ),
+            pytest.param(
+                'papel',
+                "Jogador(nome='Gustavo da Mata',"
+                ' vitorias=0, derrotas=0, empates=2)',
+                marks=[],
+            ),
+            pytest.param(
+                'tesoura',
+                "Jogador(nome='Gustavo da Mata',"
+                ' vitorias=1, derrotas=0, empates=2)',
+                marks=[],
+            ),
+            pytest.param(
+                'pedra',
+                "Jogador(nome='Gustavo da Mata',"
+                ' vitorias=1, derrotas=1, empates=2)',
+                marks=[],
+            ),
+        ],
+    )
+    def test_placar(self, entrance, expected):
+        """Test resultados."""
+        self.j1.lance = main.Jokenpo(entrance)
+        main.start_jokenpo(self.j1, self.j2)
+        assert str(self.j1) == expected
