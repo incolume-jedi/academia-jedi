0148e523850a515dadd57b609c3b46e768cfc65c
Britodfbr <britodfbr@gmail.com>
2025-03-04 07:41:27 -0300

    refactor: Aplicado boas praticas de desenvolvimento python, e pequenas correções

--- incolume/academia_jedi/ajedi20250304_jogo_da_forca/jogo_da_forca.py
+++ incolume/academia_jedi/ajedi20250304_jogo_da_forca/jogo_da_forca.py
@@ -1,16 +1,21 @@
-# !/usr/bin/env python
-# -*- coding: utf-8 -*-
-from tomli import load
+"""Jogo da forca module."""
+
 from pathlib import Path
 
+from tomli import load
+
+# ruff:noqa:T201
 alfabeto = 'abcdefghijklmnopqrstuvxwyz'
 
 chances = 6
 
+palavras_db = Path(__file__).parent / 'palavras.toml'
+
 
 def cabecalho():
+    """Header."""
     print('=' * 90)
-    print("Jogo da Forca".center(90))
+    print('Jogo da Forca'.center(90))
     print('=' * 90)
     print('Bem vindo! Aperte "ENTER" para começar.')
     print('-' * 90)
@@ -18,16 +23,25 @@ def cabecalho():
     print('Que comecem os jogos!!!!')
 
 
-def menu(arquivo_palavras: (str | Path)):
+def menu(arquivo_palavras: (str | Path)) -> list[str]:
+    """Menu.
+
+    Args:
+        arquivo_palavras (str  |  Path): arquivo de base de dados para jogo
+
+    Returns:
+        list[str]: palavras
+    """
     with Path(arquivo_palavras).open('rb') as file:
         palavras = load(file)
-    for opcao in palavras.keys():
-        print(f"* {opcao}")
-    op = input('Digite uma das opções disponíveis:')
+    for opcao in palavras:
+        print(f'* {opcao}')
+    op = input('Digite uma das opções disponíveis: ')
     return palavras.get(op)
 
 
 def run():
+    """Run it."""
     # cabecalho()
     # print(alfabeto)
     # print(chances)
@@ -38,11 +52,9 @@ def run():
     # print(escolha == "b")
     # with Path('palavras.toml').open('rb') as file:
     #     print(load(file))
-    palavras = menu('palavras.toml')
+    palavras = menu(palavras_db)
     print(palavras)
-    pass
 
 
 if __name__ == '__main__':  # pragma: no cover
-    ...
     run()
