f7b7135034c6ad49fdbbb46fe839f653c42df560
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-03-06 14:05:04 -0300

    feat: Estrutura para captar cidades dos estados brasileiros

---
+++ incolume/academia_jedi/ajedi20250306_cidades_br/__init__.py
@@ -0,0 +1,10 @@
+"""Solution for module."""
+
+from typing import Final
+
+mg: Final[str] = (
+    'https://pt.wikipedia.org/wiki/Lista_de_munic%C3%ADpios_de_Minas_Gerais'
+)
+sp: Final[str] = (
+    'https://pt.wikipedia.org/wiki/Lista_de_munic%C3%ADpios_de_S%C3%A3o_Paulo'
+)
