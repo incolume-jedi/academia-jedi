17540d1c4b2dd71f5bccc183b2abec741619b9f3
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-03-10 09:39:32 -0300

    feat: Implementação para estruturar de cidades brasileiras

--- incolume/academia_jedi/ajedi20250306_cidades_br/__init__.py
+++ incolume/academia_jedi/ajedi20250306_cidades_br/__init__.py
@@ -1,10 +1,46 @@
 """Solution for module."""
 
+import logging
+from pathlib import Path
 from typing import Final
 
-mg: Final[str] = (
-    'https://pt.wikipedia.org/wiki/Lista_de_munic%C3%ADpios_de_Minas_Gerais'
+import httpx
+import pandas as pd
+from bs4 import BeautifulSoup
+from icecream import ic
+
+mg: Final[list[str]] = [
+    'https://pt.wikipedia.org/wiki/Lista_de_munic%C3%ADpios_de_Minas_Gerais',
+]
+sp: Final[list[str]] = [
+    'https://pt.wikipedia.org/wiki/Lista_de_munic%C3%ADpios_de_S%C3%A3o_Paulo',
+    'https://www.al.sp.gov.br/documentacao/municipios-paulistas/',
+    'https://www.al.sp.gov.br/arquivos/documentacao/municipios_paulistas/',
+]
+
+cidades_file_txt: Final[Path] = (
+    Path(__file__).parents[3].joinpath('data_files', 'txt', 'cidades_br.txt')
 )
-sp: Final[str] = (
-    'https://pt.wikipedia.org/wiki/Lista_de_munic%C3%ADpios_de_S%C3%A3o_Paulo'
-)
+
+
+def get_cities_sp(url: str = '') -> list[str]:
+    """Get cities.
+
+    By default get cities from Assembléia Legislativa.
+    """
+    url = url or sp[2]
+    cities: list[str] = []
+    response = httpx.get(url)
+    soup = BeautifulSoup(response.content, 'html5lib')
+
+    cities.extend(
+        city.get_text() for city in soup.select('a[class="linkCidade"]')
+    )
+    return cities
+
+
+def get_cities(file: Path | None = None) -> pd.DataFrame:
+    """Get cities."""
+    file = file or cidades_file_txt
+    logging.debug(ic(file))
+    return pd.read_csv(file, sep=';', names=['cod', 'cidade'])
--- incolume/academia_jedi/ajedi20250306_cidades_br/test_cidades_br.py
+++ incolume/academia_jedi/ajedi20250306_cidades_br/test_cidades_br.py
@@ -1,1 +1,75 @@
 """Test module."""
+
+from pathlib import Path
+from typing import ClassVar, Final, NoReturn
+
+import pytest
+import incolume.academia_jedi.ajedi20250306_cidades_br as pkg
+
+
+class TestMunicipios0:
+    """TestMunicipios class."""
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            pytest.param(pkg.mg, list, marks=[]),
+            pytest.param(pkg.sp, list, marks=[]),
+            pytest.param(pkg.cidades_file_txt, Path, marks=[]),
+        ],
+    )
+    def test_variables(self, entrance, expected) -> NoReturn:
+        """Unittest."""
+        assert isinstance(entrance, expected)
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            pytest.param('exists', True),
+            pytest.param('is_file', True),
+            pytest.param('is_dir', False, marks=[]),
+        ],
+    )
+    def test_cidades_file(self, entrance, expected) -> NoReturn:
+        """Unittest."""
+        assert getattr(pkg.cidades_file_txt, entrance)() == expected
+
+    def test_length_get_cities_sp_645(self) -> NoReturn:
+        """Unittest."""
+        total: Final[int] = 645
+        assert len(pkg.get_cities_sp()) == total
+
+    @pytest.mark.parametrize(
+        'entrance',
+        [
+            pytest.param('Adamantina', marks=[]),
+            pytest.param('Adolfo', marks=[]),
+            pytest.param('Águas da Prata', marks=[]),
+            pytest.param('Águas de Lindóia', marks=[]),
+            pytest.param('Águas de Santa Bárbara', marks=[]),
+            pytest.param('Águas de São Pedro', marks=[]),
+            pytest.param('Votuporanga', marks=[]),
+            pytest.param('Zacarias', marks=[]),
+        ],
+    )
+    def test_get_cities_sp(self, entrance) -> NoReturn:
+        """Unittest."""
+        assert {entrance}.issubset(pkg.get_cities_sp())
+
+
+class TestMunicipios:
+    """TestMunicipios class."""
+
+    records: ClassVar[tuple[int, int]] = (5570, 2)
+
+    def test_get_cities_type(self) -> NoReturn:
+        """Unittest."""
+        assert isinstance(pkg.get_cities(), pkg.pd.DataFrame)
+
+    def test_get_cities_shape(self) -> NoReturn:
+        """Unittest."""
+        assert pkg.get_cities().shape == self.records
+
+    def test_get_cities_length(self) -> NoReturn:
+        """Unittest."""
+        assert len(pkg.get_cities()) == self.records[0]
