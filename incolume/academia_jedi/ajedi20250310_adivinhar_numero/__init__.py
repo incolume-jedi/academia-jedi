1304444519a6ae064abed9b8f324effdb8bfdd44
Britodfbr <britodfbr@gmail.com>
2025-03-10 21:56:46 -0300

    feat: solução para acertar número em 3 palpites
    close #104

---
+++ incolume/academia_jedi/ajedi20250310_adivinhar_numero/__init__.py
@@ -0,0 +1,21 @@
+import secrets
+from typing import Final
+from icecream import ic
+
+
+tries: Final[int] = 3
+number: int = secrets.randbelow(3)
+
+for _ in range(tries):
+    if (my_num:= int(input('Dê um palpite e tente adivinhar o número: '))) == number:
+        ic(my_num, number)
+        print('parabéns você acertou!!!')
+        break
+    elif my_num < number:
+        ic(my_num, number)
+        print('O número é maior que o palpite')
+    elif my_num > number:
+        ic(my_num, number)
+        print('O número é menor que o palpite')
+
+
