c61f280914dadeb0b0abcd9f94ce856e3d0d642e
Britodfbr <britodfbr@gmail.com>
2025-03-11 23:17:43 -0300

    feat: Solução para cifra de cesar
    aplicado dictcomprehention
    
    operação com código ascii
    
    close #106

--- incolume/academia_jedi/ajedi20250311/test_20250311.py
+++ incolume/academia_jedi/ajedi20250311/test_20250311.py
@@ -4,6 +4,7 @@ from typing import NoReturn
 
 import pytest
 import incolume.academia_jedi.ajedi20250311.count_vowel as pkg
+import incolume.academia_jedi.ajedi20250311.cesar_cifer as cc
 
 
 class TestCountVowel:
@@ -34,3 +35,30 @@ class TestCountVowel:
 
 class TestCesarCifer:
     """Test case."""
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            ({}, ''),
+            ({'text': 'abcd'}, 'abcd'),
+            ({'text': 'abcd', 'key': 1}, 'bcde'),
+            ({'text': 'ada', 'key': 2}, 'cfc'),
+            ({'text': 'ada', 'key': -2}, 'yby'),
+            ({'text': 'açaí', 'key': 13}, 'nçní'),
+            ({'text': 'água de açaí', 'key': -13}, 'áthn qr nçní'),
+        ],
+    )
+    def test_cesar_cifer(self, entrance, expected):
+        """Unittest."""
+        assert cc.cesar_cifer(**entrance) == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            ('abc', 'nop'),
+            ('', ''),
+        ],
+    )
+    def test_rot13(self, entrance, expected):
+        """Unittest."""
+        assert cc.rot13a(entrance) == expected
