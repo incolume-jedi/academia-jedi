d441f933c8ad08bf1b5c2e0ead8b0bc5cef87301
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-03-13 10:57:57 -0300

    chore(fix)!: realocação modulo ajedi20221128_mapping_values_into_dict

---
+++ incolume/academia_jedi/ajedi20221128_mapping_values_into_dict/run.py
@@ -0,0 +1,14 @@
+"""Run module."""
+
+from with_dict_setdefault import tratativa11
+
+__author__ = '@britodfbr'  # pragma: no cover
+
+
+def run():
+    """Run it."""
+    tratativa11()
+
+
+if __name__ == '__main__':  # pragma: no cover
+    run()
