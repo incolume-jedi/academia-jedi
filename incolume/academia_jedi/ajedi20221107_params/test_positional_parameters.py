976b1da38effaac8df3c36838c9dc9a294305fe4
Britodfbr <britodfbr@gmail.com>
2025-03-12 22:49:47 -0300

    fix: NotFoundError identificado e corrigido
    close #112

--- incolume/academia_jedi/ajedi20221107_params/test_positional_parameters.py
+++ incolume/academia_jedi/ajedi20221107_params/test_positional_parameters.py
@@ -1,8 +1,8 @@
+"""Test module."""
+
 import pytest
 
-# ruff: noqa: A001 A002 ANN001 ANN002 ANN003 ANN201 ANN202 ANN204 ANN401 ARG001 ARG002 ASYNC101 B007 B008 B009 B011 B015 B904 B905 BLE001 C408 C419 C901 D100 D101 D102 D103 D104 D105 D107 D205 D402 D415 D419 DTZ001 DTZ003 DTZ005 DTZ007 E501 E741 EM101 EM102 ERA001 EXE005 F402 F403 F405 F601 F811 F821 F841 FBT001 FBT002 FBT003 FIX002 G001 G002 G004 N801 N802 N805 N806 N816 N999 NPY002 PD901 PERF203 PERF401 PERF402 PIE796 PLE1205 PLR0913 PLR1714 PLR2004 PLW0602 PLW0603 PLW2901 PT004 PT006 PT012 PT015 PTH118 PTH123 PYI024 PYI041 RET503 RET504 RUF001 RUF012 RUF013 S101 S113 S201 S301 S307 S310 S311 S602 S603 S605 S607 S608 SIM103 SIM109 SIM113 SIM115 SIM117 SLF001 SLOT000 T201 T203 TCH003 TD002 TD003 TD004 TRY002 TRY003 TRY300 TRY301 TRY401 W293
-
-from .positional_parameters import divmod, func
+from .positional_params import mydivmod, func
 
 __author__ = '@britodfbr'  # pragma: no cover
 
@@ -19,12 +19,14 @@ __author__ = '@britodfbr'  # pragma: no cover
 
 
 def test_func(capsys):
+    """Unittest."""
     func(10, 20, 30, d=40, e=50, f=60)
-    msgerr, msgout = capsys.readouterr()
-    assert msgerr.strip() == '10 20 30 40 50 60'
+    result = capsys.readouterr()
+    assert result.out.strip() == '10 20 30 40 50 60'
 
 
 def test_func1():
+    """Unittest."""
     with pytest.raises(
         TypeError,
         match=r'got some positional-only '
@@ -34,6 +36,7 @@ def test_func1():
 
 
 def test_func2():
+    """Unittest."""
     with pytest.raises(
         TypeError,
         match=r'takes 4 positional arguments but 5 positional '
@@ -43,19 +46,23 @@ def test_func2():
 
 
 def test_divmod():
-    assert divmod(1, 2)
+    """Unittest."""
+    assert mydivmod(1, 2)
 
 
 def test_divmod1():
+    """Unittest."""
     with pytest.raises(TypeError, match=''):
-        divmod(1, b=2)
+        mydivmod(1, b=2)
 
 
 def test_divmod2():
+    """Unittest."""
     with pytest.raises(TypeError, match=''):
-        divmod(a=1, b=2)
+        mydivmod(a=1, b=2)
 
 
 def test_divmod3():
+    """Unittest."""
     with pytest.raises(TypeError, match=''):
-        divmod(None, None, a=1)
+        mydivmod(None, None, a=1)
