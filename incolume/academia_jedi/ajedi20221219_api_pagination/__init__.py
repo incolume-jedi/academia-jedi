f990c72d36d440a9cc7038eff57f76a8ced779a5
Britodfbr <britodfbr@gmail.com>
2025-03-15 23:22:43 -0300

    chore(fix)!: realocação modulo ajedi20221219_api_pagination

---
+++ incolume/academia_jedi/ajedi20221219_api_pagination/__init__.py
@@ -0,0 +1,50 @@
+"""Pagination module.
+
+[Is This a Good Way to deal with API Pagination?](https://www.youtube.com/watch?v=X4WctWZ2ANw).
+"""
+
+# ruff: noqa: T201
+import logging
+from collections.abc import Iterator
+
+import requests
+from config import settings
+
+__author__ = '@britodfbr'  # pragma: no cover
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s;%(levelname)-8s;%(name)s;'
+    '%(module)s;%(funcName)s;%(message)s',
+)
+timeout: float = settings.timeout
+
+
+def data_get(url: str) -> Iterator:
+    """Dataget."""
+    totals: list = []
+
+    def make_request(url: str, result: list) -> list[str]:
+        """Make request."""
+        resp = requests.get(url, timeout=timeout)
+        logging.debug(url)
+
+        result += resp.json()['results']
+        if resp.json()['info']['next'] is None:
+            return
+
+        make_request(resp.json()['info']['next'], result)
+
+    make_request(url, totals)
+    return totals
+
+
+def run():
+    """Run it."""
+    url = 'https://rickandmortyapi.com/api/character?page=1'
+    result = data_get(url)
+    print(len(result))
+
+
+if __name__ == '__main__':  # pragma: no cover
+    run()
