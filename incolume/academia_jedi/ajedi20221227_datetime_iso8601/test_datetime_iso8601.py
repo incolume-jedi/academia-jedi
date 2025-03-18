c19642eabdcd663fbf2feaf8101ec03ce896df1e
Britodfbr <britodfbr@gmail.com>
2025-03-17 21:19:21 -0300

    refactor: fatorado modulo completamento para sanar ImportPathMisMatchError
    close #127

--- incolume/academia_jedi/ajedi20221227_datetime_iso8601/test_datetime_iso8601.py
+++ incolume/academia_jedi/ajedi20221227_datetime_iso8601/test_datetime_iso8601.py
@@ -1,7 +1,11 @@
+"""Test iso8601."""
+
+# ruff: noqa: E501
+
 import logging
-
-from datetime import datetime
-
+from icecream import ic
+import datetime as dt
+import pytest
 import pytz
 import incolume.academia_jedi.ajedi20221227_datetime_iso8601.handler_datetime_iso8601 as pkg
 
@@ -10,31 +14,53 @@ logging.basicConfig(
     format='%(asctime)s;%(levelname)-8s;%(name)s;'
     '%(module)s;%(funcName)s;%(message)s',
 )
+
+
 class TestISO8601:
     """Test case."""
-    DATE: datetime = datetime(
-    1978,
-    6,
-    20,
-    1,
-    23,
-    45,
-    6789,
-    pytz.timezone('America/Sao_Paulo'),
-)
 
-    pytest.mark.parametrize(
+    DATE: dt.datetime = dt.datetime(
+        1978,
+        6,
+        20,
+        1,
+        23,
+        45,
+        6789,
+        pytz.timezone('America/Sao_Paulo'),
+    )
+
+    @pytest.fixture()
+    def _patch_datetime_now(self, monkeypatch):
+        """Fixture."""
+
+        class FakeDateTime(dt.datetime):
+            """Class fake datetime."""
+
+            @classmethod
+            def now(cls, *args, **kwargs):
+                """Method now."""
+                ic(args, kwargs)
+                return self.DATE
+
+        monkeypatch.setattr(dt, 'datetime', FakeDateTime)
+
+    @pytest.mark.parametrize(
         'func entrance expected'.split(),
         [
-        ('iso8601_format_01', DATE, ''),
-        ('iso8601_format_02', None, ''),
-        ('iso8601_format_03', DATE, ''),
-        ('iso8601_format_04', DATE, ''),
-        ('iso8601_format_05', DATE, ''),
-        ('iso8601_format_06', DATE, ''),
-        ('iso8601_format_07', DATE, ''),
+            ('iso8601_format_01', DATE, '1978-06-20T01:23:45.006789-03:06'),
+            ('iso8601_format_03', DATE, '1978-06-20T01:23:45.006-03:06'),
+            ('iso8601_format_04', DATE, '1978-06-20T01:23:45.006789-0306'),
+            ('iso8601_format_05', DATE, '1978-06-20 01:23:45.006789-0306'),
+            ('iso8601_format_06', DATE, '1978-06-20T01:23:45.006-03:06'),
+            ('iso8601_format_07', DATE, '1978-06-20T01:23:45-03:06'),
         ],
     )
     def test_iso8601(self, func, entrance, expected):
         """Unittest."""
         assert getattr(pkg, func)(entrance) == expected
+
+    @pytest.mark.usefixtures('_patch_datetime_now')
+    def test_iso8602_format02(self):
+        """Unittest."""
+        assert pkg.iso8601_format_02() == '1978-06-20 01:23:45.006-03:06'
