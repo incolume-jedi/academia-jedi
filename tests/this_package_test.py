1763b74e2e9124883de09f3567be0517eda53127
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-03-18 07:54:22 -0300

    test: Aplicado principio DRY - Don´t repeate yourself.

--- tests/this_package_test.py
+++ tests/this_package_test.py
@@ -10,38 +10,16 @@ class TestPackage:
     """Test for test this package."""
 
     @pytest.mark.parametrize(
-        'entrance expected'.split(),
-        [
-            pytest.param(
-                'format_log',
-                r'%(asctime)s;%(levelname)-8s;%(name)s;%(module)s;%(funcName)s;%(message)s',
-                marks=[],
-            ),
-            pytest.param(
-                'format_log_win',
-                r'%(asctime)s;%(levelname)-8s;%(name)s;%(module)s;%(funcName)s;%(message)s',
-                marks=[],
-            ),
-            pytest.param('msg', 'Hello Dev', marks=[]),
-            pytest.param('tz', 'America/Sao_Paulo', marks=[]),
-            pytest.param('blue', '#0060B5', marks=[]),
-        ],
-    )
-    def test_load_envvar_default(self, entrance, expected) -> NoReturn:
-        """Unittest."""
-        assert getattr(pkg.settings, entrance) == expected
-
-    @pytest.mark.parametrize(
         'environment entrance expected'.split(),
         [
             pytest.param(
-                'production',
+                '',
                 'format_log',
                 r'%(asctime)s;%(levelname)-8s;%(name)s;%(module)s;%(funcName)s;%(message)s',
                 marks=[],
             ),
             pytest.param(
-                'production',
+                '',
                 'format_log_win',
                 r'%(asctime)s;%(levelname)-8s;%(name)s;%(module)s;%(funcName)s;%(message)s',
                 marks=[],
@@ -50,11 +28,25 @@ class TestPackage:
             pytest.param('production', 'msg', 'Hello User', marks=[]),
             pytest.param('development', 'msg', 'Hello Dev', marks=[]),
             pytest.param('testing', 'msg', 'Hello tester', marks=[]),
+            pytest.param('', 'tz', 'America/Sao_Paulo', marks=[]),
+            pytest.param('', 'blue', '#0060B5', marks=[]),
+            pytest.param(
+                'production',
+                'format_log',
+                r'%(asctime)s;%(levelname)-8s;%(name)s;%(module)s;%(funcName)s;%(message)s',
+                marks=[],
+            ),
+            pytest.param(
+                'production',
+                'format_log_win',
+                r'%(asctime)s;%(levelname)-8s;%(name)s;%(module)s;%(funcName)s;%(message)s',
+                marks=[],
+            ),
             pytest.param('testing', 'tz', 'America/Sao_Paulo', marks=[]),
             pytest.param('testing', 'blue', '#0060B5', marks=[]),
         ],
     )
-    def test_load_envvar_per_env(
+    def test_load_envvar(
         self,
         environment,
         entrance,
