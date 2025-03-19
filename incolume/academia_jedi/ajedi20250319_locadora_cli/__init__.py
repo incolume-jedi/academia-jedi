05b4374836512c09a35a6d929087a366194efebb
Britodfbr <britodfbr@gmail.com>
2025-03-19 20:11:34 -0300

    feat: implementação com carga da categoria via arquivos yaml e respectivos testes

--- incolume/academia_jedi/ajedi20250319_locadora_cli/__init__.py
+++ incolume/academia_jedi/ajedi20250319_locadora_cli/__init__.py
@@ -1,1 +1,29 @@
 """Locadora CLI."""
+
+from dataclasses import dataclass
+from enum import IntEnum, auto
+from pathlib import Path
+
+import yaml
+from icecream import ic
+
+fileconf = Path(__file__).parent / 'locadora.yaml'
+config = ic(yaml.safe_load(fileconf.open()))
+
+
+Montadora: IntEnum = IntEnum('Montadora', config['montadora'], module=__name__)
+Categoria: IntEnum = IntEnum('Categoria', config['categoria'], module=__name__)
+
+
+@dataclass
+class Veiculo:
+    """Veiculo dataclass."""
+
+    modelo: str
+    montadora: Montadora
+    categoria: Categoria
+
+
+if __name__ == '__main__':
+    """..."""
+    ic(yaml.safe_load(fileconf.open()))
--- incolume/academia_jedi/ajedi20250319_locadora_cli/test_20250319_locadora_cli.py
+++ incolume/academia_jedi/ajedi20250319_locadora_cli/test_20250319_locadora_cli.py
@@ -1,1 +1,71 @@
 """Tests Locadora CLI."""
+
+from enum import IntEnum
+from typing import NoReturn
+
+import pytest
+from . import Categoria, Montadora, Veiculo
+from dataclasses import is_dataclass
+
+
+class TestLocadora:
+    """Test case."""
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            (Categoria, False),
+            (Montadora, False),
+            (Veiculo, True),
+        ],
+    )
+    def test_is_dataclass(self, entrance, expected) -> NoReturn:
+        """Unittest."""
+        assert is_dataclass(entrance) is expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            pytest.param(Categoria, True, marks=[pytest.mark.skip]),
+            pytest.param(Montadora, True, marks=[pytest.mark.skip]),
+            pytest.param(Veiculo, False, marks=[pytest.mark.skip]),
+        ],
+    )
+    def test_is_enum(self, entrance, expected) -> NoReturn:
+        """Unittest."""
+        assert isinstance(entrance, IntEnum) is expected
+
+    @pytest.mark.parametrize(
+        'fenum entrance expected'.split(),
+        [
+            pytest.param(Categoria, 1, Categoria.passeio, marks=[]),
+            pytest.param(Categoria, 2, Categoria.carga, marks=[]),
+            pytest.param(Categoria, 3, Categoria.transporte, marks=[]),
+            pytest.param(Montadora, 1, Montadora.indefinida, marks=[]),
+            pytest.param(Montadora, 2, Montadora.byd, marks=[]),
+            pytest.param(Montadora, 3, Montadora.gm, marks=[]),
+        ],
+    )
+    def test_enum_values(self, fenum, entrance, expected) -> NoReturn:
+        """Unittest."""
+        assert fenum(entrance) == expected
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            pytest.param(
+                Categoria,
+                Categoria.passeio,
+                marks=[],
+            ),
+            pytest.param(Montadora, Montadora.indefinida, marks=[]),
+        ],
+    )
+    def test_enum_names(self, entrance, expected) -> NoReturn:
+        """Unittest."""
+        assert expected in list(entrance.__members__.values)
+
+    def test_0(self):
+        """Unittest."""
+        assert Montadora.__members__ == {}
+        # assert Montadora.__members__.items() == []
