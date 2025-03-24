67b3acbe8995351060cbce568b1992dc30aeee1f
Britodfbr <britodfbr@gmail.com>
2025-03-24 09:19:04 -0300

    refactor: Acrescentado movimento continuado

--- incolume/academia_jedi/ajedi20250324_pysnake/__init__.py
+++ incolume/academia_jedi/ajedi20250324_pysnake/__init__.py
@@ -22,22 +22,31 @@ class Personagem:
         """As dict."""
         return asdict(self)
 
+
 def draw_screen(window: Any) -> None:
     """Desenhar tela."""
     window.clear()
     window.border(0)
 
+
 def draw_actor(actor: Personagem, window: Any) -> None:
     """Desenha ator."""
     window.addch(actor.lin, actor.col, actor.simbol)
 
-def get_new_direction(window: Any, timeout:int = 1000) -> int|None:
+
+def get_new_direction(window: Any, timeout: int = 1000) -> int | None:
     """Checa nova direção."""
     window.timeout(timeout)
     direction = window.getch()
-    if direction in [curses.KEY_UP, curses.KEY_DOWN, curses.KEY_LEFT, curses.KEY_RIGHT]:
+    if direction in [
+        curses.KEY_UP,
+        curses.KEY_DOWN,
+        curses.KEY_LEFT,
+        curses.KEY_RIGHT,
+    ]:
         return direction
-    return
+    return None
+
 
 def move_actor(actor: Personagem, direction: int) -> None:
     """Move ator."""
@@ -56,26 +65,30 @@ def move_actor(actor: Personagem, direction: int) -> None:
             logging.debug(ic('MOVE RIGHT'))
             actor.col += 1
 
+
 def check_actor_hit_border(actor: Personagem, window: Any) -> bool:
     """Checa limite tela."""
     heigth, width = window.getmaxyx()
-    return ((actor.lin <= 0) or (actor.lin >= heigth - 1)) or ((actor.col <= 0) or (actor.col >= width - 1))
-
+    return ((actor.lin <= 0) or (actor.lin >= heigth - 1)) or (
+        (actor.col <= 0) or (actor.col >= width - 1)
+    )
 
 
 def game_loop(window):
     """Loop."""
     curses.curs_set(0)
     personagem = Personagem(10, 15, curses.ACS_DIAMOND)
-
+    current_direction = curses.KEY_DOWN
 
     while True:
         draw_screen(window)
         draw_actor(actor=personagem, window=window)
-        if (direction:=get_new_direction(window=window)):
-            move_actor(actor=personagem, direction=direction)
+        if not (direction := get_new_direction(window=window)):
+            direction = current_direction
+        move_actor(actor=personagem, direction=direction)
         if check_actor_hit_border(actor=personagem, window=window):
             return
+        current_direction = direction
 
 
 def run():
