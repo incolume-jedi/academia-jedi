441e5a3761dd7556685cf0aaf0e2f72771f58e7c
Britodfbr <britodfbr@gmail.com>
2025-03-26 14:04:30 -0300

    test: implementado test de Personagem

--- incolume/academia_jedi/ajedi20250324_pysnake/test_ajedi20250324_pysnake.py
+++ incolume/academia_jedi/ajedi20250324_pysnake/test_ajedi20250324_pysnake.py
@@ -1,1 +1,19 @@
 """Test module."""
+
+from typing import NoReturn
+import pytest
+import incolume.academia_jedi.ajedi20250324_pysnake as mypysnake
+
+
+class TestMyPySnake:
+    """Test cases."""
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            (mypysnake.Personagem(), {'col': 15, 'lin': 10}),
+        ],
+    )
+    def test_0(self, entrance, expected) -> NoReturn:
+        """Test unit."""
+        assert entrance.__dict__ == expected
