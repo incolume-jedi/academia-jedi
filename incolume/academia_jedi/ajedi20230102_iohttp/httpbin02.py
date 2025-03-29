d0eafb38b963f40c51ddeddab088f1b2a91a3eb2
Britodfbr <britodfbr@gmail.com>
2025-03-29 09:56:28 -0300

    style: Sanitização de código
    close #148

--- incolume/academia_jedi/ajedi20230102_iohttp/httpbin02.py
+++ incolume/academia_jedi/ajedi20230102_iohttp/httpbin02.py
@@ -1,15 +1,32 @@
+"""Estudos com aiohttp."""
+
 import asyncio
+from collections.abc import Coroutine
 
-# ruff: noqa: A001 A002 ANN001 ANN002 ANN003 ANN201 ANN202 ANN204 ANN401 ARG001 ARG002 ASYNC101 B007 B008 B009 B011 B015 B904 B905 BLE001 C408 C419 C901 D100 D101 D102 D103 D104 D105 D107 D205 D402 D415 D419 DTZ001 DTZ003 DTZ005 DTZ007 E501 E741 EM101 EM102 ERA001 EXE005 F402 F403 F405 F601 F811 F821 F841 FBT001 FBT002 FBT003 FIX002 G001 G002 G004 N801 N802 N805 N806 N816 N999 NPY002 PD901 PERF203 PERF401 PERF402 PIE796 PLE1205 PLR0913 PLR1714 PLR2004 PLW0602 PLW0603 PLW2901 PT004 PT006 PT012 PT015 PTH118 PTH123 PYI024 PYI041 RET503 RET504 RUF001 RUF012 RUF013 S101 S113 S201 S301 S307 S310 S311 S602 S603 S605 S607 S608 SIM103 SIM109 SIM113 SIM115 SIM117 SLF001 SLOT000 T201 T203 TCH003 TD002 TD003 TD004 TRY002 TRY003 TRY300 TRY301 TRY401 W293
 from aiohttp import ClientSession
+from icecream import ic
 
 
-async def fetch(url, session):
+async def fetch(url: str, session: ClientSession) -> Coroutine:
+    """Fetch session.
+
+    Args:
+        url (str): _description_
+        session (ClientSession): _description_
+
+    Returns:
+        Coroutine: _description_
+    """
     async with session.get(url) as response:
         return await response.read()
 
 
-async def run(r):
+async def run(r: int) -> None:
+    """Run it.
+
+    Args:
+        r (int): limit of range;
+    """
     url = 'http://httpbin.org/delay/{}'
     tasks = []
 
@@ -22,13 +39,19 @@ async def run(r):
 
         responses = await asyncio.gather(*tasks)
         # you now have all response bodies in this variable
-        print(responses)
+        ic(responses)
 
 
-def print_responses(result):
-    print(result)
+def print_responses(result: str) -> None:
+    """Print responses.
+
+    Args:
+        result (str): _description_
+    """
+    ic(result)
 
 
-loop = asyncio.get_event_loop()
-future = asyncio.ensure_future(run(4))
-loop.run_until_complete(future)
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    future = asyncio.ensure_future(run(4))
+    loop.run_until_complete(future)
