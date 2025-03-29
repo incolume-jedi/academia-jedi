29a9b0c26fd3cbdf2b3410e7fe3e486d9f110f96
Britodfbr <britodfbr@gmail.com>
2025-03-29 10:54:24 -0300

    style(fix): Sanitização em ajedi20230107_magic_mathods

--- incolume/academia_jedi/ajedi20230107_magic_mathods/currency.py
+++ incolume/academia_jedi/ajedi20230107_magic_mathods/currency.py
@@ -1,11 +1,14 @@
+"""Estudo sobre metodos mágicos."""
+
 __author__ = '@britodfbr'  # pragma: no cover
 
-# ruff: noqa: A001 A002 ANN001 ANN002 ANN003 ANN201 ANN202 ANN204 ANN401 ARG001 ARG002 ASYNC101 B007 B008 B009 B011 B015 B904 B905 BLE001 C408 C419 C901 D100 D101 D102 D103 D104 D105 D107 D205 D402 D415 D419 DTZ001 DTZ003 DTZ005 DTZ007 E501 E741 EM101 EM102 ERA001 EXE005 F402 F403 F405 F601 F811 F821 F841 FBT001 FBT002 FBT003 FIX002 G001 G002 G004 N801 N802 N805 N806 N816 N999 NPY002 PD901 PERF203 PERF401 PERF402 PIE796 PLE1205 PLR0913 PLR1714 PLR2004 PLW0602 PLW0603 PLW2901 PT004 PT006 PT012 PT015 PTH118 PTH123 PYI024 PYI041 RET503 RET504 RUF001 RUF012 RUF013 S101 S113 S201 S301 S307 S310 S311 S602 S603 S605 S607 S608 SIM103 SIM109 SIM113 SIM115 SIM117 SLF001 SLOT000 T201 T203 TCH003 TD002 TD003 TD004 TRY002 TRY003 TRY300 TRY301 TRY401 W293
 
 from dataclasses import dataclass
 
 
 class Currency:
+    """Currency class."""
+
     def __init__(
         self,
         value: float = 0.0,
@@ -13,23 +16,29 @@ class Currency:
         sigla: str = 'BRL',
         symbol: str = 'R$',
     ) -> None:
+        """Init class."""
         self.sigla = sigla
         self.symbol = symbol
         self.value = value
 
     def __str__(self) -> str:
+        """Str class."""
         return 'Currency({sigla} {value:0.2f})'.format(**self.__dict__)
 
     def __repr__(self) -> str:
+        """Repr class."""
         return '{symbol} {value:0.2f}'.format(**self.__dict__)
 
     def __lt__(self, other):
+        """Less than."""
         return self.value < other.value
 
     def __gt__(self, other):
+        """Grand than."""
         return self.value > other.value
 
     def __add__(self, other):
+        """Add."""
         if isinstance(other, Currency) and (self.sigla == other.sigla):
             self.value += other.value
         else:
@@ -37,12 +46,15 @@ class Currency:
         return self
 
     def __iadd__(self, other):
+        """Inverse add."""
         return self.__add__(other)
 
     def __radd__(self, other):
+        """Reverse add."""
         return self.__add__(other)
 
     def __sub__(self, other):
+        """Sub."""
         if isinstance(other, Currency) and (self.sigla == other.sigla):
             self.value -= other.value
         else:
@@ -50,22 +62,28 @@ class Currency:
         return self
 
     def __isub__(self, other):
+        """Inverse sub."""
         return self.__sub__(other)
 
     def __rsub__(self, other):
+        """Reverse sub."""
         return self.__sub__(other)
 
     def __mul__(self, other):
+        """Mult."""
         self.value *= other
         return self
 
     def __imul__(self, other):
+        """Inverse mult."""
         return self.__mul__(other)
 
     def __rmul__(self, other):
+        """Reverse mult."""
         return self.__mul__(other)
 
     def __floordiv__(self, other):
+        """Floordiv."""
         if isinstance(other, Currency) and self.sigla == other.sigla:
             self.value //= other.value
         else:
@@ -73,9 +91,11 @@ class Currency:
         return self
 
     def __rfloordiv__(self, other):
+        """Reverse floordiv."""
         return self.__floordiv__(other)
 
     def __truediv__(self, other):
+        """True div."""
         if isinstance(other, Currency) and self.sigla == other.sigla:
             self.value /= other.value
         else:
@@ -83,27 +103,35 @@ class Currency:
         return self
 
     def __rtruediv__(self, other):
+        """True div reverse."""
         return self.__truediv__(other)
 
     def __neg__(self):
+        """Negativ."""
         self.value *= -1
         return self
 
 
 @dataclass
 class NC:
+    """NC class."""
+
     value: float = 0.0
     sigla: str = 'BRL'
     symbol: str = 'R$'
 
     def __str__(self) -> str:
+        """Str class."""
         return 'Currency({sigla} {value:0.2f})'.format(**self.__dict__)
 
     def __repr__(self) -> str:
+        """Repr class."""
         return '{symbol} {value:0.2f}'.format(**self.__dict__)
 
     def __lt__(self, other):
+        """Less than."""
         return self.value < other.value
 
     def __gt__(self, other):
+        """Grand than."""
         return self.value > other.value
