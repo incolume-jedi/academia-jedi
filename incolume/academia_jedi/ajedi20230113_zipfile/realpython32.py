53dbcdecf633cd2a8ad255c47253a0abd36a64ae
Britodfbr <britodfbr@gmail.com>
2025-03-31 15:39:37 -0300

    build: Bumping version from 0.112.0a5 to 0.112.0a6

--- incolume/academia_jedi/ajedi20230113_zipfile/realpython32.py
+++ incolume/academia_jedi/ajedi20230113_zipfile/realpython32.py
@@ -4,7 +4,6 @@
 import logging
 import sys
 
-from incolume.academia_jedi.ajedi20230113_zipfile import base_dir
 from incolume.academia_jedi.ajedi20230113_zipfile.realpython31 import hello_pkg
 
 logging.basicConfig(
@@ -12,6 +11,7 @@ logging.basicConfig(
     format='%(asctime)s;%(levelname)-8s;%(name)s;'
     '%(module)s;%(funcName)s;%(message)s',
 )
+
 
 def run():
     """Run it."""
