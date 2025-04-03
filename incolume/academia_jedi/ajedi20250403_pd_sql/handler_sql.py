b4827f9b860a9f00f2c22a613d4f753c4a363d27
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-04-03 13:27:00 -0300

    feat: persistência em SQL

--- incolume/academia_jedi/ajedi20250403_pd_sql/handler_sql.py
+++ incolume/academia_jedi/ajedi20250403_pd_sql/handler_sql.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 import pandas as pd
 
-conn: sqlite3.Connection = sqlite3.connect('web.db')
+conn: sqlite3.Connection = sqlite3.connect('web.sqlitedb')
 
 
 def data_frame_fake(limit: int = 100, columns: int = 4) -> pd.DataFrame:
@@ -15,7 +15,7 @@ def data_frame_fake(limit: int = 100, columns: int = 4) -> pd.DataFrame:
     limit = max(limit, 10)
     serie = pd.date_range(start='1/1/2025', periods=limit)
     data = [
-        [secrets.randbelow(350) for _ in range(columns)] for _ in range(limit)
+        [max(secrets.randbelow(350), 100) for _ in range(columns)] for _ in range(limit)
     ]
     return pd.DataFrame(
         index=serie, columns=list(string.ascii_uppercase[:columns]), data=data
@@ -24,9 +24,8 @@ def data_frame_fake(limit: int = 100, columns: int = 4) -> pd.DataFrame:
 
 def dataframe2sql(
     dataframe: pd.DataFrame,
-    fileoutput: Path,
     tablename: str,
     connection: sqlite3.Connection,
 ) -> bool:
     """Dataframe to SQL."""
-    dataframe.to_sql(name=tablename, con=connection)
+    dataframe.to_sql(name=tablename, con=connection, if_exists='append')
