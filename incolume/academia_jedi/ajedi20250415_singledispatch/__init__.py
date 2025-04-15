29bdd1b4a7b63bcf18e07deaec502229999309e6
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-04-15 12:14:13 -0300

    feat: Exemplos de uso para singledispatch

--- incolume/academia_jedi/ajedi20250415_singledispatch/__init__.py
+++ incolume/academia_jedi/ajedi20250415_singledispatch/__init__.py
@@ -1,1 +1,85 @@
 """Module."""
+# ruff: noqa: T201
+
+import cmath
+from functools import singledispatch
+from typing import Union
+
+from icecream import ic
+
+__all__ = ['cmath']
+
+
+ic()
+
+
+@singledispatch
+def fun(arg, *, verbose: bool = False) -> str | tuple:  # noqa: ANN001
+    """Main function."""
+    # ic()
+    result = f'{arg}'
+    if verbose:
+        # ic()
+        result = 'Let me just say,', result
+    return result
+
+
+@fun.register
+def _(arg: int, *, verbose: bool = False) -> int | tuple:
+    """Case int type."""
+    result = arg
+    if verbose:
+        result = 'Strength in numbers, eh?', result
+    return result
+
+
+@fun.register
+def _(arg: list[str], *, verbose: bool = False) -> tuple:
+    """Case list type."""
+    result = []
+    if verbose:
+        result.append('Enumerate this:')
+    for i, elem in enumerate(arg):
+        result.append((i, elem))
+    return tuple(result)
+
+
+@fun.register
+def _(arg: float, *, verbose: bool = False) -> float | tuple:
+    """Case int or float type."""
+    result = arg
+    if verbose:
+        result = 'Strength in numbers, eh?', result
+    return result
+
+
+@fun.register
+def _(arg: Union[list, set], *, verbose: bool = False) -> list:
+    """Case set or list type."""
+    result = []
+    if verbose:
+        result.append(f'Enumerate this {type(arg).__name__}:')
+    for i, elem in enumerate(arg):
+        result.append((i, elem))
+    return result
+
+
+@fun.register(complex)
+def _(arg: complex, *, verbose: bool = False) -> None:
+    """Case set or list type."""
+    if verbose:
+        print('Better than complicated.', sep=' ')
+    print(arg.real, arg.imag)
+
+
+@fun.register(list)
+def _(arg: list[int], *, verbose: bool = False) -> None:
+    """Case list of integer type."""
+    result = []
+    if verbose:
+        result.append('Enumerate this:')
+    for i, elem in enumerate(arg):
+        result.append((i, elem))
+
+
+fun.register(type(None), lambda _: print('Nothing..'))
--- incolume/academia_jedi/ajedi20250415_singledispatch/test_singledispatch.py
+++ incolume/academia_jedi/ajedi20250415_singledispatch/test_singledispatch.py
@@ -1,1 +1,52 @@
 """Test module."""
+
+import pytest
+import incolume.academia_jedi.ajedi20250415_singledispatch as pkg
+
+
+class TestCase:
+    """Case de teste para singledispatch."""
+
+    @pytest.mark.parametrize(
+        'entrance kwargs expected'.split(),
+        [
+            pytest.param('', {}, '', marks=[]),
+            pytest.param(
+                '',
+                {'verbose': True},
+                ('Let me just say,', ''),
+                marks=[],
+            ),
+            pytest.param('Jesus te ama!', {}, 'a', marks=[pytest.mark.skip]),
+            pytest.param(
+                'Tudo é dificil até fácil se tornar.',
+                {},
+                'a',
+                marks=[pytest.mark.skip],
+            ),
+            pytest.param(
+                1,
+                {'verbose': True},
+                ('Strength in numbers, eh?', 1),
+                marks=[],
+            ),
+            pytest.param(
+                3.14,
+                {'verbose': True},
+                ('Strength in numbers, eh?', 3.14),
+                marks=[],
+            ),
+            pytest.param(
+                set('abc'),
+                {'verbose': True},
+                ['Enumerate this set:', (0, 'a'), (1, 'b'), (2, 'c')],
+                marks=[],
+            ),
+            pytest.param(list('abc'), {'verbose': True}, '', marks=[]),
+        ],
+    )
+    def test_function(self, entrance, kwargs, expected, capsys):
+        """Unittest."""
+        capture = capsys.readouterr()
+        assert pkg.fun(entrance, **kwargs) == expected
+        assert capture.out == ''
