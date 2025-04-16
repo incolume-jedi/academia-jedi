a9e4df9f054da66ab92b095fae256099c7ed3b6d
Britodfbr <britodfbr@gmail.com>
2025-04-16 16:50:54 -0300

    test: teardown para limpar modulo fake criado.

--- incolume/academia_jedi/ajedi20250416_reload_runtime/test_ajedi20250416_reload_runtime.py
+++ incolume/academia_jedi/ajedi20250416_reload_runtime/test_ajedi20250416_reload_runtime.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from typing import NoReturn
 import pytest
 from importlib import reload
+from icecream import ic
 
 
 class TestReloadRuntime:
@@ -12,13 +13,20 @@ class TestReloadRuntime:
     @classmethod
     def setup_class(cls):
         """Setup class."""
+        ic('criação do modulo fake.')
         cls.file = Path(__file__).parent.joinpath('other.py')
         with cls.file.open('w') as f:
-            f.write(r'"""Fake module."""')
+            f.write('"""Fake module."""')
 
         import incolume.academia_jedi.ajedi20250416_reload_runtime.other as pkg
 
         cls.pkg = pkg
+
+    @classmethod
+    def teardown_class(cls):
+        """Teardown class."""
+        ic('Descarte do modulo fake.')
+        cls.file.unlink(missing_ok=True)
 
     @pytest.mark.parametrize(
         'entrance expected'.split(),
@@ -34,7 +42,7 @@ class TestReloadRuntime:
         ],
     )
     def test_reaload_runtime(self, entrance, expected) -> NoReturn:
-        """Unittest."""
+        """Unittest para nomes existentes no módulo."""
         try:
             assert getattr(self.pkg, entrance) == expected
         except AttributeError:
@@ -42,8 +50,9 @@ class TestReloadRuntime:
                 getattr(self.pkg, entrance)
 
     def test_runtime(self) -> NoReturn:
-        """Unittest."""
+        """Criação e carregamento de __version__ em runtime."""
+        expected = '0.0.1'
         with self.file.open('a') as f:
-            f.write('')
-            f.write(r'__version__="0.0.1"')
+            f.write(f"\n\n__version__ = '{expected}'")
         reload(self.pkg)
+        assert self.pkg.__version__ == expected
