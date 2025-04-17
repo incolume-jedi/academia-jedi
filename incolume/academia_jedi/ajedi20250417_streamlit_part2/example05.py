6bbee6afa9c0e773dbf756adeba2cf1bae8eb335
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-04-17 09:45:58 -0300

    refactor: Estuturação de layout em colunas

--- incolume/academia_jedi/ajedi20250417_streamlit_part2/example05.py
+++ incolume/academia_jedi/ajedi20250417_streamlit_part2/example05.py
@@ -20,8 +20,15 @@ albuns = df_filtered.Album.value_counts().index
 album = st.selectbox('Album', albuns)
 df_filtered2 = df0[df0.Album == album]
 
-show = st.checkbox('Display graph')
-if show:
-    st.bar_chart(df_filtered2['Stream'])
+# show = st.checkbox('Display graph')
+# if show:
+#     st.bar_chart(df_filtered2['Stream'])
+
+col1, col2 = st.columns(2)
+col1.bar_chart(df_filtered2['Stream'])
+col2.line_chart(df_filtered2['Danceability'])
 
 st.write(artist)
+col1, col2 = st.columns([.3,.7])
+col2.bar_chart(df_filtered2['Stream'])
+col1.line_chart(df_filtered2['Danceability'])
