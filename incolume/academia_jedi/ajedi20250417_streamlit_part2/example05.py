77d92be8836b0428bc4f58d348cc3306edc3611f
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-04-17 10:33:51 -0300

    fat: Implementação de cache para projeto.

--- incolume/academia_jedi/ajedi20250417_streamlit_part2/example05.py
+++ incolume/academia_jedi/ajedi20250417_streamlit_part2/example05.py
@@ -9,9 +9,20 @@ st.set_page_config(
     layout='wide',
     page_title='Spotify songs',
 )
-df0 = pd.read_csv(datafile)
 
-st.session_state['df_spotify'] = df0  # cache state
+
+@st.cache_data
+def load_data():
+    """Carga de datafram.
+
+    Indicado para operações computacionalmente caras.
+    """
+    return pd.read_csv(datafile)
+
+
+st.session_state['df_spotify'] = load_data()  # preservação de estado
+
+df0 = st.session_state['df_spotify']
 
 df0.set_index('Track', inplace=True)
 
@@ -32,6 +43,6 @@ col1.bar_chart(df_filtered2['Stream'])
 col2.line_chart(df_filtered2['Danceability'])
 
 st.write(artist)
-col1, col2 = st.columns([.3,.7])
+col1, col2 = st.columns([0.3, 0.7])
 col2.bar_chart(df_filtered2['Stream'])
 col1.line_chart(df_filtered2['Danceability'])
