72ceae11a0550c107c784e14564cd9c6299a54f3
Britodfbr <britodfbr@gmail.com>
2025-04-18 08:36:26 -0300

    test: estrutura de testes unitários implementada

--- incolume/academia_jedi/ajedi20250417_dashboard_fifa2023/test_ajedi20250417_dashboard_fifa2023.py
+++ incolume/academia_jedi/ajedi20250417_dashboard_fifa2023/test_ajedi20250417_dashboard_fifa2023.py
@@ -1,1 +1,12 @@
 """Test Dashboard FIFA2023."""
+
+from typing import NoReturn
+from incolume.academia_jedi.ajedi20250417_dashboard_fifa2023 import URLS
+
+
+class TestDashboard:
+    """Case tests."""
+
+    def test_urls(self) -> NoReturn:
+        """Unittest."""
+        assert URLS.__annotations__ == ''
