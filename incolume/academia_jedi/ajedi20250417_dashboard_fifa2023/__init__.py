5cc5fd7c0b8809a7010ee24d4ac5b5c9c5acf539
Britodfbr <britodfbr@gmail.com>
2025-04-19 10:00:08 -0300

    feat: Finalização da tarefa
    close #164

--- incolume/academia_jedi/ajedi20250417_dashboard_fifa2023/__init__.py
+++ incolume/academia_jedi/ajedi20250417_dashboard_fifa2023/__init__.py
@@ -40,7 +40,6 @@ class URLS:
         kw_only=True,
     )
 
-@lru_cache
 def get_dataset(url_zipfile: str = '', target_file: str = '') -> io.BytesIO:
     """Get dataset."""
     url_zipfile = url_zipfile or URLS.zip_ds_fifa
