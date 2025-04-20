2427c40a9c04c87beffe7e9c0eda7a558cefb6c2
Britodfbr <britodfbr@gmail.com>
2025-04-20 16:41:46 -0300

    feat: Frontend definido.

--- incolume/academia_jedi/ajedi20250420_streamlit_auth/__init__.py
+++ incolume/academia_jedi/ajedi20250420_streamlit_auth/__init__.py
@@ -41,8 +41,36 @@ def login_form(authenticator: stauth.Authenticate):
         st.error('Usuário/Senha incorretos.')
     elif authenticator_satatus == None:
         st.warning('Por favor informe um usuário e senha')
-        if clicou_registrar:
+        if 'clicou_registrar':
             st.session_state['clicou_registrar'] = True
             st.rerun()
+
+def confirm_msg():
+    """Confirm msg."""
+    hashed_password = stauth.Hasher([st.session_state.pswrd]).generate()
+    if st.session_state.pswrd != st.session_state.confirm_pswrd:
+        st.warning('Senhas não conferem')
+    elif 'consulta_nome()':
+        st.warning('Nome de usuário já cadastrado.')
+    else:
+        'add_registro()'
+        st.success('Registro efetuado!')
+
+
+def usuario_form():
+    """Formulário de login."""
+    with st.form(key='formulario', clear_on_submit=True):
+        nome = st.text_input('Nome', key='nome')
+        username = st.text_input('Usuário', key='user')
+        password = st.text_input('Senha', key='pswrd', type='password')
+        confirm_password = st.text_input('Confirme senha', key='confir_pswrd', type='password')
+        submit = st.form_submit_button(
+            'Salvar', on_click=confirm_msg,
+        )
+        clicou_fazer_login = st.button('Fazer login?')
+        if clicou_fazer_login:
+            st.session_state['clicou_fazer_login'] = False
+            st.rerun()
+
 if __name__ == '__main__':
     main()
