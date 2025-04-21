2dd2f658599d1b63fe97fca741988fa9286399e6
Britodfbr <britodfbr@gmail.com>
2025-04-21 11:25:45 -0300

    feat: Conexão com DB;  e pequenas implementações necessárias

--- incolume/academia_jedi/ajedi20250420_streamlit_auth/__init__.py
+++ incolume/academia_jedi/ajedi20250420_streamlit_auth/__init__.py
@@ -4,73 +4,117 @@ Dependências:
    $ poetry add -G dev streamlit streamlit-authenticator psycopg2-binary python-dotenv
 
 """
+# ruff: noqa: E501
+
+from time import sleep
 from typing import Final
+
 import streamlit as st
 import streamlit_authenticator as stauth
-
+from icecream import ic
+from incolume.academia_jedi.ajedi20250420_streamlit_auth.dependencies import (
+    add_registro,
+    consulta,
+    consulta_geral,
+    cria_tabela,
+)
+from psycopg2 import OperationalError
 
 COOKIE_EXPIRY_DAYS: Final[int] = 30
 
 
 def main():
     """Main function."""
+    try:
+        consulta_geral()
+    except OperationalError:
+        cria_tabela()
+
+    db_query = consulta_geral()
+
+    registros = {'usernames': {}}
+    for data in db_query:
+        registros['usernames'][data[1]] = {
+            'name': data[0],
+            'password': data[2],
+        }
 
     authenticator = stauth.Authenticate(
-        {'usernames': {'teste': {'name': 'testando', 'passsword': 'senha secreta'}}},
+        registros,
         'random_cookie_name',
         'random_signature_key',
-
         COOKIE_EXPIRY_DAYS,
     )
     if 'clicou_registrar' not in st.session_state:
         st.session_state['clicou_registrar'] = False
 
-    if st.session_state['clicou_registrar'] == False:
+    if st.session_state['clicou_registrar'] is False:
         login_form(authenticator=authenticator)
+    else:
+        usuario_form()
 
 
-def login_form(authenticator: st.Authenticate) -> None:
-    """Login form."""
-    name, authentication_status, username = authenticator.login('main')
+def login_form(authenticator):
+    """Formulário de autenticação."""
+    name, authentication_status, username = authenticator.login('Login')
+    ic(f'{name=} {authentication_status=}, {username=}')
     if authentication_status:
         authenticator.logout('Logout', 'main')
         st.write(f'*{name} está logado!*')
         st.title('AREA DO DASHBOARD')
-    elif authentication_status == False:
+    elif authentication_status is False:
         st.error('Usuário ou senha incorretos')
-    elif authentication_status == None:
-        st.warning('Por favor informe um usuário e senha')
-        clicou_registrar = st.button("Registrar")
-        if 'clicou_registrar':
+    elif authentication_status is None:
+        st.warning('Insira um nome de usuário e uma senha')
+        clicou_em_registrar = st.button('Registrar')
+        if clicou_em_registrar:
             st.session_state['clicou_registrar'] = True
             st.rerun()
 
-def confirm_msg():
-    """Confirm msg."""
+
+def confirmation_msg():
+    """Mensagem de confirmação."""
     hashed_password = stauth.Hasher([st.session_state.pswrd]).generate()
     if st.session_state.pswrd != st.session_state.confirm_pswrd:
         st.warning('Senhas não conferem')
-    elif 'consulta_nome()':
-        st.warning('Nome de usuário já cadastrado.')
+        sleep(3)
+    elif consulta(st.session_state.user):
+        st.warning('Nome de usuário já existe.')
+        sleep(3)
     else:
-        'add_registro()'
+        add_registro(
+            st.session_state.nome,
+            st.session_state.user,
+            hashed_password[0],
+        )
         st.success('Registro efetuado!')
+        sleep(3)
 
 
 def usuario_form():
-    """Formulário de login."""
-    with st.form(key='formulario', clear_on_submit=True):
+    """Formulário de usuário."""
+    with st.form(key='test', clear_on_submit=True):
         nome = st.text_input('Nome', key='nome')
         username = st.text_input('Usuário', key='user')
-        password = st.text_input('Senha', key='pswrd', type='password')
-        confirm_password = st.text_input('Confirme senha', key='confir_pswrd', type='password')
-        submit = st.form_submit_button(
-            'Salvar', on_click=confirm_msg,
+        password = st.text_input('Password', key='pswrd', type='password')
+        confirm_password = st.text_input(
+            'Confirm Password',
+            key='confirm_pswrd',
+            type='password',
         )
-        clicou_fazer_login = st.button('Fazer login?')
-        if clicou_fazer_login:
-            st.session_state['clicou_fazer_login'] = False
-            st.rerun()
+
+        submit = st.form_submit_button(
+            'Salvar',
+            on_click=confirmation_msg,
+        )
+
+        ic(f'{nome=} {username=} {password=} {confirm_password=} {submit=}')
+
+    clicou_em_fazer_login = st.button('Fazer Login')
+    if clicou_em_fazer_login:
+        st.session_state['clicou_registrar'] = False
+        st.rerun()
+
 
 if __name__ == '__main__':
     main()
--- incolume/academia_jedi/ajedi20250420_streamlit_auth/dependencies.py
+++ incolume/academia_jedi/ajedi20250420_streamlit_auth/dependencies.py
@@ -1,12 +1,12 @@
 """DB conection."""
+# ruff: noqa: E501
 
+import os
+from contextlib import contextmanager
 
 import psycopg2
 from dotenv import load_dotenv
 from icecream import ic
-from contextlib import contextmanager
-import os
-
 
 load_dotenv()
 
@@ -16,15 +16,110 @@ USERSERVER = os.getenv('DB_USER')
 PASSWORD = os.getenv('DB_PASSWORD')
 PORT = os.getenv('DB_PORT')
 
+
 @contextmanager
 def instance_cursor():
-    """Cursor."""
-    conn = psycopg2.connect(database=DATABASE, host=HOST, user=USERSERVER, password=PASSWORD, port=PORT)
-    cursor = conn.cursor()
+    """Cria instancia do cursor.
+
+    O "cursor" é um objeto que permite que você execute comandos SQL no banco de dados e recupere os resultados.
+    Ele age como um ponteiro ou um marcador de posição dentro de uma transação ativa no banco de dados.
+    O cursor permite que você envie consultas SQL para o banco de dados, recuperar os resultados dessas consultas e, em seguida,
+    realizar operações como inserção, atualização e exclusão de dados.
+    """
+    connection = psycopg2.connect(
+        database=DATABASE,
+        host=HOST,
+        user=USERSERVER,
+        password=PASSWORD,
+        port=PORT,
+    )
+    cursor = connection.cursor()
     try:
         yield cursor
     finally:
-        if conn:
+        if connection:
             cursor.close()
-            conn.close()
-            ic('Conexão com PostgreSQL fechada.')
+            connection.close()
+            ic('Conexão com PostgreSQL encerrada')
+
+
+def consulta(user):
+    """Consulta usuário."""
+    with instance_cursor() as cursor:
+        query = """
+                SELECT nome, usuario, senha
+                FROM REGISTROS
+                WHERE usuario = %s
+                """
+        cursor.execute(query, (user,))
+        return cursor.fetchall()
+
+
+def consulta_geral():
+    """Consulta todos os registros."""
+    with instance_cursor() as cursor:
+        query = """
+                SELECT *
+                FROM REGISTROS
+                """
+        cursor.execute(
+            query,
+        )
+        return cursor.fetchall()
+
+
+def add_registro(nome, user, senha):
+    """Adiciona registros na tabela."""
+    connection = psycopg2.connect(
+        database=DATABASE,
+        host=HOST,
+        user=USERSERVER,
+        password=PASSWORD,
+        port=PORT,
+    )
+    cursor = connection.cursor()
+
+    query = f"""
+        INSERT INTO REGISTROS VALUES
+        {nome, user, senha}
+        """
+    cursor.execute(query)
+    connection.commit()
+    if connection:
+        cursor.close()
+        connection.close()
+        ic('Conexão com PostgreSQL encerrada')
+
+
+def cria_tabela():
+    """Cria a tabela se não existir."""
+    ic(DATABASE)
+    ic(HOST)
+    ic(USERSERVER)
+    ic(PASSWORD)
+    ic(PORT)
+
+    connection = psycopg2.connect(
+        database=DATABASE,
+        host=HOST,
+        user=USERSERVER,
+        password=PASSWORD,
+        port=PORT,
+    )
+    cursor = connection.cursor()
+    ic('AAAAAAA')
+    ic(connection)
+    query = """
+        CREATE TABLE REGISTROS (
+            nome varchar(255),
+            usuario varchar(255),
+            senha varchar(255)
+        )
+        """
+    cursor.execute(query)
+    connection.commit()
+    ic('Tabela criada')
+    if connection:
+        cursor.close()
+        connection.close()
+        ic('Conexão com PostgreSQL encerrada')
