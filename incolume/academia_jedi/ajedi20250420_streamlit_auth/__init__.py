74032b08251b2747d2a483b806d45680d675417a
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-04-22 10:40:10 -0300

    revert: "fix: Tratativa com exemplo da documentação"
    This reverts commit c10cfafc0ed6e92b0aedc683d2141ee3674b9f96.

--- incolume/academia_jedi/ajedi20250420_streamlit_auth/__init__.py
+++ incolume/academia_jedi/ajedi20250420_streamlit_auth/__init__.py
@@ -57,24 +57,20 @@ def main():
 
 def login_form(authenticator):
     """Formulário de autenticação."""
-    # name, authentication_status, username = authenticator.login('Login')
-    # ic(f'{name=} {authentication_status=}, {username=}')
-    # if authentication_status:
-    #     authenticator.logout('Logout', 'main')
-    #     st.write(f'*{name} está logado!*')
-    #     st.title('AREA DO DASHBOARD')
-    # elif authentication_status is False:
-    #     st.error('Usuário ou senha incorretos')
-    # elif authentication_status is None:
-    #     st.warning('Insira um nome de usuário e uma senha')
-    #     clicou_em_registrar = st.button('Registrar')
-    #     if clicou_em_registrar:
-    #         st.session_state['clicou_registrar'] = True
-    #         st.rerun()
-    try:
-        ic(authenticator.login(location='main', key='Login'))
-    except Exception as e:
-        st.error(e)
+    name, authentication_status, username = authenticator.login('Login')
+    ic(f'{name=} {authentication_status=}, {username=}')
+    if authentication_status:
+        authenticator.logout('Logout', 'main')
+        st.write(f'*{name} está logado!*')
+        st.title('AREA DO DASHBOARD')
+    elif authentication_status is False:
+        st.error('Usuário ou senha incorretos')
+    elif authentication_status is None:
+        st.warning('Insira um nome de usuário e uma senha')
+        clicou_em_registrar = st.button('Registrar')
+        if clicou_em_registrar:
+            st.session_state['clicou_registrar'] = True
+            st.rerun()
 
 
 def confirmation_msg():
