289b7f38fcb57be664bfcea748164c47b68250d2
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-04-22 09:52:42 -0300

    refactor: Tratativa em recuperar a informação de login

--- incolume/academia_jedi/ajedi20250420_streamlit_auth/__init__.py
+++ incolume/academia_jedi/ajedi20250420_streamlit_auth/__init__.py
@@ -57,15 +57,17 @@ def main():
 
 def login_form(authenticator):
     """Formulário de autenticação."""
-    name, authentication_status, username = authenticator.login('Login')
+    name, authentication_status, username = authenticator.login(location='main', key='Login') or None, None, None
     ic(f'{name=} {authentication_status=}, {username=}')
+
     if authentication_status:
         authenticator.logout('Logout', 'main')
         st.write(f'*{name} está logado!*')
         st.title('AREA DO DASHBOARD')
     elif authentication_status is False:
         st.error('Usuário ou senha incorretos')
-    elif authentication_status is None:
+
+    if authentication_status is None:
         st.warning('Insira um nome de usuário e uma senha')
         clicou_em_registrar = st.button('Registrar')
         if clicou_em_registrar:
