4cfd52a0d9e66887cd34ab2cf5af042e03e57c3c
Britodfbr <britodfbr@gmail.com>
2025-04-23 21:58:42 -0300

    test: implementado testes para write_msg

--- incolume/academia_jedi/ajedi20250423_chat_streamlit/test_ajedi20250423_chat_streamlit.py
+++ incolume/academia_jedi/ajedi20250423_chat_streamlit/test_ajedi20250423_chat_streamlit.py
@@ -1,1 +1,21 @@
 """Chat streamlit."""
+
+import pytest
+import incolume.academia_jedi.ajedi20250423_chat_streamlit as pkg
+
+
+class TestCase:
+    """TestCase."""
+
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            pytest.param(
+                ('user one', 'user two', {'as': 'as'}),
+                {'mensagens', 'user_one-user_two-20250423.pkl'},
+            ),
+        ],
+    )
+    def test_0(self, entrance, expected):
+        """Unittest."""
+        assert expected.issubset(pkg.write_msg(*entrance).parts)
