9d7b01928e398ba17414e33b1ed6c26ca009f5cd
Britodfbr <britodfbr@gmail.com>
2025-04-26 17:34:50 -0300

    feat: implementação do primeiro dash

--- incolume/academia_jedi/ajedi20250426_estudo_dash/__init__.py
+++ incolume/academia_jedi/ajedi20250426_estudo_dash/__init__.py
@@ -1,1 +1,32 @@
 """Estudos com dash."""
+import dash
+import plotly.express as px
+import pandas as pd
+
+
+app = dash.Dash(__name__)
+
+
+df0 = pd.DataFrame({
+    'Fruit':['Apple', 'Orange', 'Banana', 'Grapes', 'Apple', 'Banana'],
+    'Amount':[4, 1, 2,2,4, 5],
+    'City':['SF', 'SF', 'SF', 'Montreal', 'Montreal', 'Montreal'],},
+)
+
+fig = px.bar(df0, x='Fruit', y='Amount', color='City')
+
+
+app.layout = dash.html.Div(
+    id='div1',
+    children=[
+        dash.html.H1(children='Hello Dash'),
+        dash.html.Div(children='''Dash: A web application framework for Python.'''),
+        dash.dcc.Graph(
+            id='graph',
+            figure=fig
+    )
+])
+
+if __name__ == "__main__":
+    # Executa o script principal
+    app.run(debug=True, port=8050)
