57d58e11c817c081a677a91c2248ce092bc0869a
Britodfbr <britodfbr@gmail.com>
2025-04-26 16:43:47 -0300

    revert: "fix: contextlib.Supress erro para datefmt em logging"
    This reverts commit 52d5a12d0b5dffb702a203a6d38126c8b55fc437.

--- incolume/academia_jedi/__init__.py
+++ incolume/academia_jedi/__init__.py
@@ -1,7 +1,6 @@
 """Module academia_jedi."""
 
 import logging
-from contextlib import suppress
 from pathlib import Path
 
 from config import settings
@@ -17,8 +16,7 @@ logging.basicConfig(
     datefmt=settings.datefmt,
 )
 
-with suppress(ValueError):
-    logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 version_file = Path(__file__).parent / 'version.txt'
 project_file = Path(__file__).parents[2] / 'pyproject.toml'
