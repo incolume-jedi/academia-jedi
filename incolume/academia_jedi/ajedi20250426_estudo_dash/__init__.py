a04e403284c3c452e6fcd79f6f608d9372938fdc
Britodfbr <britodfbr@gmail.com>
2025-04-27 08:54:28 -0300

    feat: customização CSS inline

--- incolume/academia_jedi/ajedi20250426_estudo_dash/__init__.py
+++ incolume/academia_jedi/ajedi20250426_estudo_dash/__init__.py
@@ -1,19 +1,22 @@
 """Estudos com dash."""
+
 import dash
-import plotly.express as px
 import pandas as pd
+import plotly.express as px
 
 stylesheets = [
-'https://pastebin.com/raw/rbW8zaDe',
+    'https://pastebin.com/raw/rbW8zaDe',
 ]
 
 app = dash.Dash(__name__, external_stylesheets=stylesheets)
 
 
-df0 = pd.DataFrame({
-    'Fruit':['Apple', 'Orange', 'Banana', 'Grapes', 'Apple', 'Banana'],
-    'Amount':[4, 1, 2,2,4, 5],
-    'City':['SF', 'SF', 'SF', 'Montreal', 'Montreal', 'Montreal'],},
+df0 = pd.DataFrame(
+    {
+        'Fruit': ['Apple', 'Orange', 'Banana', 'Grapes', 'Apple', 'Banana'],
+        'Amount': [4, 1, 2, 2, 4, 5],
+        'City': ['SF', 'SF', 'SF', 'Montreal', 'Montreal', 'Montreal'],
+    },
 )
 
 fig = px.bar(df0, x='Fruit', y='Amount', color='City')
@@ -23,13 +26,16 @@ app.layout = dash.html.Div(
     id='div1',
     children=[
         dash.html.H1(children='Hello Dash'),
-        dash.html.Div(children='''Dash: A web application framework for Python.'''),
-        dash.dcc.Graph(
-            id='graph',
-            figure=fig
-    )
-])
+        dash.html.Div(
+            children=dash.html.P(
+                'Dash: A web application framework for Python.',
+                style={'color': '#aa00aa'},
+            ),
+        ),
+        dash.dcc.Graph(id='graph', figure=fig),
+    ],
+)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # Executa o script principal
-    app.run(debug=True, port=8050)
+    app.run(debug=True, port=8051)
