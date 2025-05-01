a67d80e5aa1d35510f8b3340c359aa3e9fbdb3fe
Britodfbr <britodfbr@gmail.com>
2025-05-01 09:13:15 -0300

    feat: implementação inicial e respectivos testes unitários

--- incolume/academia_jedi/ajedi20250430_py_email/__init__.py
+++ incolume/academia_jedi/ajedi20250430_py_email/__init__.py
@@ -1,1 +1,55 @@
 """Email with python."""
+
+import json
+from dataclasses import asdict, dataclass
+from pathlib import Path
+from typing import Final
+
+from icecream import ic
+from imbox import Imbox
+from incolume.academia_jedi import logger
+
+CREDENTIALS_PATH: Final[Path] = (
+    Path(__file__).parents[3] / 'credentials' / 'credentials.json'
+)
+SMTP_SERVER: Final[str] = 'smtp.gmail.com'
+IMAP_SERVER: Final[str] = 'imap.gmail.com'
+
+logger.info(ic(f'{CREDENTIALS_PATH=}, {SMTP_SERVER=}, {IMAP_SERVER=}'))
+
+
+@dataclass
+class Email:
+    """Email class."""
+
+    username: str
+    password: str
+    hostname: str = 'imap.gmail.com'
+
+
+def get_credentials(credentials: Path | None = None):
+    """Get the credentials from the credentials.json file."""
+    credentials = credentials or CREDENTIALS_PATH
+    logger.debug(ic(f'{credentials=}'))
+    with credentials.open() as file:
+        credentials_data = json.load(file)
+
+    logger.debug(ic(f'{credentials_data=}'))
+    return {
+        'hostname': 'imap.google.com',
+        'username': credentials_data['email'],
+        'password': credentials_data['google_password'],
+    }
+
+
+# with Imbox(**get_credentials()) as inbox:
+#    # Get unread messages
+#    unread_messages = inbox.messages(unread=True)
+#
+#    print(len(unread_messages))
+#    # for uid, message in unread_messages:
+#    #     print(f'From: {message.sent_from}')
+#    #     print(f'Subject: {message.subject}')
+#    #     print(f'Date: {message.date_str}')
+#    #     print(f"Body: {message.body['plain']}")
+#    #     print('-' * 40)
--- incolume/academia_jedi/ajedi20250430_py_email/test_ajedi20250430_py_email.py
+++ incolume/academia_jedi/ajedi20250430_py_email/test_ajedi20250430_py_email.py
@@ -1,1 +1,68 @@
 """Email with python."""
+
+import pytest
+import incolume.academia_jedi.ajedi20250430_py_email as pkg
+from pathlib import Path
+from tempfile import gettempdir
+import inspect
+import json
+
+
+class TestPyEmail:
+    """Test the ajedi20250430_py_email package.
+
+    This class contains test methods for the ajedi20250430_py_email package.
+    """
+
+    credentials_file: Path = Path(
+        gettempdir(),
+        inspect.stack()[0][3],
+        'credentials.json',
+    )
+    credentials: dict[str, str] = {
+        'email': 'dev@example.com',
+        'google_password': 'xpto',
+    }
+
+    @classmethod
+    def setup_method(cls):
+        """Setup method.
+
+        This method is called before for set up the test environment.
+        """
+        cls.credentials_file.parent.mkdir(parents=True, exist_ok=True)
+        cls.credentials_file.write_text(
+            json.dumps(cls.credentials),
+            encoding='utf-8',
+        )
+
+    def test_import(self):
+        """Test the import of the ajedi20250430_py_email package."""
+        assert pkg.__name__ == 'incolume.academia_jedi.ajedi20250430_py_email'
+
+    @pytest.mark.parametrize(
+        'entrance',
+        [
+            pytest.param(pkg.CREDENTIALS_PATH),
+            pytest.param(credentials_file),
+        ],
+    )
+    def test_credentials_path(self, entrance):
+        """Test the credentials path."""
+        assert getattr(entrance, 'is_file')
+
+    def test_credentials(self):
+        """Test the credentials."""
+        assert pkg.get_credentials(self.credentials_file)
+
+    def test_email_class(self):
+        """Test the email class."""
+        expected = {
+            'username': 'dev@example.com',
+            'password': 'xpto',
+            'hostname': 'imap.google.com',
+        }
+        assert (
+            pkg.asdict(pkg.Email(**pkg.get_credentials(self.credentials_file)))
+            == expected
+        )
