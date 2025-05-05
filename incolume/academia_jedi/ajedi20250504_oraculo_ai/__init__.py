c843fb0bc156cf5c35bcc17c3b09514f53401641
Britodfbr <britodfbr@gmail.com>
2025-05-04 22:40:24 -0300

    refactor: pequenos Ajutes

--- incolume/academia_jedi/ajedi20250504_oraculo_ai/__init__.py
+++ incolume/academia_jedi/ajedi20250504_oraculo_ai/__init__.py
@@ -1,8 +1,11 @@
 """Module oracle."""
 
 from typing import Final
+
 import streamlit as st
 from langchain.memory import ConversationBufferMemory
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
 
 
 SOURCES: Final[list[str]] = [
@@ -16,48 +19,61 @@ SOURCES: Final[list[str]] = [
 MODELOS_AI: Final[list[str]] = {
     'Groq': {
         'modelos': [
-            'gemma2-9b-it', 'llama-3.3-70b-versatile', 'whisper-large-v3-turbo', 'distil-whisper-large-v3-en',
+            'gemma2-9b-it',
+            'llama-3.3-70b-versatile',
+            'whisper-large-v3-turbo',
+            'distil-whisper-large-v3-en',
         ],
+        'chat': ChatGroq,
     },
-    'OpenAI':{
+    'OpenAI': {
         'modelos': [
             'gpt-4o-mini',
-        'o4-mini',
+            'o4-mini',
         ],
+        'chat': ChatOpenAI,
     },
 }
 
 cache_memory = ConversationBufferMemory()
 cache_memory.chat_memory.add_user_message('Olá')
-cache_memory.chat_memory.add_ai_message('Olá, sou o oráculo. Como posso ajudá-lo?')
+cache_memory.chat_memory.add_ai_message(
+    'Olá, sou o oráculo. Como posso ajudá-lo?',
+)
 
-MESSAGES = [
-    ('user', 'Olá'),
-    ('agent', 'Olá, sou o oráculo. Como posso ajudá-lo?'),
-]
+def load_model(provedor: str, modelo: str, api_key: str):
+    """Load the model."""
+    chat = MODELOS_AI[provedor]['chat'](model=modelo, api_key=api_key)
+    st.session_state['chat'] = chat
+    return chat
+
 
 def pg_chat():
     """Chat with the oracle."""
-    st.header("Oracle incolume", divider=True)
-    memoria = st.session_state.get("memoria", cache_memory)
+    st.header('Oracle incolume', divider=True)
+    chat = st.session_state.get('chat')
+    memoria = st.session_state.get('memoria', cache_memory)
 
     for mensagem in memoria.buffer_as_messages:
-        chat = st.chat_message(name=mensagem.type )
+        chat = st.chat_message(name=mensagem.type)
         chat.markdown(mensagem.content)
 
-    input_text = st.chat_input("Digite sua mensagem aqui..")
+    input_text = st.chat_input('Digite sua mensagem aqui..')
     if input_text:
         memoria.chat_memory.add_user_message(input_text)
+        resposta = chat.invoke(input_text).content
+        memoria.chat_memory.add_ai_message(resposta)
         st.session_state['memoria'] = memoria
         st.rerun()
+
 
 def sidebar():
     """Sidebar for the oracle."""
     tabs = st.tabs(['Upload de Arquivos', 'Seleção de Modelos'])
     with tabs[0]:
-        st.header("Upload de Arquivos")
+        st.header('Upload de Arquivos')
         source_selected = st.selectbox(
-            label="Selecione o tipo de arquivo",
+            label='Selecione o tipo de arquivo',
             options=SOURCES,
             index=0,
         )
@@ -67,33 +83,40 @@ def sidebar():
             selected = st.text_input('Digite a URL do vídeo')
         if source_selected == 'CSV':
             selected = st.file_uploader(
-                label="Selecione o arquivo CSV",
-                type=['.csv'])
+                label='Selecione o arquivo CSV', type=['.csv'],
+            )
         if source_selected == 'PDF':
             selected = st.file_uploader(
-                label="Selecione o arquivo PDF",
-                type=['.pdf'])
+                label='Selecione o arquivo PDF', type=['.pdf'],
+            )
         if source_selected == 'Excel':
             selected = st.file_uploader(
-                label="Selecione o arquivo Excel",
-                type=['.xlsx'])
+                label='Selecione o arquivo Excel', type=['.xlsx'],
+            )
         if source_selected == 'TXT':
             selected = st.file_uploader(
-                label="Selecione o arquivo Texto",
-                type=['.txt', '.yml', '.toml', '.yaml'])
+                label='Selecione o arquivo Texto',
+                type=['.txt', '.yml', '.toml', '.yaml'],
+            )
     with tabs[1]:
         provedor = st.selectbox('Selecione o provedor', MODELOS_AI.keys())
         modelo = st.selectbox(
-            label="Selecione o modelo",
+            label='Selecione o modelo',
             options=MODELOS_AI[provedor]['modelos'],
             index=0,
         )
         api_key = st.text_input(
-            label=f"Digite sua chave de API para {provedor}",
+            label=f'Digite sua chave de API para {provedor}',
             type='password',
             value=st.session_state.get(f'api_key_{provedor}', ''),
         )
         st.session_state[f'api_key_{provedor}'] = api_key
+
+    if st.button('Incializar agente', use_container_width=True):
+        load_model(
+            provedor=provedor,
+            modelo=modelo,
+            api_key=api_key,)
 
 
 
@@ -104,5 +127,5 @@ def main():
         sidebar()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
