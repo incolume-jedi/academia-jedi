b8ee92a8ba3bdfe952a924ce3d703529daa05637
Britodfbr <britodfbr@gmail.com>
2025-05-05 21:02:24 -0300

    refactor: reconfiguração de variáveis para arquivo yaml em vez de toml

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ settings = Dynaconf(
     envvar_prefix='AJEDII',
     load_dotenv=True,
     settings_files=[
-        Path(__file__).parent.joinpath('settings/settings.toml'),
+        Path(__file__).parent.joinpath('settings/settings.yml'),
         *Path(__file__).parent.rglob('settings/.secrets.*'),
     ],
     environments=[
