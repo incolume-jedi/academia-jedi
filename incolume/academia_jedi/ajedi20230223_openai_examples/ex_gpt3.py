415ab5be9cd91a7f2967fdc0c674d88441f38f6a
Britodfbr <britodfbr@gmail.com>
2025-05-05 21:28:43 -0300

    refactor: proteção de código implantada.

--- incolume/academia_jedi/ajedi20230223_openai_examples/ex_gpt3.py
+++ incolume/academia_jedi/ajedi20230223_openai_examples/ex_gpt3.py
@@ -5,15 +5,22 @@
 from config import settings
 from openai import OpenAI
 
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key=settings.OPENAI_API_KEY,
-)
 
-response = client.responses.create(
-    model='gpt-4o',
-    instructions='You are a coding assistant that talks like a pirate.',
-    input='How do I check if a Python object is an instance of a class?',
-)
+def run_agent():
+    """Run the agent."""
+    client = OpenAI(
+        # This is the default and can be omitted
+        api_key=settings.OPENAI_API_KEY,
+    )
 
-print(response.output_text)
+    response = client.responses.create(
+        model='gpt-4o',
+        instructions='You are a coding assistant that talks like a pirate.',
+        input='How do I check if a Python object is an instance of a class?',
+    )
+
+    print(response.output_text)
+
+
+if __name__ == '__main__':
+    run_agent()
