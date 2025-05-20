cd038d92a7389cad569f1e1b5f00f094bce306e3
Britodfbr <britodfbr@gmail.com>
2025-05-20 17:47:27 -0300

    refactor: Alterado o comando de execução e programa executado, com as dependências exclusivas do container

--- incolume/academia_jedi/ajedi20250520_estudo_container/__init__.py
+++ incolume/academia_jedi/ajedi20250520_estudo_container/__init__.py
@@ -1,5 +1,15 @@
+"""Estudo container."""
+
+# ruff:noqa: T201
+
+from icecream import ic
+import cowsay
+
+
+
 def main():
     print("Hello from ajedi20250520-estudo-container!")
+    cowsay.cow('mooo..')
 
 
 if __name__ == "__main__":
