e4367586db7a6f14f82a4a54b53cfa640e23bace
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-05-20 09:42:02 -0300

    style: QA aplicado

--- incolume/academia_jedi/ajedi20250516_task_estudo_llm/main.py
+++ incolume/academia_jedi/ajedi20250516_task_estudo_llm/main.py
@@ -1,6 +1,11 @@
+"""estudos LLM."""
+# ruff: noqa: T201
+
+
 def main():
-    print("Hello from ajedi20250516-task-estudo-llm!")
+    """Main."""
+    print('Hello from ajedi20250516-task-estudo-llm!')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
