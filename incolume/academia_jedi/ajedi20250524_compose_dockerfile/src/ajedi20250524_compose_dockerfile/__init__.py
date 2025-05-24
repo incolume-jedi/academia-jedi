2bcf09c52189682520b3fc1dbc965714b4973acd
Britodfbr <britodfbr@gmail.com>
2025-05-24 09:10:59 -0300

    chore: Estrutura base com namespace `uv` criada

---
+++ incolume/academia_jedi/ajedi20250524_compose_dockerfile/src/ajedi20250524_compose_dockerfile/__init__.py
@@ -0,0 +1,6 @@
+"""Module for the ajedi20250524-compose-dockerfile package."""
+
+
+def main() -> None:
+    """Main function for the ajedi20250524-compose-dockerfile module."""
+    print('Hello from ajedi20250524-compose-dockerfile!')
