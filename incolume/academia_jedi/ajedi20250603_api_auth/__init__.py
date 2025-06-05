8634dbf5a020e3f05ce2423a8d8431792f8b9823
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-06-05 08:52:35 -0300

    tests: refinamento dos testes

--- incolume/academia_jedi/ajedi20250603_api_auth/__init__.py
+++ incolume/academia_jedi/ajedi20250603_api_auth/__init__.py
@@ -15,18 +15,33 @@ def auth_basic(
     user: str = '',
     pw: str = '',
     url: str = 'https://httpbin.org/basic-auth/{user}/{pw}',
+    *,
+    update_credentials: bool = False,
 ) -> httpx.Response:
-    """Authenticate using Basic Auth."""
+    """Authenticate using Basic Auth.
+
+    Args:
+        user (str): Username for authentication.
+        pw (str): Password for authentication.
+        url (str): URL for the Basic Auth endpoint, defaulting to
+           'https://httpbin.org/basic-auth/{user}/{pw}'.
+        update_credentials (bool): If True, use provided user
+           and pw; otherwise, use default values.
+    """
     user = user or 'username'
     pw = pw or 'password'
+    url = (
+        url.format(user=user, pw=pw)
+        if update_credentials
+        else url.format(user='username', pw='password')
+    )
 
     auth_str = base64.b64encode(f'{user}:{pw}'.encode()).decode()
 
     ic(auth_str)
     headers = {'Authorization': f'Basic {auth_str}'}
 
-
-    response = httpx.get(url.format(user=user, pw=pw), headers=headers)
+    response = httpx.get(url, headers=headers)
     response.raise_for_status()  # Ensure we raise an error for bad responses
     ic(response.json())
     return response
@@ -36,7 +51,7 @@ def main() -> None:
     """Main function for ajedi20250603-api-auth."""
     print('Hello from ajedi20250603-api-auth!')  # noqa: T201
     auth_basic()  # Replace with actual credentials
-    auth_basic('figueredo', 'pudim') # Print the JSON response for debugging
+    auth_basic('figueredo', 'pudim')  # Print the JSON response for debugging
 
 
 if __name__ == '__main__':
--- incolume/academia_jedi/ajedi20250603_api_auth/test_api_auth.py
+++ incolume/academia_jedi/ajedi20250603_api_auth/test_api_auth.py
@@ -1,11 +1,40 @@
 """Test module."""
 
+import pytest
 from incolume.academia_jedi import ajedi20250603_api_auth as pkg
 
 
 class TestApiAuth:
     """Test class for API authentication."""
 
-    def test_api_auth(self):
+    @pytest.mark.parametrize(
+        'entrance expected'.split(),
+        [
+            pytest.param({}, {}),
+            pytest.param(
+                {'user': 'u171'},
+                {
+                    'expected_exception': pkg.httpx.HTTPStatusError,
+                    'match': "Client error '401 UNAUTHORIZED' for url 'https://httpbin.org/basic-auth/username/password'",
+                },
+            ),
+            pytest.param({'pw': '12345678'}, {}),
+            pytest.param(
+                {
+                    'user': 'root',
+                    'pw': '123456',
+                    'url': 'https://httpbin.org/basic-auth/root/123456',
+                },
+                {},
+            ),
+        ],
+    )
+    def test_api_auth(self, entrance, expected):
         """Test API authentication."""
-        assert isinstance(pkg.auth_basic(), pkg.httpx.Response)
+        result = pkg.auth_basic(**entrance)
+        assert isinstance(result, pkg.httpx.Response)
+        if 'expected_exception' in expected:
+            with pytest.raises(**expected):
+                result.raise_for_status()
+        else:
+            assert result.json().get('authenticated')
