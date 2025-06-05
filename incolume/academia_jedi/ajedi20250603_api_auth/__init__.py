a86bf725f2f55e0a1b4c8d6256d9380942f7106c
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-06-05 09:33:45 -0300

    feat: Acesso API OpenWeather
    user:satewoj;email:satewoj584@jeanssi.com;pw:Pcd4ztmub5ZHcxH;OPEN_WEATHER_API_KEY:3205dbf5b5a0b4c35915a2adad9f6c00

--- incolume/academia_jedi/ajedi20250603_api_auth/__init__.py
+++ incolume/academia_jedi/ajedi20250603_api_auth/__init__.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 import base64
 
 import httpx
+from config import settings
 from icecream import ic
 
 user = 'username'
@@ -42,6 +43,29 @@ def auth_basic(
     headers = {'Authorization': f'Basic {auth_str}'}
 
     response = httpx.get(url, headers=headers)
+    response.raise_for_status()  # Ensure we raise an error for bad responses
+    ic(response.json())
+    return response
+
+
+def auth_token(
+        city: str = '',
+        token: str = '',
+        url: str = 'https://api.openweathermap.org/data/2.5/weather',
+) -> httpx.Response:
+    """Authenticate using a token.
+
+    Args:
+        city (str): City name for the weather query, defaulting to 'Brasília'.
+        token (str): API token for OpenWeatherMap.
+        url (str): URL for the OpenWeatherMap API endpoint.
+    """
+    params = {
+        'appid': token or settings.OPEN_WEATHER_MAP_API_KEY,
+        'q': city or 'Brasília',
+        'units': 'metric',
+    }
+    response = httpx.get(url, params=params)
     response.raise_for_status()  # Ensure we raise an error for bad responses
     ic(response.json())
     return response
