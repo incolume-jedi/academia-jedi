cbfaabdb06aec619956006639479591a343ee865
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-06-05 12:02:47 -0300

    chore: Estrutura para modulo

---
+++ incolume/academia_jedi/ajedi20250606_acesso_openweather/__init__.py
@@ -0,0 +1,10 @@
+"""Module to access OpenWeatherMap API."""
+
+
+def main():
+    """Main function to run the OpenWeatherMap access module."""
+    print('Hello from ajedi1749132160-acesso-openweather!')  # noqa: T201
+
+
+if __name__ == '__main__':
+    main()
