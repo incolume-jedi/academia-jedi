915294f4226fc2dd5e8de3d5a69923d869b777d6
Britodfbr <britodfbr@gmail.com>
2025-06-06 18:07:17 -0300

    style: Interface streamlit para projeto

--- incolume/academia_jedi/ajedi20250606_acesso_openweather/main.py
+++ incolume/academia_jedi/ajedi20250606_acesso_openweather/main.py
@@ -8,7 +8,19 @@ def main():
     st.title('Web App Climatológico')
     st.write('Dados disponibilizados pela OpenWeatherMap (https://openweathermap.org/current)')
 
-    st.write(auth_token())
+    city = st.text_input('Informe o nome da cidade', key='city', value='Brasília')
+
+    if not city:
+        st.stop()
+
+    try:
+        result = auth_token(city=city, token='2126063e2374e8abb4c56139559f6f79')
+    except Exception as e:
+        raise
+        st.warning(f'Informações não disponíveis para a cidade "{city}".')
+        st.stop()
+
+    ic(result.json())
 
 
 if __name__ == '__main__':
