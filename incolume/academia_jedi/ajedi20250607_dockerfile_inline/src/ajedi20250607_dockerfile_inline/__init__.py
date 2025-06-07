527b8f6a29aa56bfc7b6d3b18d41f22d950986bf
Britodfbr <britodfbr@gmail.com>
2025-06-07 14:08:50 -0300

    feat: Estrutura docker completa somente com compose.yml; alpine + streamlit + uv + docker + namespace
    close #244

---
+++ incolume/academia_jedi/ajedi20250607_dockerfile_inline/src/ajedi20250607_dockerfile_inline/__init__.py
@@ -0,0 +1,6 @@
+"""Hello from ajedi20250607-dockerfile-inline!"""
+
+
+def main() -> str:
+    """Hello from ajedi20250607-dockerfile-inline!"""
+    return 'Hello from ajedi20250607-dockerfile-inline!'
