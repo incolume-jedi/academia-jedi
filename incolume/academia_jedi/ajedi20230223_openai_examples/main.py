e1f61a11c151f0dd52e8354ad678e300da9eebd1
Britodfbr <britodfbr@gmail.com>
2025-06-08 21:26:50 -0300

    chore(fix): Ajuste na configuração para funcionamento do módulo

---
+++ incolume/academia_jedi/ajedi20230223_openai_examples/main.py
@@ -0,0 +1,38 @@
+"""Main Module."""
+
+# ruff: noqa: T201
+
+import logging
+from platform import python_version
+
+from ajedi20230223_openai_examples import (
+    ex_ml_sklearn_nltk_spellchecker,
+    ex_spellchecker,
+    ex_textblob,
+)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s;%(levelname)-8s;%(name)s;'
+    '%(module)s;%(funcName)s;%(message)s',
+)
+
+if '3.8.6' > python_version() >= '3.11.0':
+    msg = (
+        f'Python-{python_version()} is incompatible! '
+        f'Use 3.8.6 < version < 3.11.0'
+    )
+    logging.warning(msg)
+    print(msg)
+
+
+def run():
+    """Run main module."""
+    logging.debug('starting ..')
+    ex_spellchecker.run()
+    ex_textblob.run()
+    ex_ml_sklearn_nltk_spellchecker.run()
+
+
+if __name__ == '__main__':
+    run()
