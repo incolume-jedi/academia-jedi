e71c377edeb258ee5d79e50f2e030a9be7d4045a
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-06-11 13:08:07 -0300

    style: Aplicado QA

--- incolume/academia_jedi/ajedi20250610_estudos_docker_compose/src/ajedi20250610_estudos_docker_compose/__init__.py
+++ incolume/academia_jedi/ajedi20250610_estudos_docker_compose/src/ajedi20250610_estudos_docker_compose/__init__.py
@@ -1,2 +1,6 @@
-def main() -> None:
-    print("Hello from ajedi20250610-estudos-docker-compose!")
+"""This module is part of the ajedi20250610_estudos_docker_compose package."""
+
+
+def main() -> str:
+    """Main function of the ajedi20250610_estudos_docker_compose package."""
+    return 'Hello from ajedi20250610-estudos-docker-compose!'
