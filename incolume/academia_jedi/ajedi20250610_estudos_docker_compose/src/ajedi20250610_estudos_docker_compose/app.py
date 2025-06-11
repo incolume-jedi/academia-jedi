bd45595422fc19c83a48f5058250fb06db3b494d
Britodfbr <britodfbr@gmail.com>
2025-06-11 19:28:27 -0300

    feat: Aplicação fastAPI

---
+++ incolume/academia_jedi/ajedi20250610_estudos_docker_compose/src/ajedi20250610_estudos_docker_compose/app.py
@@ -0,0 +1,37 @@
+"""Module API."""
+
+import os
+
+from fastapi import FastAPI
+from redis import Redis
+
+redis_host = os.environ.get('REDIS_HOST', 'redis')
+redis_port = os.environ.get('REDIS_PORT', 6379)
+
+
+app = FastAPI()
+redis = Redis(host=redis_host, port=redis_port)
+
+
+@app.get('/')
+async def root() -> dict[str, str]:
+    """Principal method.
+
+    Returns:
+        dict[str, str]: message getting.
+    """
+    redis.incr('hits-root')
+    return {'message': 'Hello World'}
+
+
+@app.get('/counter')
+async def counter() -> dict[str, int]:
+    """Counter access for API.
+
+    Returns:
+        int: Number of access.
+    """
+    redis.incr('hits-counter')
+    return {
+        'hits': redis.get('hits-root') + redis.get('hits-counter'),
+    }
