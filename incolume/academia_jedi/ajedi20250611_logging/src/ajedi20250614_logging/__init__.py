89c33ce2197be7661296477157c7a8b69b44dd6c
Britodfbr <britodfbr@gmail.com>
2025-06-14 19:54:17 -0300

    chore(feat): Estrutura do projeto

---
+++ incolume/academia_jedi/ajedi20250611_logging/src/ajedi20250614_logging/__init__.py
@@ -0,0 +1,14 @@
+"""Logging in an Application."""
+
+from ajedi20250611_logging.example01 import staff
+
+
+def main() -> str:
+    """Logging in an Application."""
+    staff()
+
+    return 'Hello from ajedi20250614-logging!'
+
+
+if __name__ == '__main__':
+    main()
