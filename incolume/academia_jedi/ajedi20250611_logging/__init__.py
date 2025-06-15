7876ba886b45eb796f024bd79a1f6c12d9a3e13c
Britodfbr <britodfbr@gmail.com>
2025-06-15 07:51:42 -0300

    feat: Carregamento de configuração logging dicionários

--- incolume/academia_jedi/ajedi20250611_logging/__init__.py
+++ incolume/academia_jedi/ajedi20250611_logging/__init__.py
@@ -2,13 +2,14 @@
 
 from icecream import ic
 from incolume.academia_jedi.ajedi20250611_logging.example01 import staff
-from incolume.academia_jedi.ajedi20250611_logging.example02 import load_conf_ini
+from incolume.academia_jedi.ajedi20250611_logging.example02 import load_conf_ini, load_conf_dict
 
 
 def main() -> str:
     """Logging in an Application."""
     staff()
     load_conf_ini()
+    load_conf_dict()
     return 'Hello from ajedi20250614-logging!'
 
 
