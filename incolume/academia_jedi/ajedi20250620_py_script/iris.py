fdb0600240ebd96862c38eeb8f4316199720c05f
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-06-23 09:26:53 -0300

    refactor: Adding Structure With Constants

--- incolume/academia_jedi/ajedi20250620_py_script/iris.py
+++ incolume/academia_jedi/ajedi20250620_py_script/iris.py
@@ -12,12 +12,12 @@ def main() -> None:
     """Chamada script iris."""
     print('Hello from script iris.py!')
 
-    urllib.request.urlretrieve(
-        'https://archive.ics.uci.edu/static/public/53/iris.zip',
-        'iris.zip',
-    )
+    IRIS_DATA_URL = 'https://archive.ics.uci.edu/static/public/53/iris.zip'
+    LOCAL_ZIP_FILENAME = 'iris.zip'
 
-    print('Downloaded iris.zip')
+    urllib.request.urlretrieve(IRIS_DATA_URL, LOCAL_ZIP_FILENAME)
+
+    print(f'Downloaded {LOCAL_ZIP_FILENAME}')
 
 
 if __name__ == '__main__':
