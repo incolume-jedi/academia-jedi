7a686da4f7918504b57913a12e52f6c808508405
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-06-23 09:14:41 -0300

    refactor: Modulo main substituído por iris

--- incolume/academia_jedi/ajedi20250620_py_script/iris.py
+++ incolume/academia_jedi/ajedi20250620_py_script/iris.py
@@ -1,11 +1,22 @@
+#!/usr/bin/env python3
 # /// script
 # requires-python = ">=3.10"
 # dependencies = []
 # ///
 
+import urllib.request
 
 def main() -> None:
+    """Chamada script iris."""
+
     print("Hello from iris.py!")
+
+    urllib.request.urlretrieve(
+        "https://archive.ics.uci.edu/static/public/53/iris.zip",
+        "iris.zip"
+    )
+
+    print("Downloaded iris.zip")
 
 
 if __name__ == "__main__":
--- incolume/academia_jedi/ajedi20250620_py_script/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print('Hello from ajedi20250620-py-script!')
-
-
-if __name__ == '__main__':
-    main()
