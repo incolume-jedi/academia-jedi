03338c7b02383a8532a51bc742f967de069dc720
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-06-23 09:47:10 -0300

    refactor: Managing Dependencies With PEP 723
    command:

--- incolume/academia_jedi/ajedi20250620_py_script/iris.py
+++ incolume/academia_jedi/ajedi20250620_py_script/iris.py
@@ -3,23 +3,23 @@
 # /// script
 # requires-python = ">=3.10"
 # dependencies = [
-#     "ucimlrepo",
+#     "ucilmrepo>=0.0.7",
 # ]
 # ///
 
-import urllib.request
+from ucimlrepo import fetch_ucirepo
 
 
+IRIS_DATASET_ID = 53
 def main() -> None:
     """Chamada script iris."""
     print('Hello from script iris.py!')
+    print("Fetching Iris dataset using ucimlrepo...")
+    iris = fetch_ucirepo(id=IRIS_DATASET_ID)
+    print("Dataset fetched successfully. Variable summary:")
+    print(iris.variables)
 
-    IRIS_DATA_URL = 'https://archive.ics.uci.edu/static/public/53/iris.zip'
-    LOCAL_ZIP_FILENAME = 'iris.zip'
 
-    urllib.request.urlretrieve(IRIS_DATA_URL, LOCAL_ZIP_FILENAME)
-
-    print(f'Downloaded {LOCAL_ZIP_FILENAME}')
 
 
 if __name__ == '__main__':
