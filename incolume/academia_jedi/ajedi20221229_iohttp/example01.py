8fef8cd76c9893920b7e912268d21aa31d0c58a4
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-06-24 13:05:32 -0300

    chore(fix): atualização do pacote aiohttp nos gerenciadores uv e poetry

--- incolume/academia_jedi/ajedi20221229_iohttp/example01.py
+++ incolume/academia_jedi/ajedi20221229_iohttp/example01.py
@@ -9,17 +9,19 @@ https://docs.aiohttp.org/en/stable/
 import asyncio
 
 import aiohttp
-
+from icecream import ic
 
 async def main():
     async with aiohttp.ClientSession() as session:
         async with session.get('http://python.org') as response:
-            print('Status:', response.status)
-            print('Content-type:', response.headers['content-type'])
+            ic('Status:', response.status)
+            ic('Content-type:', response.headers['content-type'])
 
             html = await response.text()
-            print('Body:', html[:15], '...')
+            ic(f'Body: {html[:15]} ...')
 
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+if __name__ == '__main__':
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(main())
+    asyncio.run(main())
