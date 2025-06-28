25c9650e9c0ca369515c6931e03d89ea123a88d4
Britodfbr <britodfbr@gmail.com>
2025-06-28 19:33:41 -0300

    test(feat): data classe para entrada de multiplos valores

--- incolume/academia_jedi/ajedi20250621_token_shortener/src/ajedi20250621_token_shortener/test_ajedi20250621_token_shortener.py
+++ incolume/academia_jedi/ajedi20250621_token_shortener/src/ajedi20250621_token_shortener/test_ajedi20250621_token_shortener.py
@@ -1,1 +1,18 @@
 """Tests for ajedi20250621_token_shortener."""
+
+import pytest
+from dataclasses import dataclass, field
+import base64
+import secrets
+from types import ModuleType
+from icecream import ic  # type: ignore[import-untyped]
+
+
+@dataclass
+class Entrance:
+    """Entrance data class."""
+
+    module: ModuleType = field(default=secrets)
+    method: str = field(default='token_urlsafe')
+    value: str = field(default=None)
+
