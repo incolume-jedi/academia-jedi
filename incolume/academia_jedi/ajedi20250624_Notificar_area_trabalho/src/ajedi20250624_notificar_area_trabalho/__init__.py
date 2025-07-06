f40952b76b0216ccf20712e776c63035c8e66f30
Britodfbr <britodfbr@gmail.com>
2025-07-06 09:32:13 -0300

    feat: Implementação conforme tutorial em vídeo
    close #276

--- incolume/academia_jedi/ajedi20250624_Notificar_area_trabalho/src/ajedi20250624_notificar_area_trabalho/__init__.py
+++ incolume/academia_jedi/ajedi20250624_Notificar_area_trabalho/src/ajedi20250624_notificar_area_trabalho/__init__.py
@@ -2,9 +2,25 @@
 
 from __future__ import annotations
 
+import time
+
 from icecream import ic
+from plyer import notification
 
 
 def main() -> None:
     """Main function for the ajedi20250624-notificar-area-trabalho package."""
     ic('Hello from ajedi20250624-notificar-area-trabalho!')
+    while True:
+        notification.notify(
+            title='Notificação de Área de Trabalho',
+            message='Esta é uma notificação de teste.',
+            app_name='ajedi20250624-notificar-area-trabalho',
+            timeout=10,  # Tempo em segundos
+            app_icon=None,  # Caminho para um ícone personalizado
+        )
+        time.sleep(10)  # Intervalo de 10 segundos entre notificações
+
+
+if __name__ == '__main__':
+    main()
