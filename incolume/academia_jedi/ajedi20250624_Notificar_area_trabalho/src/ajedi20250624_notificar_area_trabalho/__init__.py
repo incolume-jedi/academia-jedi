76e02d5276d85151ec438c12b337a56c08de80cf
Britodfbr <britodfbr@gmail.com>
2025-07-06 09:31:46 -0300

    chore(feat): Ambiente para a tarefa e instalação das dependências

---
+++ incolume/academia_jedi/ajedi20250624_Notificar_area_trabalho/src/ajedi20250624_notificar_area_trabalho/__init__.py
@@ -0,0 +1,10 @@
+"""Module for the ajedi20250624-notificar-area-trabalho package."""
+
+from __future__ import annotations
+
+from icecream import ic
+
+
+def main() -> None:
+    """Main function for the ajedi20250624-notificar-area-trabalho package."""
+    ic('Hello from ajedi20250624-notificar-area-trabalho!')
