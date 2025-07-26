e951209473dfc86d1cf4edde40e4ff63700bda4b
Britodfbr <britodfbr@gmail.com>
2025-07-26 09:05:03 -0300

    feat: Estudos com geração e validação de CPF e CNPJ com a biblioteca validate_docbr

--- incolume/academia_jedi/ajedi20250627_check_cpf_cnpj/src/ajedi20250627_check_cpf_cnpj/__init__.py
+++ incolume/academia_jedi/ajedi20250627_check_cpf_cnpj/src/ajedi20250627_check_cpf_cnpj/__init__.py
@@ -1,2 +1,5 @@
+"""Module."""
+
+
 def main() -> None:
     """Hello from ajedi20250627-check-cpf-cnpj!"""
--- incolume/academia_jedi/ajedi20250627_check_cpf_cnpj/src/ajedi20250627_check_cpf_cnpj/test_ajedi20250627_check_cpf_cnpj.py
+++ incolume/academia_jedi/ajedi20250627_check_cpf_cnpj/src/ajedi20250627_check_cpf_cnpj/test_ajedi20250627_check_cpf_cnpj.py
@@ -1,7 +1,7 @@
 """Test for ajedi20250627_check_cpf_cnpj package."""
 
 from __future__ import annotations
-from typing import NoReturn
+from typing import ClassVar, NoReturn
 import pytest
 from validate_docbr import CPF, CNPJ
 
@@ -9,18 +9,21 @@ from validate_docbr import CPF, CNPJ
 class TestAjedi20250627CheckCpfCnpj:
     """Test class for ajedi20250627_check_cpf_cnpj package."""
 
+    cpf: ClassVar = CPF()
+    cnpj: ClassVar = CNPJ()
+
     @pytest.mark.parametrize(
         ['entrance', 'expected'],
         [
             pytest.param('12345678900', False, marks=[]),
             pytest.param('00000000191', True, marks=[]),
             pytest.param('00000000272', True, marks=[]),
+            pytest.param(cpf.generate(), True, marks=[]),
         ],
     )
     def test_check_cpf(self, entrance, expected) -> NoReturn:
         """Test the main function."""
-        cpf = CPF()
-        assert cpf.validate(entrance) == expected
+        assert self.cpf.validate(entrance) == expected
 
     @pytest.mark.parametrize(
         ['entrance', 'expected'],
@@ -28,9 +31,11 @@ class TestAjedi20250627CheckCpfCnpj:
             pytest.param('12345678900', False, marks=[]),
             pytest.param('00000000191', False, marks=[]),
             pytest.param('00000000272', False, marks=[]),
+            pytest.param('87777426622054', True, marks=[]),
+            pytest.param('62898915347733', True, marks=[]),
+            pytest.param(cnpj.generate(), True, marks=[]),
         ],
     )
     def test_check_cnpj(self, entrance, expected) -> NoReturn:
         """Test the main function."""
-        cnpj = CNPJ()
-        assert cnpj.validate(entrance) == expected
+        assert self.cnpj.validate(entrance) == expected
