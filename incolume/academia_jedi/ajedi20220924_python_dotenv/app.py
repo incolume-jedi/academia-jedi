aaa90421a375b1edfbd0fb7bbf7b51fca5df3966
Britodfbr <britodfbr@gmail.com>
2025-08-12 22:03:08 -0300

    style: Aplicado QA

--- incolume/academia_jedi/ajedi20220924_python_dotenv/app.py
+++ incolume/academia_jedi/ajedi20220924_python_dotenv/app.py
@@ -4,13 +4,14 @@
 from os import environ, getenv
 
 from dotenv import load_dotenv
+from icecream import ic
 
 __author__ = '@britodfbr'  # pragma: no cover
 
 load_dotenv()
 
 for env in environ.items():
-    print(env)
-print(getenv('DOMAIN'))
-print(getenv('ADMIN_EMAIL'))
-print(getenv('ROOT_URL'))
+    ic(env)
+ic(getenv('DOMAIN'))
+ic(getenv('ADMIN_EMAIL'))
+ic(getenv('ROOT_URL'))
