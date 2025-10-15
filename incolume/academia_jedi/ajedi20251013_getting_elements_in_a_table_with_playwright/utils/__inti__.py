7ea6bd294eee905226620d9e0b8bfb41f5072677
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-10-15 08:16:44 -0300

    refactor!: subdivisão de código

---
+++ incolume/academia_jedi/ajedi20251013_getting_elements_in_a_table_with_playwright/utils/__inti__.py
@@ -0,0 +1,47 @@
+"""utils for submodule."""
+
+import tempfile
+from pathlib import Path
+from typing import Any
+
+dout = Path(tempfile.gettempdir()) / Path(__file__).parent.stem
+
+
+def config(content_index: str = '') -> Path:
+    """Config it."""
+    dsite: Path = dout / 'site-fake'
+    dsite.mkdir(parents=True, exist_ok=True)
+    site = dsite / 'index.html'
+    site.write_text(content_index)
+    return site
+
+
+def filename(**kwargs: [str, Any]) -> Path:
+    """Filename.
+
+    Args:
+        kwargs:
+          contains:
+            mode: OpenBinaryMode = "w+b",
+            buffering: int = -1,
+            encoding: str | None = None,
+            newline: str | None = None,
+            suffix: AnyStr@NamedTemporaryFile = .png,
+            prefix: AnyStr@NamedTemporaryFile = print-screen-,
+            dir: GenericPath[AnyStr@NamedTemporaryFile] = $OSTEMPFILEDIR,
+            delete: bool = True,
+            errors: str | None = None
+
+    """
+    args: dict[str, Any] = {
+        'mode': kwargs.get('mode', 'w+b'),
+        'buffering': kwargs.get('buffering', -1),
+        'encoding': kwargs.get('encoding'),
+        'suffix': kwargs.get('suffix', '.png'),
+        'prefix': kwargs.get('prefix', 'print-screen-'),
+        'dir': kwargs.get('dir', dout),
+        'delete': kwargs.get('delete', True),
+        'errors': kwargs.get('errors'),
+    }
+    with tempfile.NamedTemporaryFile(**args) as fl:
+        return fl.name
