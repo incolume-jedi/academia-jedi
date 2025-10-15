ba1c42be9bfea5e7f2b69cd80e1a33a5e5d1b80e
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-10-15 09:02:37 -0300

    refactor: #360  chamada dos modulos emancipados

--- incolume/academia_jedi/ajedi20251013_getting_elements_in_a_table_with_playwright/main.py
+++ incolume/academia_jedi/ajedi20251013_getting_elements_in_a_table_with_playwright/main.py
@@ -1,25 +1,45 @@
 """Module."""
 
+# ruff: noqa: E501
+
 import contextlib
-import tempfile
-from pathlib import Path
 from subprocess import Popen
-from typing import Any
 
 from icecream import ic
+from incolume.academia_jedi.ajedi20251013_getting_elements_in_a_table_with_playwright import (
+    example0 as ex0,
+)
+from incolume.academia_jedi.ajedi20251013_getting_elements_in_a_table_with_playwright import (
+    example1 as ex1,
+)
+from incolume.academia_jedi.ajedi20251013_getting_elements_in_a_table_with_playwright import (
+    utils,
+)
 
-from .utils import config
-from .example0 import html_text, automation1
+
+def tratativa1() -> None:
+    """Example 1."""
+    site = utils.config(ex0.html_text)
+    ic(site)
+    with contextlib.suppress(FileNotFoundError):
+        Popen(f'python -m http.server 8000 -d {site.parent}', shell=True)
+    ex0.automation1('http://localhost:8000', filename=utils.filename)
+
+def tratativa2() -> None:
+    """Example 2."""
+    site = utils.config(ex1.str_html)
+    ic(ex1.str_html)
+    ic(site)
+    with contextlib.suppress(FileNotFoundError):
+        Popen(f'python -m http.server 8000 -d {site.parent}', shell=True)
+    ex1.actions()
 
 
 def main() -> None:
     """Run it."""
     ic('Hello from ajedi20251013-getting-elements-in-a-table-with-playwright!')
-    site = config()
-    with contextlib.suppress(FileNotFoundError):
-        Popen(f'python -m http.server 8000 -d {site.parent}', shell=True)
-    ic(html_text)
-    automation1()
+    # tratativa1()
+    tratativa2()
 
 
 if __name__ == '__main__':
