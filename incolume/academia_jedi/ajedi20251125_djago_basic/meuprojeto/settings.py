75d344ba7f2d0c343546b08a2781e657896f721b
Britodfbr <britodfbr@gmail.com>
2025-11-25 16:06:00 -0300

    feat: #382 template global

--- incolume/academia_jedi/ajedi20251125_djago_basic/meuprojeto/settings.py
+++ incolume/academia_jedi/ajedi20251125_djago_basic/meuprojeto/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'meuprojeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
