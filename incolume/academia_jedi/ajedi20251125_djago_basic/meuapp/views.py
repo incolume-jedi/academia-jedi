f29352c9bec73ff502d26dfa4b64103c45c1fb09
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-11-25 09:21:18 -0300

    feat: #382 novo App para conexão ao projeto

---
+++ incolume/academia_jedi/ajedi20251125_djago_basic/meuapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+
+# Create your views here.
+def home(request: HttpRequest):
+    """Home."""
+    return HttpResponse('Olá, Mundo')
