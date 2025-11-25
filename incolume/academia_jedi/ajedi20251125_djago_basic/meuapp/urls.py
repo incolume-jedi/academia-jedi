c465ce6e3bd6b33d46cd247a9f0bb91bd691b227
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-11-25 09:38:04 -0300

    refactor: #382 configura rotas por app

--- incolume/academia_jedi/ajedi20251125_djago_basic/meuapp/urls.py
+++ incolume/academia_jedi/ajedi20251125_djago_basic/meuapp/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
-from meuapp import views
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('meuapp', include('meuapp.urls')),
+    path('', views.home, name='home'),
 ]
