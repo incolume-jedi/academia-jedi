7eebd157ed4b817039b88b0b05f11118371879f2
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-11-25 09:26:23 -0300

    refactor: #382 valores dinâmilos para view

--- incolume/academia_jedi/ajedi20251125_djago_basic/meuapp/views.py
+++ incolume/academia_jedi/ajedi20251125_djago_basic/meuapp/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
+from datetime import datetime
+from pytz import timezone
+
 
 # Create your views here.
 def home(request: HttpRequest):
     """Home."""
-    return HttpResponse('Olá, Mundo')
+    hora_atual = datetime.now(tz=timezone('America/Sao_Paulo'))
+
+    return HttpResponse(f'São {hora_atual}')
