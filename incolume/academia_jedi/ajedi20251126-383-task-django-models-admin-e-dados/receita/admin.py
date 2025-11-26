6ef3a888b6cdce5f7759973383b9152113ed622f
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-11-26 09:33:39 -0300

    refactor: #383 personalização de exibição, pesquisa e filtro

--- incolume/academia_jedi/ajedi20251126-383-task-django-models-admin-e-dados/receita/admin.py
+++ incolume/academia_jedi/ajedi20251126-383-task-django-models-admin-e-dados/receita/admin.py
@@ -1,7 +1,16 @@
+from typing import ClassVar
+
 from django.contrib import admin
+
 from .models import Categoria, Receita
 
 
+class ReceitasDisplay(admin.ModelAdmin):
+    """Class ReceitaDisplay."""
+    list_display: ClassVar[list[str]] = ['titulo', 'categoria', 'data_criado']
+    search_fields: ClassVar[list[str]] = ['titulo', 'categoria']
+    list_filter: ClassVar[list[str]] = ['categoria', 'data_criado']
+
 # Register your models here.
 admin.site.register(Categoria)
-admin.site.register(Receita)
+admin.site.register(Receita, ReceitasDisplay)
