43c1835d65f5a6038d769ca978982c765e751837
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-11-26 09:52:45 -0300

    feat: #383 acesso publico as receitas cadastradas

--- incolume/academia_jedi/ajedi20251126-383-task-django-models-admin-e-dados/receita/views.py
+++ incolume/academia_jedi/ajedi20251126-383-task-django-models-admin-e-dados/receita/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .models import Receita
+
+
 # Create your views here.
+def receitas(request):
+    """Receitas."""
+    receitas = Receita.objects.all()
+    context = {'receitas': receitas}
+
+    return render(request, 'minhas_receitas.html', context=context)
