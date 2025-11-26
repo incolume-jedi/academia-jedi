b3630ae8974a2ab16eeb4936345c234384b5b870
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-11-26 10:10:30 -0300

    refactor: #383 endpoint para detalhes_receita

--- incolume/academia_jedi/ajedi20251126-383-task-django-models-admin-e-dados/receita/urls.py
+++ incolume/academia_jedi/ajedi20251126-383-task-django-models-admin-e-dados/receita/urls.py
@@ -20,4 +20,5 @@ from . import views
 
 urlpatterns = [
     path('receitas/', views.receitas, name='receitas'),
+    path('detalhes_receita/<int:id_receita>/', views.detalhes_receita, name='detalhes_receita')
 ]
--- incolume/academia_jedi/ajedi20251126-383-task-django-models-admin-e-dados/receita/views.py
+++ incolume/academia_jedi/ajedi20251126-383-task-django-models-admin-e-dados/receita/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-
+from django.http import HttpRequest
 from .models import Receita
 
 
 # Create your views here.
-def receitas(request):
+def receitas(request: HttpRequest):
     """Receitas."""
     receitas = Receita.objects.all()
     context = {'receitas': receitas}
 
     return render(request, 'minhas_receitas.html', context=context)
+
+
+def detalhes_receita(request: HttpRequest, id_receita: int):
+    """Detalhes da receita."""
+    receita = Receita.objects.get(id=id_receita)
+    context = {'receita': receita}
+
+    return render(request, template_name='detalhes_receita.html', context=context)
