f4c92d8c6234a65cd670a61904df1129a943e52b
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-11-26 08:21:14 -0300

    feat: #383 criado modelos para DB via ORM

--- incolume/academia_jedi/ajedi20251126-383-task-django-models-admin-e-dados/receita/models.py
+++ incolume/academia_jedi/ajedi20251126-383-task-django-models-admin-e-dados/receita/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    """Class Categoria."""
+    nome = models.CharField(max_length=25)
+    def __str__(self) -> str:
+        return self.nome
+
+class Receita(models.Model):
+    """Class Receita."""
+    titulo = models.CharField(max_length=100)
+    ingredientes = models.TextField()
+    modo_preparo = models.TextField()
+    data_criado = models.DateTimeField(auto_now_add=True)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.titulo
+
