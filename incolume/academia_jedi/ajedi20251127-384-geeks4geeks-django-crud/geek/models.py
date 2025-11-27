138b3ca1f3d1849703a9b47d4bee70cf8a766851
Ricardo Brito do Nascimento <ricardo.brito@presidencia.gov.br>
2025-11-27 12:47:06 -0300

    refactor: #384 configuração para modelo

--- incolume/academia_jedi/ajedi20251127-384-geeks4geeks-django-crud/geek/models.py
+++ incolume/academia_jedi/ajedi20251127-384-geeks4geeks-django-crud/geek/models.py
@@ -1,3 +1,16 @@
+"""Models for geek."""
 from django.db import models
 
-# Create your models here.
+
+# declare a new model with a name "GeeksModel"
+class GeeksModel(models.Model):
+    """Models."""
+
+    # fields of the model
+    title = models.CharField(max_length = 200)
+    description = models.TextField()
+
+    # renames the instances of the model with their title name
+    def __str__(self):
+        """Dundler str."""
+        return self.title
