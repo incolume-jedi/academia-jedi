67eec4142d70365c491d96816578b6c48f19e553
Britodfbr <britodfbr@gmail.com>
2025-11-27 18:09:35 -0300

    feat: #384 view com template

--- incolume/academia_jedi/ajedi20251127-384-geeks4geeks-django-crud/geek/views.py
+++ incolume/academia_jedi/ajedi20251127-384-geeks4geeks-django-crud/geek/views.py
@@ -1,9 +1,17 @@
 from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
-
+from . import models
 
 # Create your views here.
 def geek_home(request: HttpRequest) -> HttpResponse:
     """View function for the geek home page."""
     return HttpResponse("Welcome to the Geek Home Page!")
 
+def list_view(request: HttpRequest) -> HttpResponse:
+    """List view function to display all GeekModel entries."""
+    # dictionary for initial data with field names as keys
+    context ={}
+
+    # add the dictionary during initialization
+    context["dataset"] = models.GeeksModel.objects.all()
+    return render(request, "list_view.html", context)
