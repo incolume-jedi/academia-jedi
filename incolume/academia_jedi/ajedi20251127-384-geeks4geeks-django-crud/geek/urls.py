96524175b2da7b180b87946366d4e51b52ac7b4a
Britodfbr <britodfbr@gmail.com>
2025-11-27 17:47:08 -0300

    refactor: #384 endpoints de acesso configurados

--- incolume/academia_jedi/ajedi20251127-384-geeks4geeks-django-crud/geek/urls.py
+++ incolume/academia_jedi/ajedi20251127-384-geeks4geeks-django-crud/geek/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for project_generic project.
+URL configuration for module.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,10 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('geek/', include('geek.urls')),
+    path('', view=views.geek_home, name='geek_home'),
 ]
--- incolume/academia_jedi/ajedi20251127-384-geeks4geeks-django-crud/geek/views.py
+++ incolume/academia_jedi/ajedi20251127-384-geeks4geeks-django-crud/geek/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
+
 # Create your views here.
+def geek_home(request: HttpRequest) -> HttpResponse:
+    """View function for the geek home page."""
+    return HttpResponse("Welcome to the Geek Home Page!")
+
