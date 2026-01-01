6a05e88d7e6aed55228937cd35862285ae42f0b3
Britodfbr <britodfbr@gmail.com>
2026-01-01 19:59:29 -0300

    fix: corrigido conflito de pacotes entre spellchecker e pyspellchecker

--- incolume/academia_jedi/ajedi20230222_pyspellchecker/tratativa1.py
+++ incolume/academia_jedi/ajedi20230222_pyspellchecker/tratativa1.py
@@ -1,11 +1,25 @@
 """Estudos com pyspellchecker."""
 
-
 # ruff: noqa: G004
 import logging
 
 from icecream import ic
 from spellchecker import SpellChecker
+
+
+def ex0():
+    """Exemplo da documentação em pypi."""
+    spell = SpellChecker()
+
+    # find those words that may be misspelled
+    misspelled = spell.unknown(['something', 'is', 'hapenning', 'here'])
+
+    for word in misspelled:
+        # Get the one `most likely` answer
+        ic(spell.correction(word))
+
+        # Get a list of `likely` options
+        ic(spell.candidates(word))
 
 
 def ex1():
